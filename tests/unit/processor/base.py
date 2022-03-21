5f141afe5fe7aaaceb9a72b25dffe354de803234
ekneg54 <ekneg54@pm.me>
2022-03-21 21:32:07 +0000

    Refactor BaseProcessorTestCase to be abstract
    needed to ensure the test methods in BaseProcessorTestCase are not run
    for BaseProcessorTestCase itself
    
    linting

--- tests/unit/processor/base.py
+++ tests/unit/processor/base.py
@@ -1,11 +1,12 @@
-from abc import abstractmethod
+# pylint: disable=missing-module-docstring
+from abc import ABC, abstractmethod
+from encodings import utf_8
 from logging import getLogger
 import json
-import unittest
 from logprep.processor.base.processor import RuleBasedProcessor
 
 
-class BaseProcessorTestCase(unittest.TestCase):
+class BaseProcessorTestCase(ABC):
 
     factory = None
 
@@ -18,35 +19,52 @@ class BaseProcessorTestCase(unittest.TestCase):
     @property
     @abstractmethod
     def specific_rules_dirs(self):
-        pass
+        """
+        the specific rules_dirs for the processor
+        has to be implemented
+        """
+        ...
 
     @property
     @abstractmethod
     def generic_rules_dirs(self):
-        pass
+        """
+        the generic rules_dirs for the processor
+        has to be implemented
+        """
+        ...
 
     def set_rules(self, rules_dirs):
+        """
+        sets the rules from the given rules_dirs
+        """
         specific_rules = list()
 
         for specific_rules_dir in rules_dirs:
-            rule_paths = RuleBasedProcessor._list_json_files_in_directory(
+            rule_paths = RuleBasedProcessor._list_json_files_in_directory(  # pylint: disable=protected-access
                 specific_rules_dir
             )
             for rule_path in rule_paths:
-                with open(rule_path, "r") as rule_file:
+                with open(rule_path, "r", encoding=utf_8) as rule_file:
                     rules = json.load(rule_file)
                     for rule in rules:
                         specific_rules.append(rule)
 
         return specific_rules
 
-    def setUp(self) -> None:
-        super().setUp()
-        self.object = self.object = self.factory.create(
-            "Test Instance Name", self.CONFIG, self.logger
-        )
-        self.specific_rules = self.set_rules(self.specific_rules_dirs)
-        self.generic_rules = self.set_rules(self.generic_rules_dirs)
+    def setUp(self) -> None:  # pylint: disable=invalid-name
+        """
+        setUp class for the imported TestCase
+        """
+        super().setUp()  # pylint: disable=no-member
+        if self.factory is not None:
+            self.object = self.factory.create(
+                "Test Instance Name", self.CONFIG, self.logger
+            )
+            self.specific_rules = self.set_rules(self.specific_rules_dirs)
+            self.generic_rules = self.set_rules(self.generic_rules_dirs)
 
     def test_my_false(self):
         assert True
+        if self.factory is None:
+            assert False
