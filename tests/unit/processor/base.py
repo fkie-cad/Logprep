e0418a2abd76fc190da6c4fd1e1290872c4a736e
ekneg54 <ekneg54@pm.me>
2022-03-21 20:20:21 +0000

    Adds BaseProcessorTestCase
    Co-authored-by: urdit52 <urdit52@users.noreply.github.com>

---
+++ tests/unit/processor/base.py
@@ -0,0 +1,52 @@
+from abc import abstractmethod
+from logging import getLogger
+import json
+import unittest
+from logprep.processor.base.processor import RuleBasedProcessor
+
+
+class BaseProcessorTestCase(unittest.TestCase):
+
+    factory = None
+
+    CONFIG = {}
+
+    logger = getLogger()
+
+    object = None
+
+    @property
+    @abstractmethod
+    def specific_rules_dirs(self):
+        pass
+
+    @property
+    @abstractmethod
+    def generic_rules_dirs(self):
+        pass
+
+    def set_rules(self, rules_dirs):
+        specific_rules = list()
+
+        for specific_rules_dir in rules_dirs:
+            rule_paths = RuleBasedProcessor._list_json_files_in_directory(
+                specific_rules_dir
+            )
+            for rule_path in rule_paths:
+                with open(rule_path, "r") as rule_file:
+                    rules = json.load(rule_file)
+                    for rule in rules:
+                        specific_rules.append(rule)
+
+        return specific_rules
+
+    def setUp(self) -> None:
+        super().setUp()
+        self.object = self.object = self.factory.create(
+            "Test Instance Name", self.CONFIG, self.logger
+        )
+        self.specific_rules = self.set_rules(self.specific_rules_dirs)
+        self.generic_rules = self.set_rules(self.generic_rules_dirs)
+
+    def test_my_false(self):
+        assert True
