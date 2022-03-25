e66dcf35dcb808dc6381a7d56b29d2a49940bb03
ekneg54 <ekneg54@pm.me>
2022-03-25 09:35:40 +0000

    Capitalize constant

--- tests/unit/processor/pseudonymizer/test_pseudonymizer.py
+++ tests/unit/processor/pseudonymizer/test_pseudonymizer.py
@@ -2,9 +2,7 @@ import copy
 import datetime
 import time
 from copy import deepcopy
-from json import load
-from logging import getLogger
-from unittest import mock, TestCase
+from unittest import mock
 
 import pytest
 from tests.unit.processor.base import BaseProcessorTestCase
@@ -17,14 +15,14 @@ from logprep.processor.processor_factory_error import ProcessorFactoryError
 from logprep.processor.pseudonymizer.factory import Pseudonymizer, PseudonymizerFactory
 from logprep.processor.pseudonymizer.rule import PseudonymizerRule
 
-cap_group_regex_mapping = (
+CAP_GROUP_REGEX_MAPPING = (
     "tests/testdata/unit/pseudonymizer/pseudonymizer_regex_mapping.yml"
 )
 
-cache_max_timedelta = datetime.timedelta(milliseconds=100)
+CACHE_MAX_TIMEDELTA = datetime.timedelta(milliseconds=100)
 
 
-class TestPseudonymizer(BaseProcessorTestCase, TestCase):
+class TestPseudonymizer(BaseProcessorTestCase):
 
     factory = PseudonymizerFactory
 
@@ -128,7 +126,7 @@ class TestPseudonymizer(BaseProcessorTestCase, TestCase):
         assert len(pseudonyms) == 1 and set(pseudonyms[0]) == {"pseudonym", "origin"}
 
     def test_recently_stored_pseudonyms_are_not_stored_again(self):
-        self.object._cache_max_timedelta = cache_max_timedelta
+        self.object._cache_max_timedelta = CACHE_MAX_TIMEDELTA
         self.object.setup()
         event = {"event_id": 1234, "something": "something"}
 
@@ -157,7 +155,7 @@ class TestPseudonymizer(BaseProcessorTestCase, TestCase):
             )
             assert len(pseudonyms) == 0
 
-            time.sleep(cache_max_timedelta.total_seconds())
+            time.sleep(CACHE_MAX_TIMEDELTA.total_seconds())
 
     def _load_specific_rule(self, rule, regex_mappping_path):
         self.object._load_regex_mapping(regex_mappping_path)
@@ -599,7 +597,7 @@ class TestPseudonymizer(BaseProcessorTestCase, TestCase):
             "pseudonymize": {"pseudo_this": regex_pattern},
             "url_fields": ["do_not_pseudo_this"],
         }
-        self._load_specific_rule(rule, cap_group_regex_mapping)
+        self._load_specific_rule(rule, CAP_GROUP_REGEX_MAPPING)
         self.object._pseudonymize_event(event)
 
         assert event["do_not_pseudo_this"] == url
@@ -624,7 +622,7 @@ class TestPseudonymizer(BaseProcessorTestCase, TestCase):
             },
             "url_fields": ["pseudo_this", "and_pseudo_this"],
         }
-        self._load_specific_rule(rule, cap_group_regex_mapping)
+        self._load_specific_rule(rule, CAP_GROUP_REGEX_MAPPING)
         self.object._pseudonymize_event(event)
 
         assert event["and_pseudo_this"] == pseudonymized_url
@@ -645,7 +643,7 @@ class TestPseudonymizer(BaseProcessorTestCase, TestCase):
             "pseudonymize": {"pseudo_this": regex_pattern},
             "url_fields": ["pseudo_this"],
         }
-        self._load_specific_rule(rule, cap_group_regex_mapping)
+        self._load_specific_rule(rule, CAP_GROUP_REGEX_MAPPING)
         self.object._pseudonymize_event(event)
 
         assert event["pseudo_this"] == pseudonymized
@@ -656,7 +654,7 @@ class TestPseudonymizer(BaseProcessorTestCase, TestCase):
             "filter": "filter_this: does_not_matter",
             "pseudonymize": {"pseudo_this": regex_pattern},
         }
-        self._load_specific_rule(rule, cap_group_regex_mapping)
+        self._load_specific_rule(rule, CAP_GROUP_REGEX_MAPPING)
         self.object._pseudonymize_event(event)
         return event
 
@@ -667,7 +665,7 @@ class TestPseudonymizer(BaseProcessorTestCase, TestCase):
             "pseudonymize": {"pseudo_this": regex_pattern},
             "url_fields": ["pseudo_this"],
         }
-        self._load_specific_rule(rule, cap_group_regex_mapping)
+        self._load_specific_rule(rule, CAP_GROUP_REGEX_MAPPING)
         self.object._pseudonymize_event(event)
         return event
 
