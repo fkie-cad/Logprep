96526632560f6b2c0904c79bb37ae59efe9feb83
ekneg54 <ekneg54@pm.me>
2022-03-31 06:27:54 +0000

    Fix imports in main

--- tests/unit/processor/base.py
+++ tests/unit/processor/base.py
@@ -4,8 +4,6 @@ import json
 import re
 from abc import ABC, abstractmethod
 from logging import getLogger
-import json
-import re
 
 import pytest
 
@@ -139,9 +137,7 @@ class BaseProcessorTestCase(ABC):
     def test_add_rules_from_directory(self):
         generic_rules_size = self.object._generic_tree.get_size()
         specific_rules_size = self.object._specific_tree.get_size()
-        self.object.add_rules_from_directory(
-            self.generic_rules_dirs, self.specific_rules_dirs
-        )
+        self.object.add_rules_from_directory(self.generic_rules_dirs, self.specific_rules_dirs)
         new_generic_rules_size = self.object._generic_tree.get_size()
         new_specific_rules_size = self.object._specific_tree.get_size()
         assert new_generic_rules_size > generic_rules_size
@@ -153,14 +149,10 @@ class BaseProcessorTestCase(ABC):
         in the rules directories
         ensures that every rule in rule tree is unique
         """
-        self.object.add_rules_from_directory(
-            self.generic_rules_dirs, self.specific_rules_dirs
-        )
+        self.object.add_rules_from_directory(self.generic_rules_dirs, self.specific_rules_dirs)
         generic_rules_size = self.object._generic_tree.get_size()
         specific_rules_size = self.object._specific_tree.get_size()
-        self.object.add_rules_from_directory(
-            self.generic_rules_dirs, self.specific_rules_dirs
-        )
+        self.object.add_rules_from_directory(self.generic_rules_dirs, self.specific_rules_dirs)
         new_generic_rules_size = self.object._generic_tree.get_size()
         new_specific_rules_size = self.object._specific_tree.get_size()
         assert new_generic_rules_size == generic_rules_size
