ad82f2d7212c10b2104ce55e5965567238342c80
ekneg54 <ekneg54@pm.me>
2022-04-20 09:26:17 +0000

    Add rules property to get all rules from rule_tree

--- logprep/framework/rule_tree/rule_tree.py
+++ logprep/framework/rule_tree/rule_tree.py
@@ -244,6 +244,19 @@ class RuleTree:
 
         return size
 
+    def _get_rules_as_list(self) -> List[Rule]:
+        """get all rules
+        Returns
+        -------
+        rules: List[Rule]
+        """
+
+        return [rule for rule in self._rule_mapping]
+
     @property
-    def root(self) -> Node:
+    def rules(self):  # pylint: disable=missing-docstring
+        return self._get_rules_as_list()
+
+    @property
+    def root(self) -> Node:  # pylint: disable=missing-docstring
         return self._root
