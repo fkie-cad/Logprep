f24e45d5900e25c829f0620d187872ed61ab2631
ekneg54 <ekneg54@pm.me>
2022-04-21 13:03:25 +0000

    Implement strategy pattern to template_replacer

--- logprep/processor/template_replacer/processor.py
+++ logprep/processor/template_replacer/processor.py
@@ -1,23 +1,14 @@
 """This module contains functionality replacing a text field using a template."""
-from time import time
-from typing import List
-from logging import Logger, DEBUG
-
-
+from logging import DEBUG, Logger
 from multiprocessing import current_process
+from typing import List
 
 from ruamel.yaml import YAML
-from logprep.framework.rule_tree.rule_tree import RuleTree
 
+from logprep.framework.rule_tree.rule_tree import RuleTree
 from logprep.processor.base.processor import RuleBasedProcessor
 from logprep.processor.template_replacer.rule import TemplateReplacerRule
-from logprep.processor.base.exceptions import (
-    InvalidRuleDefinitionError,
-    InvalidRuleFileError,
-)
-
 from logprep.util.processor_stats import ProcessorStats
-from logprep.util.time_measurement import TimeMeasurement
 
 yaml = YAML(typ="safe", pure=True)
 
@@ -131,36 +122,10 @@ class TemplateReplacer(RuleBasedProcessor):
 
     # pylint: enable=arguments-differ
 
-    def _load_rules_from_file(self, path: str):
-        try:
-            return TemplateReplacerRule.create_rules_from_file(path)
-        except InvalidRuleDefinitionError as error:
-            raise InvalidRuleFileError(self._name, path) from error
-
     def describe(self) -> str:
         return f"TemplateReplacer ({self._name})"
 
-    @TimeMeasurement.measure_time("template_replacer")
-    def process(self, event: dict):
-        self._event = event
-
-        for rule in self._generic_tree.get_matching_rules(event):
-            begin = time()
-            self._apply_rules(event)
-            processing_time = float("{:.10f}".format(time() - begin))
-            idx = self._generic_tree.get_rule_id(rule)
-            self.ps.update_per_rule(idx, processing_time)
-
-        for rule in self._specific_tree.get_matching_rules(event):
-            begin = time()
-            self._apply_rules(event)
-            processing_time = float("{:.10f}".format(time() - begin))
-            idx = self._specific_tree.get_rule_id(rule)
-            self.ps.update_per_rule(idx, processing_time)
-
-        self.ps.increment_processed_count()
-
-    def _apply_rules(self, event):
+    def _apply_rules(self, event, rule):
         _dict = self._mapping
         for field in self._fields:
             dotted_field_value = self._get_dotted_field_value(event, field)
