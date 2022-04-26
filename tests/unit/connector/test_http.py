62a1d6545f95a89bf9492992f0809575d3bd6f2e
ekneg54 <ekneg54@pm.me>
2022-04-26 09:32:23 +0000

    Add JSON and PLAINTEXT http connectors

--- tests/unit/connector/test_http.py
+++ tests/unit/connector/test_http.py
@@ -1,13 +1,19 @@
 # pylint: disable=missing-docstring
+# pylint: disable=protected-access
 import json
 from fastapi.testclient import TestClient
 from fastapi import FastAPI
-from logprep.connector.http.connector import HttpConnector
+
+from logprep.connector.http.connector import (
+    JSONHttpConnector,
+    HttpConnector,
+    PlaintextHttpConnector,
+)
 
 
-class TestHttpConnector:
+class TestJSONHttpConnector:
     def setup_method(self):
-        self.connector = HttpConnector()
+        self.connector = JSONHttpConnector()
         self.client = TestClient(self.connector.app)
 
     def test_create_connector(self):
@@ -16,7 +22,41 @@ class TestHttpConnector:
     def test_has_fastapi_app(self):
         assert isinstance(self.connector.app, FastAPI)
 
-    def test_accept_post_message(self):
-        data = json.dumps({"message": "my test message"})
-        resp = self.client.post("/event", data)
+    def test_endpoint_accepts_post_request(self):
+        data = {"message": "my log message"}
+        endpoint_path = self.connector._endpoint_path
+        resp = self.client.post(f"{endpoint_path}", json.dumps(data))
         assert resp.status_code == 200
+
+    def test_json_message_is_put_in_queue(self):
+        data = {"message": "my log message"}
+        resp = self.client.post("/json", json.dumps(data))
+        assert resp.status_code == 200
+        event_from_queue = self.connector._messages.get()
+        assert event_from_queue == data
+
+
+class TestPlaintextHttpConnector:
+    def setup_method(self):
+        self.connector = PlaintextHttpConnector()
+        self.client = TestClient(self.connector.app)
+
+    def test_create_connector(self):
+        assert isinstance(self.connector, HttpConnector)
+
+    def test_has_fastapi_app(self):
+        assert isinstance(self.connector.app, FastAPI)
+
+    def test_endpoint_accepts_post_request(self):
+        data = "my log message"
+        endpoint_path = self.connector._endpoint_path
+        resp = self.client.post(f"{endpoint_path}", data)
+        assert resp.status_code == 200
+
+    def test_json_message_is_put_in_queue(self):
+        data = "my log message"
+        headers = {"Content-type": "text/plain"}
+        resp = self.client.post("/plaintext", data=data, headers=headers)
+        assert resp.status_code == 200
+        event_from_queue = self.connector._messages.get()
+        assert event_from_queue.get("message") == data
