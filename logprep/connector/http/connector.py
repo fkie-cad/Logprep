ea905b98ff4d978dff665406b41dcfda665c141a
ekneg54 <ekneg54@pm.me>
2022-04-26 09:32:23 +0000

    Add JSON and PLAINTEXT http connectors

--- logprep/connector/http/connector.py
+++ logprep/connector/http/connector.py
@@ -1,22 +1,74 @@
 """ module for http connector """
-from fastapi import FastAPI
+from abc import ABC, abstractmethod
+import queue
+from fastapi import FastAPI, Request
 from pydantic import BaseModel  # pylint: disable=no-name-in-module
+from logprep.input.input import Input
 
 
-class Event(BaseModel):
-    message: str
+app = FastAPI()
 
 
-class HttpConnector:
+class HttpConnector(Input, ABC):
     """
     Connector to accept log messages as http post requests
     """
 
-    app: FastAPI = FastAPI()
+    app: FastAPI = app
+
+    _messages: queue.Queue = queue.Queue()
 
     def __init__(self) -> None:
-        self.app.add_api_route(path="/event", endpoint=self.message, methods=["POST"])
+        self.app.add_api_route(
+            path=f"{self._endpoint_path}", endpoint=self._endpoint, methods=["POST"]
+        )
 
-    def message(self, event: Event):
-        """index"""
-        return {"status": "SUCCESS", "data": event}
+    def describe_endpoint(self):
+        return f"{self.__class__.__name__}"
+
+    def get_next(self, timeout: float):
+        """returns the first message from the queue"""
+        return self._messages.get(timeout=timeout)
+
+    @abstractmethod
+    async def _endpoint(self, **kwargs):
+        """callback method for route"""
+        ...
+
+    @property
+    @abstractmethod
+    def _endpoint_path(self):
+        """returns the path where you want to receive"""
+        ...
+
+
+class JSONHttpConnector(HttpConnector):
+    """json endpoint http connector"""
+
+    class Event(BaseModel):
+        """model for event"""
+
+        message: str
+
+    async def _endpoint(self, event: Event):  # pylint: disable=arguments-differ
+        """json endpoint method"""
+        self._messages.put(event)
+
+    @property
+    def _endpoint_path(self):
+        """json endpoint path"""
+        return "/json"
+
+
+class PlaintextHttpConnector(HttpConnector):
+    """plaintext endpoint http connector"""
+
+    async def _endpoint(self, request: Request):  # pylint: disable=arguments-differ
+        """plaintext endpoint method"""
+        data = await request.body()
+        self._messages.put({"message": data.decode("utf8")})
+
+    @property
+    def _endpoint_path(self):
+        """plaintext endpoint path"""
+        return "/plaintext"
