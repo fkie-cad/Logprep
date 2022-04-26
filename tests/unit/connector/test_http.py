92aa754e9e5d86cf82587f49dc6348cdd3f239ed
ekneg54 <ekneg54@pm.me>
2022-04-26 13:34:21 +0000

    Refactor to endpoints and one connector

--- tests/unit/connector/test_http.py
+++ tests/unit/connector/test_http.py
@@ -4,16 +4,12 @@ import json
 from fastapi.testclient import TestClient
 from fastapi import FastAPI
 
-from logprep.connector.http.connector import (
-    JSONHttpConnector,
-    HttpConnector,
-    PlaintextHttpConnector,
-)
+from logprep.connector.http.connector import HttpConnector
 
 
-class TestJSONHttpConnector:
+class TestHttpConnector:
     def setup_method(self):
-        self.connector = JSONHttpConnector()
+        self.connector = HttpConnector()
         self.client = TestClient(self.connector.app)
 
     def test_create_connector(self):
@@ -22,10 +18,9 @@ class TestJSONHttpConnector:
     def test_has_fastapi_app(self):
         assert isinstance(self.connector.app, FastAPI)
 
-    def test_endpoint_accepts_post_request(self):
+    def test_json_endpoint_accepts_post_request(self):
         data = {"message": "my log message"}
-        endpoint_path = self.connector._endpoint_path
-        resp = self.client.post(f"{endpoint_path}", json.dumps(data))
+        resp = self.client.post("/json", json.dumps(data))
         assert resp.status_code == 200
 
     def test_json_message_is_put_in_queue(self):
@@ -35,28 +30,14 @@ class TestJSONHttpConnector:
         event_from_queue = self.connector._messages.get()
         assert event_from_queue == data
 
-
-class TestPlaintextHttpConnector:
-    def setup_method(self):
-        self.connector = PlaintextHttpConnector()
-        self.client = TestClient(self.connector.app)
-
-    def test_create_connector(self):
-        assert isinstance(self.connector, HttpConnector)
-
-    def test_has_fastapi_app(self):
-        assert isinstance(self.connector.app, FastAPI)
-
-    def test_endpoint_accepts_post_request(self):
+    def test_plaintext_endpoint_accepts_post_request(self):
         data = "my log message"
-        endpoint_path = self.connector._endpoint_path
-        resp = self.client.post(f"{endpoint_path}", data)
+        resp = self.client.post("/plaintext", data)
         assert resp.status_code == 200
 
-    def test_json_message_is_put_in_queue(self):
+    def test_plaintext_message_is_put_in_queue(self):
         data = "my log message"
-        headers = {"Content-type": "text/plain"}
-        resp = self.client.post("/plaintext", data=data, headers=headers)
+        resp = self.client.post("/plaintext", data=data)
         assert resp.status_code == 200
         event_from_queue = self.connector._messages.get()
         assert event_from_queue.get("message") == data
