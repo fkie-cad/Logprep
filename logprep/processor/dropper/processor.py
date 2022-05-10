ad08aab7c8ea407f60eac1d2a4086536649c2586
Piotr Pauksztelo <piotr.pauksztelo@fkie.fraunhofer.de>
2022-05-10 15:41:34 +0200

    Add ability to drop nested structure, refactor dropper and add tests

--- logprep/processor/dropper/processor.py
+++ logprep/processor/dropper/processor.py
@@ -1,17 +1,12 @@
 """This module contains a Dropper that deletes specified fields."""
 
-from typing import List, Optional, Union
 from logging import Logger, DEBUG
-
-from time import time
 from multiprocessing import current_process
+from typing import List
 
-from logprep.framework.rule_tree.rule_tree import RuleTree
 from logprep.processor.base.processor import RuleBasedProcessor
 from logprep.processor.dropper.rule import DropperRule
-
 from logprep.util.processor_stats import ProcessorStats
-from logprep.util.time_measurement import TimeMeasurement
 
 
 class DropperError(BaseException):
@@ -24,90 +19,82 @@ class DropperError(BaseException):
 class Dropper(RuleBasedProcessor):
     """Normalize log events by copying specific values to standardized fields."""
 
-    def __init__(self, name: str, tree_config: str, logger: Logger):
+    def __init__(self, name: str, configuration: dict, logger: Logger):
+        tree_config = configuration.get("tree_config")
+        specific_rules_dirs = configuration.get("specific_rules")
+        generic_rules_dirs = configuration.get("generic_rules")
         super().__init__(name, tree_config, logger)
-        self._logger = logger
         self.ps = ProcessorStats()
 
-        self._name = name
-        self._event = None
-        self._tree = RuleTree(config_path=tree_config)
+        self.add_rules_from_directory(
+            generic_rules_dirs=generic_rules_dirs,
+            specific_rules_dirs=specific_rules_dirs,
+        )
 
-    def add_rules_from_directory(self, rules_dirs: List[str]):
-        for rules_dir in rules_dirs:
-            rule_paths = self._list_json_files_in_directory(rules_dir)
+    # pylint: disable=arguments-differ
+    def add_rules_from_directory(
+        self, specific_rules_dirs: List[str], generic_rules_dirs: List[str]
+    ):
+        """Add rules from given directory."""
+        for specific_rules_dir in specific_rules_dirs:
+            rule_paths = self._list_json_files_in_directory(specific_rules_dir)
             for rule_path in rule_paths:
                 rules = DropperRule.create_rules_from_file(rule_path)
                 for rule in rules:
-                    self._tree.add_rule(rule, self._logger)
+                    self._specific_tree.add_rule(rule, self._logger)
+        for generic_rules_dir in generic_rules_dirs:
+            rule_paths = self._list_json_files_in_directory(generic_rules_dir)
+            for rule_path in rule_paths:
+                rules = DropperRule.create_rules_from_file(rule_path)
+                for rule in rules:
+                    self._generic_tree.add_rule(rule, self._logger)
+
         if self._logger.isEnabledFor(DEBUG):
             self._logger.debug(
-                "{} loaded {} rules ({})".format(
-                    self.describe(), self._tree.rule_counter, current_process().name
-                )
+                f"{self.describe()} loaded {self._specific_tree.rule_counter} "
+                f"specific rules ({current_process().name})"
             )
-        self.ps.setup_rules([None] * self._tree.rule_counter)
+            self._logger.debug(
+                f"{self.describe()} loaded {self._generic_tree.rule_counter} generic rules "
+                f"({current_process().name})"
+            )
+
+        self.ps.setup_rules(
+            [None] * self._generic_tree.rule_counter + [None] * self._specific_tree.rule_counter
+        )
 
     def describe(self) -> str:
         return f"Dropper ({self._name})"
 
-    @TimeMeasurement.measure_time("dropper")
-    def process(self, event: dict):
-        self._event = event
-        self._apply_rules()
-
-        self.ps.increment_processed_count()
-
-    def _field_exists(self, dotted_field: str) -> bool:
-        fields = dotted_field.split(".")
-        dict_ = self._event
-        for field in fields:
-            if field in dict_:
-                dict_ = dict_[field]
-            else:
-                return False
-        return True
-
-    def _get_dotted_field_value(self, dotted_field: str) -> Optional[Union[dict, list, str]]:
-        fields = dotted_field.split(".")
-        dict_ = self._event
-        for field in fields:
-            if field in dict_:
-                dict_ = dict_[field]
-        return dict_
-
-    def _traverse_dict(self, dict_: dict, sub_fields, drop_full: bool):
+    def _traverse_dict_and_delete(self, dict_: dict, sub_fields, drop_full: bool):
         sub_field = sub_fields[0] if sub_fields else None
+        remaining_sub_fields = sub_fields[1:]
+
+        if not remaining_sub_fields and sub_field in dict_.keys():
+            del dict_[sub_field]
         if (
             isinstance(dict_, dict)
             and sub_field in dict_
             and (isinstance(dict_[sub_field], dict) and dict_[sub_field])
         ):
-            self._traverse_dict(dict_[sub_field], sub_fields[1:], drop_full)
+            self._traverse_dict_and_delete(dict_[sub_field], remaining_sub_fields, drop_full)
             if dict_[sub_field] == {} and drop_full:
                 del dict_[sub_field]
         elif sub_field in dict_.keys():
             del dict_[sub_field]
 
-    def _drop_field(self, dotted_field: str, drop_full: bool):
+    def _drop_field(self, event: dict, dotted_field: str, drop_full: bool):
         sub_fields = dotted_field.split(".")
-        self._traverse_dict(self._event, sub_fields, drop_full)
+        self._traverse_dict_and_delete(event, sub_fields, drop_full)
 
-    def _apply_rules(self):
+    def _apply_rules(self, event: dict, rule: DropperRule):
         """Drops fields from event Logs."""
 
-        for rule in self._tree.get_matching_rules(self._event):
-            begin = time()
+        if self._logger.isEnabledFor(DEBUG):
+            self._logger.debug(f"{self.describe()} processing matching event")
+        for drop_field in rule.fields_to_drop:
+            self._try_dropping_field(event, drop_field, rule.drop_full)
 
-            if self._logger.isEnabledFor(DEBUG):
-                self._logger.debug("{} processing matching event".format(self.describe()))
-            for drop_field in rule.fields_to_drop:
-                self._try_dropping_field(drop_field, rule.drop_full)
-
-                processing_time = float("{:.10f}".format(time() - begin))
-                idx = self._tree.get_rule_id(rule)
-                self.ps.update_per_rule(idx, processing_time)
-
-    def _try_dropping_field(self, field: str, drop_full: bool):
-        if self._field_exists(field):
-            self._drop_field(field, drop_full)
+    def _try_dropping_field(self, event: dict, dotted_field: str, drop_full: bool):
+        if self._field_exists(event, dotted_field):
+            self._drop_field(event, dotted_field, drop_full)
---
+++ tests/unit/processor/dropper/test_dropper.py
@@ -0,0 +1,155 @@
+# pylint: disable=protected-access
+# pylint: disable=missing-docstring
+# pylint: disable=wrong-import-position
+# pylint: disable=wrong-import-order
+import pytest
+
+from tests.unit.processor.base import BaseProcessorTestCase
+
+pytest.importorskip("logprep.processor.dropper")
+
+import copy
+from logging import getLogger
+
+from logprep.processor.dropper.factory import Dropper, DropperFactory
+from logprep.processor.processor_factory_error import ProcessorFactoryError
+from logprep.processor.dropper.rule import DropperRule
+
+logger = getLogger()
+
+
+class TestDropper(BaseProcessorTestCase):
+    factory = DropperFactory
+
+    CONFIG = {
+        "type": "dropper",
+        "specific_rules": ["tests/testdata/unit/dropper/rules/specific/"],
+        "generic_rules": ["tests/testdata/unit/dropper/rules/generic/"],
+        "tree_config": "tests/testdata/unit/shared_data/tree_config.json",
+    }
+
+    @property
+    def specific_rules_dirs(self):
+        return self.CONFIG["specific_rules"]
+
+    @property
+    def generic_rules_dirs(self):
+        return self.CONFIG["generic_rules"]
+
+    def _load_specific_rule(self, rule):
+        specific_rule = DropperRule._create_from_dict(rule)
+        self.object._specific_tree.add_rule(specific_rule, self.logger)
+
+    def test_dropper_instantiates(self):
+        rule = {"filter": "drop_me", "drop": ["drop_me"]}
+        self._load_specific_rule(rule)
+        assert isinstance(self.object, Dropper)
+
+    def test_not_nested_field_gets_dropped_with_rule_loaded_from_file(self):
+        expected = {}
+        document = {"drop_me": "something"}
+        self.object.process(document)
+
+        assert document == expected
+
+    def test_nested_field_gets_dropped(self):
+        rule = {"filter": "drop.me", "drop": ["drop.me"]}
+        expected = {}
+        document = {"drop": {"me": "something"}}
+        self._load_specific_rule(rule)
+        self.object.process(document)
+
+        assert document == expected
+
+    def test_nested_field_with_neighbour_gets_dropped(self):
+        rule = {"filter": "keep_me.drop_me", "drop": ["keep_me.drop_me"]}
+        expected = {"keep_me": {"keep_me_too": "something"}}
+        document = {"keep_me": {"drop_me": "something", "keep_me_too": "something"}}
+        self._load_specific_rule(rule)
+        self.object.process(document)
+
+        assert document == expected
+
+    def test_deep_nested_field_gets_dropped(self):
+        rule = {"filter": "keep_me.drop.me", "drop": ["keep_me.drop.me"], "drop_full": False}
+        expected = {"keep_me": {"drop": {}}}
+        document = {"keep_me": {"drop": {"me": "something"}}}
+        self._load_specific_rule(rule)
+        self.object.process(document)
+
+        assert document == expected
+
+    def test_deep_nested_field_gets_dropped_fully(self):
+        rule = {"filter": "please.drop.me.fully", "drop": ["please.drop.me.fully"]}
+        expected = {}
+        document = {"please": {"drop": {"me": {"fully": "something"}}}}
+        self._load_specific_rule(rule)
+        self.object.process(document)
+
+        assert document == expected
+
+    def test_deep_nested_field_with_neighbour_gets_dropped(self):
+        rule = {"filter": "keep_me.drop.me", "drop": ["keep_me.drop.me"], "drop_full": False}
+        expected = {"keep_me": {"drop": {}, "keep_me_too": "something"}}
+        document = {"keep_me": {"drop": {"me": "something"}, "keep_me_too": "something"}}
+        self._load_specific_rule(rule)
+        self.object.process(document)
+
+        assert document == expected
+
+    def test_nested_field_with_child_gets_dropped(self):
+        rule = {"filter": "drop.child", "drop": ["drop"]}
+        expected = {}
+        document = {"drop": {"child": "something"}}
+        self._load_specific_rule(rule)
+        self.object.process(document)
+
+        assert document == expected
+
+    def test_deep_nested_field_with_child_gets_dropped(self):
+        rule = {"filter": "drop.me", "drop": ["drop.me"]}
+        expected = {}
+        document = {"drop": {"me": {"child": "foo"}}}
+        self._load_specific_rule(rule)
+        self.object.process(document)
+
+        assert document == expected
+
+    def test_deep_nested_field_with_child_and_neighbour_gets_dropped(self):
+        rule = {"filter": "drop.me", "drop": ["drop.me"]}
+        expected = {"drop": {"neighbour": "bar"}}
+        document = {"drop": {"me": {"child": "foo"}, "neighbour": "bar"}}
+        self._load_specific_rule(rule)
+        self.object.process(document)
+
+        assert document == expected
+
+    def test_deep_nested_field_with_child_and_not_drop_full_gets_partially_dropped(self):
+        rule = {"filter": "drop.me", "drop": ["drop.me"], "drop_full": False}
+        expected = {"drop": {}}
+        document = {"drop": {"me": {"child": "foo"}}}
+        self._load_specific_rule(rule)
+        self.object.process(document)
+
+        assert document == expected
+
+    def test_deep_nested_field_with_child_neighbour_and_not_drop_full_gets_partially_dropped(self):
+        rule = {"filter": "drop.child", "drop": ["drop.child"], "drop_full": False}
+        expected = {"drop": {"neighbour": "bar"}}
+        document = {"drop": {"child": {"grand_child": "foo"}, "neighbour": "bar"}}
+        self._load_specific_rule(rule)
+        self.object.process(document)
+
+        assert document == expected
+
+
+class TestDropperFactory(TestDropper):
+    def test_create(self):
+        assert isinstance(DropperFactory.create("foo", self.CONFIG, logger), Dropper)
+
+    def test_check_configuration(self):
+        DropperFactory._check_configuration(self.CONFIG)
+        cfg = copy.deepcopy(self.CONFIG)
+        cfg.pop("type")
+        with pytest.raises(ProcessorFactoryError):
+            DropperFactory._check_configuration(cfg)
