7f031da5764335ba5aae485753bf601ea73ccfc2
ekneg54 <ekneg54@pm.me>
2022-05-19 11:14:25 +0000

    Refactor to new processor interface

--- logprep/processor/pre_detector/processor.py
+++ logprep/processor/pre_detector/processor.py
@@ -4,6 +4,7 @@ from logging import DEBUG, Logger
 from multiprocessing import current_process
 from typing import List
 from uuid import uuid4
+from logprep.abc import Processor
 
 from logprep.processor.base.processor import RuleBasedProcessor
 from logprep.processor.pre_detector.ip_alerter import IPAlerter
@@ -22,65 +23,21 @@ class PreDetectorConfigurationError(PreDetectorError):
     """Generic PreDetector configuration error."""
 
 
-class PreDetector(RuleBasedProcessor):
+class PreDetector(Processor):
     """Processor used to pre_detect log events."""
 
     detection_results: list
 
+    rule_class = PreDetectorRule
+
     def __init__(self, name: str, configuration: dict, logger: Logger):
-        tree_config = configuration.get("tree_config")
         pre_detector_topic = configuration.get("pre_detector_topic")
         alert_ip_list_path = configuration.get("alert_ip_list_path")
-        specific_rules_dirs = configuration.get("specific_rules")
-        generic_rules_dirs = configuration.get("generic_rules")
-        super().__init__(name, tree_config, logger)
-        self._logger = logger
-        self.ps = ProcessorStats()
-
-        self._name = name
+        super().__init__(name=name, configuration=configuration, logger=logger)
         self._pre_detector_topic = pre_detector_topic
         self._event = None
-
         self._ids = []
-
         self._ip_alerter = IPAlerter(alert_ip_list_path)
-        self.add_rules_from_directory(
-            specific_rules_dirs=specific_rules_dirs, generic_rules_dirs=generic_rules_dirs
-        )
-
-    # pylint: disable=arguments-differ
-    def add_rules_from_directory(
-        self, specific_rules_dirs: List[str], generic_rules_dirs: List[str]
-    ):
-        for specific_rules_dir in specific_rules_dirs:
-            rule_paths = self._list_json_files_in_directory(specific_rules_dir)
-            for rule_path in rule_paths:
-                rules = PreDetectorRule.create_rules_from_file(rule_path)
-                for rule in rules:
-                    self._specific_tree.add_rule(rule, self._logger)
-        for generic_rules_dir in generic_rules_dirs:
-            rule_paths = self._list_json_files_in_directory(generic_rules_dir)
-            for rule_path in rule_paths:
-                rules = PreDetectorRule.create_rules_from_file(rule_path)
-                for rule in rules:
-                    self._generic_tree.add_rule(rule, self._logger)
-        if self._logger.isEnabledFor(DEBUG):
-            self._logger.debug(
-                f"{self.describe()} loaded {self._specific_tree.rule_counter} "
-                f"specific rules ({current_process().name})"
-            )
-            self._logger.debug(
-                f"{self.describe()} loaded {self._generic_tree.rule_counter} generic rules "
-                f"({current_process().name})"
-            )
-        self.ps.setup_rules(
-            [None] * self._generic_tree.rule_counter + [None] * self._specific_tree.rule_counter
-        )
-
-    # pylint: enable=arguments-differ
-
-    def setup(self):
-        pass
 
     def process(self, event: dict) -> tuple:
         self._event = event
