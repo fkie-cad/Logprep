94435ece0f3416a27ad4f47a02a8bcdcbea28eef
ekneg54 <ekneg54@pm.me>
2022-05-19 11:01:52 +0000

    Refactor to new processor interface

--- logprep/processor/selective_extractor/processor.py
+++ logprep/processor/selective_extractor/processor.py
@@ -4,20 +4,20 @@ The events will be returned and written to a specified kafka topic.
 As the processor is applied to all events it does not need further filtering by rules.
 """
 
-from multiprocessing import current_process
-from logging import Logger, DEBUG
+from logging import Logger
 from typing import List
+from logprep.abc.processor import Processor
 
-from logprep.processor.base.processor import RuleBasedProcessor
 from logprep.processor.selective_extractor.rule import SelectiveExtractorRule
-from logprep.util.helper import add_field_to
-from logprep.util.processor_stats import ProcessorStats
+from logprep.util.helper import add_field_to, get_dotted_field_value
 
 
-class SelectiveExtractor(RuleBasedProcessor):
+class SelectiveExtractor(Processor):
     """Processor used to selectively extract fields from log events."""
 
     _filtered_events: List[tuple] = None
+
+    rule_class = SelectiveExtractorRule
 
     def __init__(
         self,
@@ -25,47 +25,8 @@ class SelectiveExtractor(RuleBasedProcessor):
         configuration: dict,
         logger: Logger,
     ):
-        tree_config = configuration.get("tree_config")
-        super().__init__(name, tree_config, logger)
-        self._logger = logger
-        self.ps = ProcessorStats()
-
-        self._name = name
+        super().__init__(name=name, configuration=configuration, logger=logger)
         self._filtered_events = []
-        generic_rules_dirs = configuration.get("generic_rules")
-        specific_rules_dirs = configuration.get("specific_rules")
-        self.add_rules_from_directory(
-            generic_rules_dirs=generic_rules_dirs, specific_rules_dirs=specific_rules_dirs
-        )
-
-    # pylint: disable=arguments-differ
-    def add_rules_from_directory(
-        self, specific_rules_dirs: List[str], generic_rules_dirs: List[str]
-    ):
-        for specific_rules_dir in specific_rules_dirs:
-            rule_paths = self._list_json_files_in_directory(specific_rules_dir)
-            for rule_path in rule_paths:
-                rules = SelectiveExtractorRule.create_rules_from_file(rule_path)
-                for rule in rules:
-                    self._specific_tree.add_rule(rule, self._logger)
-        for generic_rules_dir in generic_rules_dirs:
-            rule_paths = self._list_json_files_in_directory(generic_rules_dir)
-            for rule_path in rule_paths:
-                rules = SelectiveExtractorRule.create_rules_from_file(rule_path)
-                for rule in rules:
-                    self._generic_tree.add_rule(rule, self._logger)
-        if self._logger.isEnabledFor(DEBUG):
-            self._logger.debug(
-                f"{self.describe()} loaded {self._specific_tree.rule_counter} "
-                f"specific rules ({current_process().name})"
-            )
-            self._logger.debug(
-                f"{self.describe()} loaded {self._generic_tree.rule_counter} generic rules "
-                f"generic rules ({current_process().name})"
-            )
-        self.ps.setup_rules(
-            [None] * self._generic_tree.rule_counter + [None] * self._specific_tree.rule_counter
-        )
 
     def process(self, event: dict) -> tuple:
         super().process(event)
@@ -92,7 +53,7 @@ class SelectiveExtractor(RuleBasedProcessor):
         filtered_event = {}
 
         for field in rule.extracted_field_list:
-            field_value = self._get_dotted_field_value(event, field)
+            field_value = get_dotted_field_value(event, field)
             if field_value is not None:
                 add_field_to(filtered_event, field, field_value)
 
