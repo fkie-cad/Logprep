35818b88852c08fb221bdf62d8f93c2009f799cc
ekneg54 <ekneg54@pm.me>
2022-05-28 22:26:54 +0000

    Fix linting

--- logprep/processor/selective_extractor/processor.py
+++ logprep/processor/selective_extractor/processor.py
@@ -5,7 +5,7 @@ As the processor is applied to all events it does not need further filtering by 
 """
 
 from logging import Logger
-from typing import TYPE_CHECKING, List
+from typing import List
 
 from logprep.abc.processor import Processor
 
