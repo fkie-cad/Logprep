7b17083fe8dc0335f32b068283a822974c043cae
ekneg54 <ekneg54@pm.me>
2022-05-28 17:28:00 +0000

    Remove unused exceptions

--- logprep/processor/base/exceptions.py
+++ logprep/processor/base/exceptions.py
@@ -2,10 +2,8 @@
 
 from typing import Any
 
-from logprep.processor.processor_factory_error import (
-    InvalidConfigurationError,
-    ProcessorFactoryError,
-)
+from logprep.processor.processor_factory_error import ProcessorFactoryError
+
 
 
 class RuleError(BaseException):
@@ -22,27 +20,6 @@ class InvalidRuleDefinitionError(RuleError):
 
 class MismatchedRuleDefinitionError(RuleError):
     """Raise if defined rule does not conform to schema."""
-
-
-class NotARulesDirectoryError(RuleError):
-    """Raise if path is not a rules directory."""
-
-    def __init__(self, name: str, path: str):
-        super().__init__(name, f'Not a rule directory: "{path}"')
-
-
-class InvalidRuleFileError(InvalidConfigurationError):
-    """Raise if rule file at path is invalid."""
-
-    def __init__(self, name: str, path: str, message: str = ""):
-        if message:
-            super().__init__(name, f'Invalid rule file "{path}": {message}')
-        else:
-            super().__init__(name, f'Invalid rule file "{path}".')
-
-
-class InvalidRuleConfigurationError(InvalidConfigurationError):
-    """Raise if rule configuration is invalid."""
 
 
 class KeyDoesnotExistInSchemaError(MismatchedRuleDefinitionError):
