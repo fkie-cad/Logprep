32cec773c599ea3f7151a72573d8ec40712b073f
dtrai2 <d.trai282@gmail.com>
2022-07-04 10:07:35 +0200

    Add test for run_logprep config arg

--- tests/unit/test_run_logprep.py
+++ tests/unit/test_run_logprep.py
@@ -1,17 +1,27 @@
 # pylint: disable=missing-docstring
 # pylint: disable=no-self-use
+import os.path
 import sys
 from unittest import mock
 
 import pytest
+from yaml import safe_load
+
 from logprep import run_logprep
+from logprep._version import get_versions
+from logprep.run_logprep import DEFAULT_LOCATION_CONFIG
 
 
 class TestRunLogprep:
     @mock.patch("logprep.run_logprep._run_logprep")
     def test_main_calls_run_logprep_with_quickstart_config(self, mock_run_logprep):
         """ensures the quickstart config is valid"""
-        sys.argv = ["logprep", "--disable-logging", "quickstart/exampledata/config/pipeline.yml"]
+        sys.argv = [
+            "logprep",
+            "--disable-logging",
+            "--config",
+            "quickstart/exampledata/config/pipeline.yml",
+        ]
         run_logprep.main()
         mock_run_logprep.assert_called()
 
@@ -22,6 +32,7 @@ class TestRunLogprep:
             "logprep",
             "--disable-logging",
             "--validate-rules",
+            "--config",
             "quickstart/exampledata/config/pipeline.yml",
         ]
         with pytest.raises(SystemExit):
@@ -34,8 +45,61 @@ class TestRunLogprep:
             "logprep",
             "--disable-logging",
             "--validate-rules",
+            "--config",
             "quickstart/exampledata/config/pipeline.yml",
         ]
         with pytest.raises(SystemExit) as e_info:
             run_logprep.main()
         assert e_info.value.code == 0
+
+    def test_version_arg_prints_logprep(self, capsys):
+        sys.argv = ["logprep", "--version"]
+        with pytest.raises(SystemExit):
+            run_logprep.main()
+        captured = capsys.readouterr()
+        logprep_line, config_line = captured.out.strip().split("\n")
+        assert logprep_line == f"logprep version: \t\t {get_versions()['version']}"
+        assert (
+            config_line
+            == f"configuration version: \t no configuration found in '{DEFAULT_LOCATION_CONFIG}'"
+        )
+
+    def test_version_arg_prints_also_config_version_if_version_key_is_found(self, capsys):
+        config_path = "quickstart/exampledata/config/pipeline.yml"
+        sys.argv = ["logprep", "--version", "--config", config_path]
+        with pytest.raises(SystemExit):
+            run_logprep.main()
+        captured = capsys.readouterr()
+        lines = captured.out.strip()
+        with open(config_path, "r", encoding="utf-8") as file:
+            configuration = safe_load(file)
+        expected_lines = (
+            f"logprep version: \t\t {get_versions()['version']}\n"
+            f"configuration version: \t {configuration['version']}, {os.path.abspath(config_path)}"
+        )
+        assert lines == expected_lines
+
+    def test_no_config_error_is_printed_if_no_config_was_arg_was_given(self, capsys):
+        sys.argv = ["logprep"]
+        with pytest.raises(SystemExit):
+            run_logprep.main()
+        captured = capsys.readouterr()
+        error_lines = captured.err.strip()
+        expected_lines = (
+            f"The given config file does not exist: {DEFAULT_LOCATION_CONFIG}\nCreate "
+            "the configuration or change the path with the '--config' argument."
+        )
+        assert error_lines == expected_lines
+
+    def test_no_config_error_is_printed_if_given_config_file_does_not_exist(self, capsys):
+        non_existing_config_file = "/tmp/does/not/exist.yml"
+        sys.argv = ["logprep", "--config", non_existing_config_file]
+        with pytest.raises(SystemExit):
+            run_logprep.main()
+        captured = capsys.readouterr()
+        error_lines = captured.err.strip()
+        expected_lines = (
+            f"The given config file does not exist: {non_existing_config_file}\nCreate "
+            "the configuration or change the path with the '--config' argument."
+        )
+        assert error_lines == expected_lines
