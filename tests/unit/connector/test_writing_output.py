dce200c1ec86a92eef1090063133d814594da820
ekneg54 <ekneg54@pm.me>
2022-08-30 08:53:00 +0000

    Refactor and complete testing for writing_output

--- tests/unit/connector/test_writing_output.py
+++ tests/unit/connector/test_writing_output.py
@@ -1,60 +1,37 @@
-from os import remove
-from os.path import isfile
-from json import dumps
-
-import pytest
-
+# pylint: disable=missing-docstring
+# pylint: disable=attribute-defined-outside-init
+# pylint: disable=protected-access
+from unittest import mock
 from logprep.output.writing_output import WritingOutput
 
-OUTPUT_PATH = "tests/testdata/acceptance/test_kafka_data_processing_acceptance.out"
-OUTPUT_PATH_CUSTOM = "tests/testdata/test_kafka_data_processing_acceptance_custom.out"
 
-
-@pytest.fixture
-def output():
-    _remove_file_if_exists(OUTPUT_PATH)
-    yield WritingOutput(OUTPUT_PATH)
-    _remove_file_if_exists(OUTPUT_PATH)
-
-
-@pytest.fixture
-def output_custom():
-    _remove_file_if_exists(OUTPUT_PATH)
-    _remove_file_if_exists(OUTPUT_PATH_CUSTOM)
-    yield WritingOutput(OUTPUT_PATH, output_path_custom=OUTPUT_PATH_CUSTOM)
-    _remove_file_if_exists(OUTPUT_PATH_CUSTOM)
-    _remove_file_if_exists(OUTPUT_PATH)
-
-
-@pytest.fixture(scope="session")
-def document():
-    return {"the": "document"}
-
-
-def _remove_file_if_exists(test_output_path):
-    try:
-        remove(test_output_path)
-    except FileNotFoundError:
-        pass
-
-
+@mock.patch("builtins.open")
 class TestWritingOutput:
-    def test_describe_endpoint(self, output, document):
+    def setup_method(self):
+        self.original_write_json = WritingOutput._write_json
+        self.document = {"the": "document"}
+
+    def teardown_method(self):
+        WritingOutput._write_json = self.original_write_json
+
+    def test_describe_endpoint(self, _):
+        output = WritingOutput("doesnotmatter")
         assert output.describe_endpoint() == "writer"
 
-    def test_store_appends_document_to_variable(self, output, document):
-        output.store(document)
-
+    def test_store_appends_document_to_variable(self, _):
+        output = WritingOutput("doesnotmatter")
+        output.store(self.document)
         assert len(output.events) == 1
-        assert output.events[0] == document
+        assert output.events[0] == self.document
 
-    def test_store_custom_appends_document_to_variable(self, output_custom, document):
-        output_custom.store_custom(document, target="whatever")
+    def test_store_custom_appends_document_to_variable(self, _):
+        output = WritingOutput(output_path="doesnotmatter", output_path_custom="doesnotmatter")
+        output.store_custom(self.document, target="whatever")
+        assert len(output.events) == 1
+        assert output.events[0] == self.document
 
-        assert len(output_custom.events) == 1
-        assert output_custom.events[0] == document
-
-    def test_store_maintains_order_of_documents(self, output):
+    def test_store_maintains_order_of_documents(self, _):
+        output = WritingOutput("doesnotmatter")
         for i in range(0, 3):
             output.store({"order": i})
 
@@ -62,37 +39,45 @@ class TestWritingOutput:
         for order in range(0, 3):
             assert output.events[order]["order"] == order
 
-    def test_stores_failed_events_in_respective_list(self, output):
+    def test_stores_failed_events_in_respective_list(self, _):
+        output = WritingOutput(
+            output_path="doesnotmatter",
+            output_path_custom="doesnotmatter",
+            output_path_error="doesnotmatter",
+        )
         output.store_failed("message", {"doc": "received"}, {"doc": "processed"})
 
         assert len(output.failed_events) == 1
         assert output.failed_events[0] == ("message", {"doc": "received"}, {"doc": "processed"})
 
-    def test_write_document_to_file_on_store(self, output, document):
-        output.store(document)
-        output.shut_down()
-        assert isfile(OUTPUT_PATH)
+    def test_write_document_to_file_on_store(self, mock_open):
+        output = WritingOutput("/file/for/store")
+        output.store(self.document)
+        mock_open.assert_called_with("/file/for/store", "a+")
+        mock_open().write.assert_called_with('{"the": "document"}\n')
 
-        with open(OUTPUT_PATH, "r") as output_file:
-            assert output_file.readline().strip() == dumps(document)
-            assert output_file.readline().strip() == ""
+    def test_write_document_to_file_on_store_custom(self, mock_open):
+        output = WritingOutput(output_path="/file/for/store", output_path_custom="/file/for/custom")
+        mock_open.assert_called_with("/file/for/custom", "a+")
+        WritingOutput._write_json = mock.MagicMock()
+        output.store_custom(self.document, target="whatever")
+        output._write_json.assert_called_with(output._output_file_custom, self.document)
 
-    def test_write_document_to_file_on_store_custom(self, output_custom, document):
-        output_custom.store_custom(document, target="whatever")
-        output_custom.shut_down()
-        assert isfile(OUTPUT_PATH_CUSTOM)
+    def test_write_multiple_documents_to_file_on_store(self, mock_open):
+        output = WritingOutput("/file/to/store")
+        output.store(self.document)
+        output.store(self.document)
+        # assert mock_open().write.call_count == 2
+        assert mock_open().write.call_args_list == [
+            mock.call('{"the": "document"}\n'),
+            mock.call('{"the": "document"}\n'),
+        ]
 
-        with open(OUTPUT_PATH_CUSTOM, "r") as output_file2:
-            assert output_file2.readline().strip() == dumps(document)
-            assert output_file2.readline().strip() == ""
-
-    def test_write_multiple_documents_to_file_on_store(self, output, document):
-        output.store(document)
-        output.store(document)
-        output.shut_down()
-        assert isfile(OUTPUT_PATH)
-
-        with open(OUTPUT_PATH, "r") as output_file:
-            assert output_file.readline().strip() == dumps(document)
-            assert output_file.readline().strip() == dumps(document)
-            assert output_file.readline().strip() == ""
+    def test_store_failed_writes_errors(self, mock_open):
+        output = WritingOutput(output_path="file/to/store", output_path_error="file/to/error")
+        output.store_failed("my error message", self.document, self.document)
+        mock_open().write.assert_called_with(
+            '{"error_message": "my error message", '
+            '"document_received": {"the": "document"}, '
+            '"document_processed": {"the": "document"}}\n'
+        )
