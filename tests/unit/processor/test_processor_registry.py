49d9f5087dd2e05a00cb7f44786c27406eeebb6a
Paul Schiffer <paul.josef.schiffer@fkie.fraunhofer.de>
2022-08-31 15:05:54 +0200

    Improve variable naming

--- tests/unit/processor/test_processor_registry.py
+++ tests/unit/processor/test_processor_registry.py
@@ -3,11 +3,11 @@ from logprep.processor.processor_registry import ProcessorRegistry
 
 
 def test_get_processor_class_throws_deprecation_warning():
-    with warnings.catch_warnings(record=True) as w:
+    with warnings.catch_warnings(record=True) as warning_messages:
         ProcessorRegistry.get_processor_class("delete")
-        assert len(w) == 1
-        assert isinstance(w[0], warnings.WarningMessage)
+        assert len(warning_messages) == 1
+        assert isinstance(warning_messages[0], warnings.WarningMessage)
         assert (
-            str(w[0].message)
+            str(warning_messages[0].message)
             == "delete processor is deprecated and will be removed in a future release. Please use deleter instead."
         )
