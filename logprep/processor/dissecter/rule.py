171cf4eaac4ec6492bddc91196b47cf5b2a66b49
ekneg54 <ekneg54@pm.me>
2022-09-23 13:16:15 +0000

    Implement dissecter rule

--- logprep/processor/dissecter/rule.py
+++ logprep/processor/dissecter/rule.py
@@ -1,8 +1,46 @@
+"""Dissecter Rule Module"""
+from typing import List
+from attrs import define, validators, field, Factory
 from logprep.processor.base.rule import Rule
+from logprep.processor.base.exceptions import InvalidRuleDefinitionError
+
+
+DISSECT = r"(%\{[A-Za-z0-9+&].*\})"
+SEPERATOR = r"((?!%\{.*\}).+)"
 
 
 class DissecterRule(Rule):
     """dissecter rule"""
+
+    @define(kw_only=True)
+    class Config:
+        """Config for Dissecter"""
+
+        mapping: dict = field(
+            validator=[
+                validators.instance_of(dict),
+                validators.deep_mapping(
+                    key_validator=validators.instance_of(str),
+                    value_validator=validators.matches_re(rf"^({DISSECT}{SEPERATOR})+{DISSECT}$"),
+                ),
+            ],
+            default=Factory(dict),
+        )
+        convert_datatype: dict = field(
+            validator=[
+                validators.instance_of(dict),
+                validators.deep_mapping(
+                    key_validator=validators.instance_of(str),
+                    value_validator=validators.in_(["float", "int", "string"]),
+                ),
+            ],
+            default=Factory(dict),
+        )
+        tag_on_failure: list = field(
+            validator=validators.instance_of(list), default=["_dissectfailure"]
+        )
+
+    _config: "DissecterRule.Config"
 
     def __eq__(self, other: "Rule") -> bool:
         return False
@@ -10,4 +48,8 @@ class DissecterRule(Rule):
     @staticmethod
     def _create_from_dict(rule: dict) -> "DissecterRule":
         filter_expression = Rule._create_filter_expression(rule)
+        config = rule.get("dissecter")
+        if not isinstance(config, dict):
+            raise InvalidRuleDefinitionError("config is not a dict")
+        config = DissecterRule.Config(**config)
         return DissecterRule(filter_expression)
--- tests/unit/processor/dissecter/test_dissecter_rule.py
+++ tests/unit/processor/dissecter/test_dissecter_rule.py
@@ -1,10 +1,117 @@
 # pylint: disable=missing-docstring
 # pylint: disable=protected-access
+import pytest
 from logprep.processor.dissecter.rule import DissecterRule
+from logprep.processor.base.exceptions import InvalidRuleDefinitionError
 
 
-class DissecterRuleTest:
-    def test_create_filter_expression_returns(self):
-        rule = {"filter": "message", "dissecter": {"pattern": ""}}
-        filter_expression = DissecterRule._create_filter_expression(rule)
-        assert filter_expression
+class TestDissecterRule:
+
+    rule = {"filter": "message", "dissecter": {"mapping": {}}}
+
+    def test_create_from_dict_retuns_dissecter_rule(self):
+        dissecter_rule = DissecterRule._create_from_dict(self.rule)
+        assert isinstance(dissecter_rule, DissecterRule)
+
+    @pytest.mark.parametrize(
+        ["rule", "error", "message"],
+        [
+            (
+                {"filter": "message", "dissecter": "I'm not a dict"},
+                InvalidRuleDefinitionError,
+                "config is not a dict",
+            ),
+            ({"filter": "message", "dissecter": {}}, None, None),
+            (
+                {"filter": "message", "dissecter": {"mapping": {}}},
+                None,
+                None,
+            ),
+            (
+                {"filter": "message", "dissecter": {"convert_datatype": {}}},
+                None,
+                None,
+            ),
+            (
+                {"filter": "message", "dissecter": {"mapping": {"message": 2}}},
+                TypeError,
+                "expected string or bytes-like object",
+            ),
+            (
+                {"filter": "message", "dissecter": {"mapping": {1: "the message"}}},
+                TypeError,
+                "must be <class 'str'>",
+            ),
+            (
+                {"filter": "message", "dissecter": {"mapping": {1: ["invalid"]}}},
+                TypeError,
+                "must be <class 'str'>",
+            ),
+            (
+                {
+                    "filter": "message",
+                    "dissecter": {
+                        "mapping": {
+                            "field": "%{ts} %{+ts} %{+ts} %{src} %{} %{prog}[%{pid}]: %{msg}"
+                        }
+                    },
+                },
+                None,
+                None,
+            ),
+            (
+                {
+                    "filter": "message",
+                    "dissecter": {"mapping": {"field": "invalid pattern"}},
+                },
+                ValueError,
+                "must match regex",
+            ),
+            (
+                {
+                    "filter": "message",
+                    "dissecter": {"mapping": {"field": "%{ts}"}},
+                },
+                ValueError,
+                "must match regex",
+            ),
+            (
+                {
+                    "filter": "message",
+                    "dissecter": {"mapping": {"field": "%{ts}:"}},
+                },
+                ValueError,
+                "must match regex",
+            ),
+            (
+                {
+                    "filter": "message",
+                    "dissecter": {"mapping": {"field": "%{ts}:%{ts} "}},
+                },
+                ValueError,
+                "must match regex",
+            ),
+            (
+                {
+                    "filter": "message",
+                    "dissecter": {"mapping": {"field": "%{ts}:%{ts}:%{ts}"}},
+                },
+                None,
+                None,
+            ),
+            (
+                {
+                    "filter": "message",
+                    "dissecter": {"mapping": {"field": "%{%prog} %{pid}:%{msg}"}},
+                },
+                ValueError,
+                "must match regex",
+            ),
+        ],
+    )
+    def test_create_from_dict_validates_config(self, rule, error, message):
+        if error:
+            with pytest.raises(error, match=message):
+                DissecterRule._create_from_dict(rule)
+        else:
+            DissecterRule._create_from_dict(rule)
