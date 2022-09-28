515a1d0745d4c8933139ad6503e448c23f20de04
dtrai2 <d.trai282@gmail.com>
2022-09-28 11:05:46 +0200

    Use _load_specific_rule method

--- tests/unit/processor/concatenator/test_concatenator.py
+++ tests/unit/processor/concatenator/test_concatenator.py
@@ -3,7 +3,6 @@
 import pytest
 
 from logprep.processor.concatenator.processor import DuplicationError
-from logprep.processor.concatenator.rule import ConcatenatorRule
 from tests.unit.processor.base import BaseProcessorTestCase
 
 
@@ -24,57 +23,51 @@ class TestConcatenator(BaseProcessorTestCase):
         return self.CONFIG["specific_rules"]
 
     def test_process_creates_target_field_with_concatenated_source_fields(self):
-        rule = ConcatenatorRule._create_from_dict(
-            {
-                "filter": "field.a",
-                "concatenator": {
-                    "source_fields": ["field.a", "field.b", "other_field.c"],
-                    "target_field": "target_field",
-                    "seperator": "-",
-                    "overwrite_target": False,
-                    "delete_source_fields": False,
-                },
-            }
-        )
-        self.object._specific_tree.add_rule(rule)
+        rule = {
+            "filter": "field.a",
+            "concatenator": {
+                "source_fields": ["field.a", "field.b", "other_field.c"],
+                "target_field": "target_field",
+                "seperator": "-",
+                "overwrite_target": False,
+                "delete_source_fields": False,
+            },
+        }
+        self._load_specific_rule(rule)
         document = {"field": {"a": "first", "b": "second"}, "other_field": {"c": "third"}}
         self.object.process(document)
         assert "target_field" in document
         assert document.get("target_field") == "first-second-third"
 
     def test_process_ignores_source_fields_that_do_not_exist(self):
-        rule = ConcatenatorRule._create_from_dict(
-            {
-                "filter": "field.a",
-                "concatenator": {
-                    "source_fields": ["field.a", "field.b", "will.be.ignored"],
-                    "target_field": "target_field",
-                    "seperator": "-",
-                    "overwrite_target": False,
-                    "delete_source_fields": False,
-                },
-            }
-        )
-        self.object._specific_tree.add_rule(rule)
+        rule = {
+            "filter": "field.a",
+            "concatenator": {
+                "source_fields": ["field.a", "field.b", "will.be.ignored"],
+                "target_field": "target_field",
+                "seperator": "-",
+                "overwrite_target": False,
+                "delete_source_fields": False,
+            },
+        }
+        self._load_specific_rule(rule)
         document = {"field": {"a": "first", "b": "second"}}
         self.object.process(document)
         assert "target_field" in document
         assert document.get("target_field") == "first-second"
 
     def test_process_does_not_overwrite_target_field_and_raise_duplication_error(self):
-        rule = ConcatenatorRule._create_from_dict(
-            {
-                "filter": "field.a",
-                "concatenator": {
-                    "source_fields": ["field.a", "field.b"],
-                    "target_field": "target_field",
-                    "seperator": "-",
-                    "overwrite_target": False,
-                    "delete_source_fields": False,
-                },
-            }
-        )
-        self.object._specific_tree.add_rule(rule)
+        rule = {
+            "filter": "field.a",
+            "concatenator": {
+                "source_fields": ["field.a", "field.b"],
+                "target_field": "target_field",
+                "seperator": "-",
+                "overwrite_target": False,
+                "delete_source_fields": False,
+            },
+        }
+        self._load_specific_rule(rule)
         document = {"field": {"a": "first", "b": "second"}, "target_field": "has already content"}
         with pytest.raises(
             DuplicationError,
@@ -87,38 +80,34 @@ class TestConcatenator(BaseProcessorTestCase):
         assert document.get("target_field") == "has already content"
 
     def test_process_does_overwrite_target_field(self):
-        rule = ConcatenatorRule._create_from_dict(
-            {
-                "filter": "field.a",
-                "concatenator": {
-                    "source_fields": ["field.a", "field.b"],
-                    "target_field": "target_field",
-                    "seperator": "-",
-                    "overwrite_target": True,
-                    "delete_source_fields": False,
-                },
-            }
-        )
-        self.object._specific_tree.add_rule(rule)
+        rule = {
+            "filter": "field.a",
+            "concatenator": {
+                "source_fields": ["field.a", "field.b"],
+                "target_field": "target_field",
+                "seperator": "-",
+                "overwrite_target": True,
+                "delete_source_fields": False,
+            },
+        }
+        self._load_specific_rule(rule)
         document = {"field": {"a": "first", "b": "second"}, "target_field": "has already content"}
         self.object.process(document)
         assert "target_field" in document
         assert document.get("target_field") == "first-second"
 
     def test_process_deletes_source_fields(self):
-        rule = ConcatenatorRule._create_from_dict(
-            {
-                "filter": "field.a",
-                "concatenator": {
-                    "source_fields": ["field.a", "field.b"],
-                    "target_field": "target_field",
-                    "seperator": "-",
-                    "overwrite_target": False,
-                    "delete_source_fields": True,
-                },
-            }
-        )
-        self.object._specific_tree.add_rule(rule)
+        rule = {
+            "filter": "field.a",
+            "concatenator": {
+                "source_fields": ["field.a", "field.b"],
+                "target_field": "target_field",
+                "seperator": "-",
+                "overwrite_target": False,
+                "delete_source_fields": True,
+            },
+        }
+        self._load_specific_rule(rule)
         document = {"field": {"a": "first", "b": "second", "c": "not third"}, "another": "field"}
         self.object.process(document)
         assert document == {
@@ -128,55 +117,49 @@ class TestConcatenator(BaseProcessorTestCase):
         }
 
     def test_process_deletes_source_fields_and_all_remaining_empty_dicts(self):
-        rule = ConcatenatorRule._create_from_dict(
-            {
-                "filter": "field.a",
-                "concatenator": {
-                    "source_fields": ["field.a", "field.b"],
-                    "target_field": "target_field",
-                    "seperator": "-",
-                    "overwrite_target": False,
-                    "delete_source_fields": True,
-                },
-            }
-        )
-        self.object._specific_tree.add_rule(rule)
+        rule = {
+            "filter": "field.a",
+            "concatenator": {
+                "source_fields": ["field.a", "field.b"],
+                "target_field": "target_field",
+                "seperator": "-",
+                "overwrite_target": False,
+                "delete_source_fields": True,
+            },
+        }
+        self._load_specific_rule(rule)
         document = {"field": {"a": "first", "b": "second"}, "another": "field"}
         self.object.process(document)
         assert document == {"another": "field", "target_field": "first-second"}
 
     def test_process_deletes_source_fields_in_small_dict_such_that_only_target_field_remains(self):
-        rule = ConcatenatorRule._create_from_dict(
-            {
-                "filter": "field.a",
-                "concatenator": {
-                    "source_fields": ["field.a", "field.b"],
-                    "target_field": "target_field",
-                    "seperator": "-",
-                    "overwrite_target": False,
-                    "delete_source_fields": True,
-                },
-            }
-        )
-        self.object._specific_tree.add_rule(rule)
+        rule = {
+            "filter": "field.a",
+            "concatenator": {
+                "source_fields": ["field.a", "field.b"],
+                "target_field": "target_field",
+                "seperator": "-",
+                "overwrite_target": False,
+                "delete_source_fields": True,
+            },
+        }
+        self._load_specific_rule(rule)
         document = {"field": {"a": "first", "b": "second"}}
         self.object.process(document)
         assert document == {"target_field": "first-second"}
 
     def test_process_overwrites_target_and_deletes_source_fields(self):
-        rule = ConcatenatorRule._create_from_dict(
-            {
-                "filter": "field.a",
-                "concatenator": {
-                    "source_fields": ["field.a", "field.b"],
-                    "target_field": "target_field",
-                    "seperator": "-",
-                    "overwrite_target": True,
-                    "delete_source_fields": True,
-                },
-            }
-        )
-        self.object._specific_tree.add_rule(rule)
+        rule = {
+            "filter": "field.a",
+            "concatenator": {
+                "source_fields": ["field.a", "field.b"],
+                "target_field": "target_field",
+                "seperator": "-",
+                "overwrite_target": True,
+                "delete_source_fields": True,
+            },
+        }
+        self._load_specific_rule(rule)
         document = {
             "field": {"a": "first", "b": "second", "c": "another one"},
             "target_field": "has already content",
