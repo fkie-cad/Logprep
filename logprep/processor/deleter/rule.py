c17c9ac2bade992c9f35a06d4ba88ee2a9eddb47
ekneg54 <ekneg54@pm.me>
2022-10-23 15:23:54 +0000

    Refactor deleter

--- logprep/processor/deleter/rule.py
+++ logprep/processor/deleter/rule.py
@@ -1,51 +1,25 @@
 """This module is used to delete full events matching a given filter."""
-
-from logprep.filter.expression.filter_expression import FilterExpression
-
-from logprep.processor.base.rule import Rule, InvalidRuleDefinitionError
-
-
-class DeleteRuleError(InvalidRuleDefinitionError):
-    """Base class for Delete rule related exceptions."""
-
-    def __init__(self, message: str):
-        super().__init__(f"Delete rule ({message}): ")
-
-
-class InvalidDeleterDefinition(DeleteRuleError):
-    """Raise if Delete definition invalid."""
-
-    def __init__(self, definition):
-        message = f"The following Delete definition is invalid: {definition}"
-        super().__init__(message)
+from attrs import define, field, validators
+from logprep.processor.base.rule import Rule
 
 
 class DeleterRule(Rule):
     """Check if documents match a filter."""
 
-    def __init__(self, filter_rule: FilterExpression, delete: bool):
-        super().__init__(filter_rule)
-        self._delete_or_not = delete
+    @define(kw_only=True)
+    class Config:
+        """Config for DeleterRule"""
 
-    def __eq__(self, other: "DeleterRule") -> bool:
-        return all([other.filter == self._filter, self._delete_or_not == other.delete_or_not])
+        delete: bool = field(validator=validators.instance_of(bool))
+
+    @classmethod
+    def _create_from_dict(cls, rule: dict) -> "DeleterRule":
+        filter_expression = Rule._create_filter_expression(rule)
+        config = cls.Config(delete=rule.get("delete"))
+        return cls(filter_expression, config)
 
     # pylint: disable=C0111
     @property
     def delete_or_not(self) -> bool:
-        return self._delete_or_not
-
-    # pylint: enable=C0111
-
-    @staticmethod
-    def _create_from_dict(rule: dict) -> "DeleterRule":
-        DeleterRule._check_rule_validity(rule, "delete")
-        DeleterRule._check_if_delete_valid(rule)
-
-        filter_expression = Rule._create_filter_expression(rule)
-        return DeleterRule(filter_expression, rule["delete"])
-
-    @staticmethod
-    def _check_if_delete_valid(rule: dict):
-        if not isinstance(rule["delete"], bool):
-            raise InvalidDeleterDefinition(f'Delete value "{rule["delete"]}" is not a boolean!')
+        """Returns delete_or_not"""
+        return self._config.delete
