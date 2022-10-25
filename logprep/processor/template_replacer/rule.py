0a52d227ab63277411c0dcc2b390082409e8f277
ekneg54 <ekneg54@pm.me>
2022-10-25 19:31:09 +0000

    Refactor TemplateReplacer Rule

--- logprep/processor/template_replacer/rule.py
+++ logprep/processor/template_replacer/rule.py
@@ -1,32 +1,7 @@
 """This module is used to replace a text field using a template."""
 
-from logprep.processor.base.rule import Rule, InvalidRuleDefinitionError
-
-
-class TemplateReplacerRuleError(InvalidRuleDefinitionError):
-    """Base class for TemplateReplacer rule related exceptions."""
-
-    def __init__(self, message: str):
-        super().__init__(f"TemplateReplacer rule ({message}): ")
-
-
-class InvalidTemplateReplacerDefinition(TemplateReplacerRuleError):
-    """Raise if TemplateReplacer definition invalid."""
-
-    def __init__(self, definition):
-        message = f"The following TemplateReplacer definition is invalid: {definition}"
-        super().__init__(message)
+from logprep.processor.base.rule import Rule
 
 
 class TemplateReplacerRule(Rule):
     """Check if documents match a filter."""
-
-    def __eq__(self, other: "TemplateReplacerRule") -> bool:
-        return other.filter == self._filter
-
-    @staticmethod
-    def _create_from_dict(rule: dict) -> "TemplateReplacerRule":
-        TemplateReplacerRule._check_rule_validity(rule, "template_replacer")
-
-        filter_expression = Rule._create_filter_expression(rule)
-        return TemplateReplacerRule(filter_expression)
