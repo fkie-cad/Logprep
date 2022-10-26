3c90016f96bade8fef31ebc7e171e6a72a07fa11
ekneg54 <ekneg54@pm.me>
2022-10-26 03:04:24 +0000

    Use normalize_rule_dict

--- logprep/processor/deleter/rule.py
+++ logprep/processor/deleter/rule.py
@@ -1,6 +1,7 @@
 """This module is used to delete full events matching a given filter."""
 from attrs import define, field, validators
 from logprep.processor.base.rule import Rule
+from logprep.util.helper import pop_dotted_field_value, add_and_overwrite
 
 
 class DeleterRule(Rule):
@@ -13,10 +14,11 @@ class DeleterRule(Rule):
         delete: bool = field(validator=validators.instance_of(bool))
 
     @classmethod
-    def _create_from_dict(cls, rule: dict) -> "DeleterRule":
-        filter_expression = Rule._create_filter_expression(rule)
-        config = cls.Config(delete=rule.get("delete"))
-        return cls(filter_expression, config)
+    def normalize_rule_dict(cls, rule: dict) -> None:
+        if rule.get("deleter") is None:
+            deleter_config = pop_dotted_field_value(rule, "delete")
+        if deleter_config is not None:
+            add_and_overwrite(rule, "deleter.delete", deleter_config)
 
     @property
     def delete_event(self) -> bool:
