2f0326d1303d83d5c2c8f7bc88e3cc508d99b0a7
ekneg54 <ekneg54@pm.me>
2022-10-31 08:33:02 +0000

    Fix keychecker tests

--- logprep/processor/key_checker/rule.py
+++ logprep/processor/key_checker/rule.py
@@ -44,8 +44,6 @@ from functools import partial
 
 from attrs import define, field, validators
 
-from logprep.filter.expression.filter_expression import FilterExpression
-from logprep.processor.base.exceptions import InvalidRuleDefinitionError
 from logprep.processor.base.rule import Rule
 
 
@@ -56,7 +54,7 @@ class KeyCheckerRule(Rule):
     """key_checker rule"""
 
     @define(kw_only=True)
-    class Config:
+    class Config(Rule.Config):
         """key_checker rule config"""
 
         key_list: set = field(
@@ -72,13 +70,6 @@ class KeyCheckerRule(Rule):
 
         output_field: str = field(validator=validators.instance_of(str))
 
-    def __eq__(self, other: "KeyCheckerRule") -> bool:
-        return all([other.filter == self._filter, other._config == self._config])
-
-    def __init__(self, filter_rule: FilterExpression, config: "KeyCheckerRule.Config"):
-        super().__init__(filter_rule)
-        self._config = config
-
     @property
     def key_list(self) -> list:  # pylint: disable=missing-docstring
         return self._config.key_list
@@ -86,12 +77,3 @@ class KeyCheckerRule(Rule):
     @property
     def output_field(self) -> str:  # pylint: disable=missing-docstring
         return self._config.output_field
-
-    @staticmethod
-    def _create_from_dict(rule: dict) -> "KeyCheckerRule":
-        filter_expression = Rule._create_filter_expression(rule)
-        config = rule.get("key_checker")
-        if not isinstance(config, dict):
-            raise InvalidRuleDefinitionError("config is not a dict")
-        config = KeyCheckerRule.Config(**config)
-        return KeyCheckerRule(filter_expression, config)
