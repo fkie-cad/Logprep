41109fa4615ee067cb1ed2c58bae07a17e6f0223
herrfeder <d.lassig@t-online.de>
2023-01-16 15:26:41 +0100

    file input connector can now track added lines and thread will exit gracefully

--- logprep/connector/file/input.py
+++ logprep/connector/file/input.py
@@ -21,40 +21,51 @@ import time
 import threading 
 import time
 import queue
+from multiprocessing import Process, Event, Queue
 
-class RepeatedTimer(object):
-    def __init__(self, interval, function, *args, **kwargs):
+
+class RepeatedTimerThread(object):
+    def __init__(self, interval, function, event,*args, **kwargs):
         self._timer = None
         self.interval = interval
         self.function = function
         self.args = args
         self.kwargs = kwargs
         self.is_running = False
+        self.stopped = event
         self.next_call = time.time()
         self.start()
 
+
     def _run(self):
-        self.is_running = False
-        self.start()
-        self.function(*self.args, **self.kwargs)
+        if not self.stopped.is_set():
+            self.is_running = False
+            self.start()
+            self.function(*self.args, **self.kwargs)
+        else:
+            self.stop()
 
     def start(self):
-        if not self.is_running:
+        if (not self.is_running) and (not self.stopped.is_set()):
             self.next_call += self.interval
             self._timer = threading.Timer(self.next_call - time.time(), self._run)
             self._timer.start()
             self.is_running = True
+        else:
+            self.stop()
 
     def stop(self):
-        self._timer.cancel()
         self.is_running = False
+        self._timer.cancel()
+
 
 
 class FileInput(Input):
     """FileInput Connector"""
 
     _messages: queue.Queue = queue.Queue()
-    
+    _fileinfo_dict: dict = {}
+
     @define(kw_only=True)
     class Config(Input.Config):
         """FileInput connector specific configuration"""
@@ -68,23 +79,22 @@ class FileInput(Input):
         return self._iterator
    
 
-    def _follow_file(self, sleep_sec=1, exit_end=False): 
+    def _follow_file(self, file_name): 
         """ Yield line from given input file and wait for arriving new lines.
-        `sleep_sec` allows to wait for rereading the file buffer to decrease compute load
-        `exit_end` allows deactivate tail behaviour and will exit on the last input line if activated
 
         TODO: Turn this from dirty while True to Timer triggered parallel thread or process"""
-        with open(self._config.documents_path) as file:
+        with open(file_name) as file:
             while True:
+                if self._fileinfo_dict.get(file_name):
+                    file.seek(self._fileinfo_dict[file_name])
+
                 line = file.readline()
+                self._fileinfo_dict[file_name] = file.tell()
                 if line is not '':
                     if line.endswith("\n"):
                         self._messages.put(self._line_to_dict(line))
-                        line = ''
-                elif exit_end:
+                else:
                     break
-                elif sleep_sec:
-                    time.sleep(sleep_sec)
 
     
     def _line_to_dict(self, input_line: str) -> dict:
@@ -107,4 +117,9 @@ class FileInput(Input):
     
     def setup(self):
         super().setup()
-        rt = RepeatedTimer(1,self._follow_file)
+        self.stop_flag = Event()
+        self.rt = RepeatedTimerThread(1,self._follow_file, self.stop_flag, file_name=self._config.documents_path)
+
+
+    def shut_down(self):
+        self.stop_flag.set()
