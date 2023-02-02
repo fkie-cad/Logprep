39d33ac59ed3a6809fdca21ec9561547b622c512
dtrai2 <95028228+dtrai2@users.noreply.github.com>
2023-02-02 14:30:50 +0100

    add deprecation filter to always (#309)

--- logprep/run_logprep.py
+++ logprep/run_logprep.py
@@ -4,6 +4,7 @@
 import inspect
 import logging
 import sys
+import warnings
 from argparse import ArgumentParser
 from logging import ERROR, Logger, getLogger
 from os.path import basename
@@ -23,6 +24,7 @@ from logprep.util.rule_dry_runner import DryRunner
 from logprep.util.schema_and_rule_checker import SchemaAndRuleChecker
 from logprep.util.time_measurement import TimeMeasurement
 
+warnings.simplefilter("always", DeprecationWarning)
 logging.captureWarnings(True)
 
 DEFAULT_LOCATION_CONFIG = "/etc/logprep/pipeline.yml"
