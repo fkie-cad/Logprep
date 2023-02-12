7ccd47d07e49b93e2dbdb59c4a798b8fd4caa8c2
ekneg54 <ekneg54@pm.me>
2023-02-12 20:42:44 +0000

    add list and not list support

--- logprep/processor/ip_informer/processor.py
+++ logprep/processor/ip_informer/processor.py
@@ -17,11 +17,14 @@ Example
         generic_rules:
             - tests/testdata/rules/generic/
 """
+from functools import partial
 import ipaddress
+from itertools import chain
+from typing import Iterable
 
 from logprep.processor.field_manager.processor import FieldManager
 from logprep.processor.ip_informer.rule import IpInformerRule
-from logprep.util.helper import get_source_fields_dict
+from logprep.util.helper import get_dotted_field_value
 
 
 class IpInformer(FieldManager):
@@ -30,9 +33,15 @@ class IpInformer(FieldManager):
     rule_class = IpInformerRule
 
     def _apply_rules(self, event: dict, rule: IpInformerRule) -> None:
-        source_field_dict = get_source_fields_dict(event, rule)
-        result = {ip: self._ip_properties(ip) for _, ip in source_field_dict.items()}
+        ip_address_list = self._get_ip_addresses(event, rule)
+        result = {ip: self._ip_properties(ip) for ip in ip_address_list}
         self._write_target_field(event, rule, result)
+
+    def _get_ip_addresses(self, event: dict, rule: IpInformerRule) -> Iterable:
+        source_field_values = list(map(partial(get_dotted_field_value, event), rule.source_fields))
+        list_elements = filter(lambda x: isinstance(x, list), source_field_values)
+        str_elements = filter(lambda x: isinstance(x, str), source_field_values)
+        return chain(*list_elements, str_elements)
 
     def _ip_properties(self, ip_address: str) -> dict[str, any]:
         ip_address = ipaddress.ip_address(ip_address)
