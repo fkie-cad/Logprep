602975cdf2eff4b8061f68f00643c57b809d1573
Jörg Zimmermann <101292599+ekneg54@users.noreply.github.com>
2023-03-20 15:55:06 +0100

    Update logprep/processor/key_checker/processor.py
    Co-authored-by: dtrai2 <95028228+dtrai2@users.noreply.github.com>

--- logprep/processor/key_checker/processor.py
+++ logprep/processor/key_checker/processor.py
@@ -22,10 +22,9 @@ Example
 from typing import Iterable
 
 from logprep.abc.processor import Processor
-from logprep.processor.base.exceptions import FieldExsistsWarning
 from logprep.processor.base.rule import Rule
 from logprep.processor.key_checker.rule import KeyCheckerRule
-from logprep.util.helper import add_field_to, get_dotted_field_value
+from logprep.util.helper import get_dotted_field_value
 
 
 class KeyChecker(Processor):
