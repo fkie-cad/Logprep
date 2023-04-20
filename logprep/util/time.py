ad151036a90ae6c48d69be504ad9b51fe0cab3c2
ekneg54 <ekneg54@pm.me>
2023-04-20 23:35:46 +0000

    add parse_timestamp

--- logprep/util/time.py
+++ logprep/util/time.py
@@ -1,3 +1,4 @@
+"""logprep time helpers module"""
 from datetime import datetime
 
 import ciso8601
@@ -5,7 +6,19 @@ import pendulum
 
 
 def parse_timestamp(source: str) -> datetime:
+    """parses input string to datetime object
+
+    Parameters
+    ----------
+    source : str
+        input string
+
+    Returns
+    -------
+    datetime
+        datetime object
+    """
     try:
-        return ciso8601.parse_datetime(source)
+        return ciso8601.parse_datetime(source)  # pylint: disable=c-extension-no-member
     except ValueError:
         return pendulum.parse(source, strict=False)
--- tests/unit/util/test_time.py
+++ tests/unit/util/test_time.py
@@ -1,8 +1,10 @@
+# pylint: disable=missing-docstring
 from datetime import datetime
 
+import pendulum
 import pytest
 
-from logprep.util.timestamp import parse_timestamp
+from logprep.util.time import parse_timestamp
 
 
 class TestParseTimestamp:
@@ -13,6 +15,46 @@ class TestParseTimestamp:
             (
                 "1999 12 12 - 12:12:22",
                 {"year": 1999, "month": 12, "day": 12, "hour": 12, "minute": 12, "second": 22},
+            ),
+            (
+                "Monday, June 15, 2009 1:45 PM",
+                {"year": 2009, "month": 6, "day": 15, "hour": 13, "minute": 45},
+            ),
+            (
+                "6/15/2009 1:45 PM",
+                {"year": 2009, "month": 6, "day": 15, "hour": 13, "minute": 45},
+            ),
+            (
+                "15/06/2009 13:45",
+                {"year": 2009, "month": 6, "day": 15, "hour": 13, "minute": 45},
+            ),
+            (
+                "2009/6/15 13:45",
+                {"year": 2009, "month": 6, "day": 15, "hour": 13, "minute": 45},
+            ),
+            (
+                "6/15/2009 1:45:30 PM",
+                {"year": 2009, "month": 6, "day": 15, "hour": 13, "minute": 45, "second": 30},
+            ),
+            (
+                "June 15",
+                {"year": pendulum.now().year, "month": 6, "day": 15},
+            ),
+            (
+                "2009-06-15T13:45:30.0000000-07:00",
+                {"year": 2009, "month": 6, "day": 15, "hour": 13, "minute": 45, "second": 30},
+            ),
+            (
+                "Mon, 15 Jun 2009 20:45:30 GMT",
+                {"year": 2009, "month": 6, "day": 15, "hour": 20, "minute": 45, "second": 30},
+            ),
+            (
+                "2009-06-15 13:45:30Z",
+                {"year": 2009, "month": 6, "day": 15, "hour": 13, "minute": 45, "second": 30},
+            ),
+            (
+                "2012-W05-5",
+                {"year": 2012, "month": 2, "day": 3},
             ),
         ],
     )
