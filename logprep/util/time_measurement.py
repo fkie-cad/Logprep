6f1b7e9d597317896d7b91282486ee9c194e4802
dtrai2 <d.trai282@gmail.com>
2023-10-18 13:13:12 +0200

    revise timemeasurement

--- logprep/util/time_measurement.py
+++ logprep/util/time_measurement.py
@@ -1,7 +1,5 @@
 """This module is used to measure the execution time of functions and add the results to events."""
 
-from time import time
-
 
 class TimeMeasurement:
     """Measures the execution time of functions and adds the results to events via a decorator."""
@@ -12,14 +10,11 @@ class TimeMeasurement:
 
         def inner_decorator(func):
             def inner(*args, **kwargs):  # nosemgrep
-                begin = time()
-                result = func(*args, **kwargs)
-                end = time()
-                processing_time = end - begin
                 caller = args[0]
                 if func.__name__ in ("_process_rule_tree", "_process_rule"):
                     caller = args[-1]
-                caller.metrics.processing_time_per_event += processing_time
+                with caller.metrics.processing_time_per_event.time():
+                    result = func(*args, **kwargs)
                 return result
 
             return inner
