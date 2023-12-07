c53d358b1b9185afac1fb248aea50f9b171fa641
dtrai2 <d.trai282@gmail.com>
2023-12-07 16:04:01 +0100

    prevent duplicate pseudonyms

--- logprep/processor/pseudonymizer/processor.py
+++ logprep/processor/pseudonymizer/processor.py
@@ -117,7 +117,7 @@ class Pseudonymizer(Processor):
         max_cached_pseudonymized_urls: int = field(
             validator=[validators.instance_of(int), validators.gt(0)], default=10000
         )
-        """The maximum number of cached pseudonymized urls. Default is 10000. 
+        """The maximum number of cached pseudonymized urls. Default is 10000.
         Behaves similarly to the max_cached_pseudonyms. Has to be greater than 0."""
         tld_lists: Optional[list] = field(default=None, validator=[list_of_urls_validator])
         """Optional list of path to files with top-level domain lists
@@ -220,7 +220,10 @@ class Pseudonymizer(Processor):
     def process(self, event: dict):
         self.pseudonyms = []
         super().process(event)
-        return (self.pseudonyms, self._config.outputs) if self.pseudonyms else None
+        unique_pseudonyms = list(
+            {pseudonyms["pseudonym"]: pseudonyms for pseudonyms in self.pseudonyms}.values()
+        )
+        return (unique_pseudonyms, self._config.outputs) if unique_pseudonyms else None
 
     def _apply_rules(self, event: dict, rule: PseudonymizerRule):
         for dotted_field, regex in rule.pseudonyms.items():
