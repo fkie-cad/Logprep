4811b7a43f22254878b29c7bb5bfdd9fe9a3aef4
Jörg Zimmermann <101292599+ekneg54@users.noreply.github.com>
2024-02-02 16:33:13 +0100

    Update logprep/util/configuration.py
    Co-authored-by: dtrai2 <95028228+dtrai2@users.noreply.github.com>

--- logprep/util/configuration.py
+++ logprep/util/configuration.py
@@ -27,7 +27,7 @@ They contain settings for each separate processor and connector.
 Details for configuring connectors are described in
 :ref:`output` and :ref:`input` and for processors in :ref:`processors`.
 General information about the configuration of the pipeline can be found
-in :ref:`pipeline_config` .
+in :ref:`pipeline_config`.
 
 It is possible to use environment variables in all configuration
 and rules files in all places.
