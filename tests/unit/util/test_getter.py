11b1247d9fe665ad22c9a34ecffe0e159b84ff82
djkhl <49399649+djkhl@users.noreply.github.com>
2024-03-28 11:43:57 +0100

    Update tests/unit/util/test_getter.py
    Co-authored-by: Jörg Zimmermann <101292599+ekneg54@users.noreply.github.com>

--- tests/unit/util/test_getter.py
+++ tests/unit/util/test_getter.py
@@ -577,34 +577,6 @@ class TestHttpGetter:
                 in http_getter._credentials_registry.get(f"https://{domain}")._session.cert
             )
 
-    @responses.activate
-    def test_get_raw_uses_mtls_but_cert_is_invalid(self, tmp_path):
-        domain = str(uuid.uuid4())
-        with responses.RequestsMock(assert_all_requests_are_fired=False):
-            req_kwargs = {
-                "cert": ("path/to/cert", "path/to/key"),
-                "verify": True,
-            }
-            responses.add(
-                responses.GET,
-                url=f"https://{domain}/bar",
-                match=[matchers.request_kwargs_matcher(req_kwargs)],
-                status=401,
-            )
-        credentials_file_content = {
-            f"https://{domain}": {
-                "client_key": "path/to/key",
-                "cert": "path/to/cert",
-            }
-        }
-        credentials_file: Path = tmp_path / "credentials.json"
-        credentials_file.write_text(json.dumps(credentials_file_content))
-        mock_env = {"LOGPREP_CREDENTIALS_FILE": str(credentials_file)}
-        with pytest.raises(requests.exceptions.HTTPError) as error:
-            with mock.patch.dict("os.environ", mock_env):
-                http_getter: HttpGetter = GetterFactory.from_string(f"https://{domain}/bar")
-                http_getter.get_raw()
-            assert error.value.response.status_code == 401
 
     @responses.activate
     def test_get_raw_uses_mtls_with_session_cert_and_ca_cert(self, tmp_path):
