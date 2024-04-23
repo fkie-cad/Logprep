d1803b074f0ec5b5938a06fd4764af1e181d9d8e
ekneg54 <ekneg54@pm.me>
2024-04-23 10:40:55 +0200

    reduce nav depth

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -118,7 +118,7 @@ html_theme = "sphinx_rtd_theme"
 # documentation.
 #
 html_theme_options = {
-    "navigation_depth": 5,
+    "navigation_depth": 4,
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
