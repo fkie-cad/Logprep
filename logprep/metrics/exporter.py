f79c80ef5bf3b80f2fa09ec8f4726607402ee62d
ekneg54 <ekneg54@pm.me>
2024-04-25 14:11:18 +0200

    extract ThreadingHTTPServer

--- logprep/metrics/exporter.py
+++ logprep/metrics/exporter.py
@@ -9,7 +9,7 @@ from logging import getLogger
 import uvicorn
 from prometheus_client import REGISTRY, make_asgi_app, multiprocess
 
-from logprep.util import defaults
+from logprep.util import defaults, http
 from logprep.util.configuration import MetricsConfig
 
 
@@ -18,11 +18,18 @@ class PrometheusExporter:
 
     def __init__(self, configuration: MetricsConfig):
         self.is_running = False
-        self._logger = getLogger("Prometheus Exporter")
+        logger_name = "Prometheus Exporter"
+        self._logger = getLogger(logger_name)
         self._logger.debug("Initializing Prometheus Exporter")
         self.configuration = configuration
         self._port = configuration.port
         self._app = make_asgi_app(REGISTRY)
+        self._server = http.ThreadingHTTPServer(
+            configuration.uvicorn_config | {"port": self._port},
+            self._app,
+            daemon=True,
+            logger_name=logger_name,
+        )
 
     def _prepare_multiprocessing(self):
         """
@@ -55,45 +62,9 @@ class PrometheusExporter:
         """
         multiprocess.mark_process_dead(pid)
 
-    def _init_log_config(self) -> dict:
-        """Use for Uvicorn same log formatter like for Logprep"""
-        log_config = uvicorn.config.LOGGING_CONFIG
-        log_config["formatters"]["default"]["fmt"] = defaults.DEFAULT_LOG_FORMAT
-        log_config["formatters"]["access"]["fmt"] = defaults.DEFAULT_LOG_FORMAT
-        log_config["handlers"]["default"]["stream"] = "ext://sys.stdout"
-        return log_config
-
-    def _override_runtime_logging(self):
-        """Uvicorn doesn't provide API to change name and handler beforehand
-        needs to be done during runtime"""
-        http_server_name = self._logger.name
-        for logger_name in ["uvicorn", "uvicorn.access"]:
-            logging.getLogger(logger_name).removeHandler(logging.getLogger(logger_name).handlers[0])
-            logging.getLogger(logger_name).addHandler(
-                logging.getLogger("Logprep").parent.handlers[0]
-            )
-        logging.getLogger("uvicorn.access").name = http_server_name
-        logging.getLogger("uvicorn.error").name = http_server_name
-
     def run(self):
         """Starts the default prometheus http endpoint"""
         self._prepare_multiprocessing()
-
-        self.uvicorn_config = self.configuration.uvicorn_config
-        log_config = self._init_log_config()
-        self.compiled_config = uvicorn.Config(
-            **self.uvicorn_config,
-            port=self._port,
-            app=self._app,
-            log_level="info",
-            log_config=log_config,
-        )
-        self.server = uvicorn.Server(self.compiled_config)
-        self._override_runtime_logging()
-        self.thread = threading.Thread(daemon=True, target=self.server.run)
-        self.thread.start()
-        while not self.server.started:
-            continue
-
+        self._server.start()
         self._logger.info(f"Prometheus Exporter started on port {self._port}")
         self.is_running = True
---
+++ logprep/util/http.py
@@ -0,0 +1,92 @@
+import logging
+import threading
+
+import uvicorn
+
+from logprep.util import defaults
+
+
+class ThreadingHTTPServer:  # pylint: disable=too-many-instance-attributes
+    """Singleton Wrapper Class around Uvicorn Thread that controls
+    lifecycle of Uvicorn HTTP Server. During Runtime this singleton object
+    is stateful and therefore we need to check for some attributes during
+    __init__ when multiple consecutive reconfigurations are happening.
+
+    Parameters
+    ----------
+    connector_config: Input.Config
+        Holds full connector config for config change checks
+    endpoints_config: dict
+        Endpoint paths as key and initiated endpoint objects as
+        value
+    log_level: str
+        Log level to be set for uvicorn server
+    """
+
+    _instance = None
+    _lock = threading.Lock()
+
+    @property
+    def _log_config(self) -> dict:
+        """Use for Uvicorn same log formatter like for Logprep"""
+        log_config = uvicorn.config.LOGGING_CONFIG
+        log_config["formatters"]["default"]["fmt"] = defaults.DEFAULT_LOG_FORMAT
+        log_config["formatters"]["access"]["fmt"] = defaults.DEFAULT_LOG_FORMAT
+        log_config["handlers"]["default"]["stream"] = "ext://sys.stdout"
+        return log_config
+
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            with cls._lock:
+                if not cls._instance:
+                    cls._instance = super(ThreadingHTTPServer, cls).__new__(cls)
+        return cls._instance
+
+    def __init__(
+        self, uvicorn_config: dict, app, daemon=True, logger_name="Logprep HTTPServer"
+    ) -> None:
+        """Creates object attributes with necessary configuration.
+        As this class creates a singleton object, the existing server
+        will be stopped and restarted on consecutively creations"""
+
+        if hasattr(self, "thread"):
+            if self.thread.is_alive():  # pylint: disable=access-member-before-definition
+                self._stop()
+        self._logger_name = logger_name
+        uvicorn_config = uvicorn.Config(**uvicorn_config, app=app, log_config=self._log_config)
+        self.server = uvicorn.Server(uvicorn_config)
+        self._override_runtime_logging()
+        self.thread = threading.Thread(daemon=daemon, target=self.server.run)
+
+    def start(self):
+        """Collect all configs, initiate application server and webserver
+        and run thread with uvicorn+falcon http server and wait
+        until it is up (started)"""
+
+        self.thread.start()
+        while not self.server.started:
+            continue
+
+    def _stop(self):
+        """Stop thread with uvicorn+falcon http server, wait for uvicorn
+        to exit gracefully and join the thread"""
+        if self.thread.is_alive():
+            self.server.should_exit = True
+            while self.thread.is_alive():
+                continue
+        self.thread.join()
+
+    def _override_runtime_logging(self):
+        """Uvicorn doesn't provide API to change name and handler beforehand
+        needs to be done during runtime"""
+        for logger_name in ["uvicorn", "uvicorn.access"]:
+            logging.getLogger(logger_name).removeHandler(logging.getLogger(logger_name).handlers[0])
+            logging.getLogger(logger_name).addHandler(
+                logging.getLogger("Logprep").parent.handlers[0]
+            )
+        logging.getLogger("uvicorn.access").name = self._logger_name
+        logging.getLogger("uvicorn.error").name = self._logger_name
+
+    def shut_down(self):
+        """Shutdown method to trigger http server shutdown externally"""
+        self._stop()
