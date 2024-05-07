d750ff638b0317740abd0c99f2cbab8e096aecb6
Piotr Pauksztelo <piotr.pauksztelo@fkie.fraunhofer.de>
2024-05-07 10:37:09 +0200

    Fix dry runner tests

--- tests/unit/util/test_rule_dry_runner.py
+++ tests/unit/util/test_rule_dry_runner.py
@@ -160,7 +160,7 @@ class TestRunLogprep:
         dry_runner.run()
 
         captured = capsys.readouterr()
-        assert captured.err == ""
+        assert "'tags': ['_pseudonymizer_missing_field_warning']" in captured.err
         assert "------ PROCESSED EVENT ------" in captured.out
         assert "------ TRANSFORMED EVENTS: 1/1 ------" in captured.out
         assert "------ CUSTOM OUTPUTS ------" in captured.out
