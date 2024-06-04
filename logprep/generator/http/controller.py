16ec6958095139c09d8db9747d14b1cd0e349c0a
ekneg54 <ekneg54@pm.me>
2024-06-04 09:54:25 +0200

    fix logging

--- logprep/generator/http/controller.py
+++ logprep/generator/http/controller.py
@@ -3,16 +3,17 @@ This generator will parse example events, manipulate their timestamps and send t
 a defined output
 """
 
-import json
 import logging
 import time
-from collections import Counter
 from concurrent.futures import ThreadPoolExecutor
 from logging import Logger
 
 from logprep.connector.http.output import HttpOutput
 from logprep.factory import Factory
 from logprep.generator.http.input import Input
+from logprep.util.logging import LogprepMPQueueListener, logqueue
+
+logger: Logger = logging.getLogger("Generator")
 
 
 class Controller:
@@ -23,9 +24,9 @@ class Controller:
 
     def __init__(self, **kwargs):
         self.config = kwargs
+        self._setup_logging()
         self.thread_count: int = kwargs.get("thread_count")
         self.use_reporter: bool = kwargs.get("report")
-        self.log: Logger = logging.getLogger("Generator")
         self.input: Input = Input(self.config)
         output_config = {
             "generator_output": {
@@ -37,28 +38,32 @@ class Controller:
         }
         self.output: HttpOutput = Factory.create(output_config)
 
+    def _setup_logging(self):
+        console_logger = logging.getLogger("console")
+        if console_logger.handlers:
+            console_handler = console_logger.handlers.pop()  # last handler is console
+            self.loghandler = LogprepMPQueueListener(logqueue, console_handler)
+            self.loghandler.start()
+
     def run(self):
         """
         Iterate over all event classes, trigger their processing and count the return statistics
         """
         # TODO use a logprep pipeline to handle processing
-        self.log.info("Started Data Processing")
+        logger.info("Started Data Processing")
         self.input.reformat_dataset()
         run_time_start = time.perf_counter()
-        statistics = Counter()
         try:
-            self._generate_load(statistics)
+            self._generate_load()
         except KeyboardInterrupt:
-            self.log.info("Gracefully shutting down...")
+            logger.info("Gracefully shutting down...")
         self.input.clean_up_tempdir()
         run_duration = time.perf_counter() - run_time_start
-        stats = json.dumps(statistics, sort_keys=True, indent=4, separators=(",", ": "))
-        self.log.info("Completed with following http return code statistics: %s", stats)
-        self.log.info("Execution time: %f seconds", run_duration)
-        return statistics
+        stats = self.output.metrics.status_codes.tracker.collect()[0].samples
+        logger.info("Completed with following http return code statistics: %s", stats)
+        logger.info("Execution time: %f seconds", run_duration)
+        self.loghandler.stop()
 
-    def _generate_load(self, statistics):
+    def _generate_load(self):
         with ThreadPoolExecutor(max_workers=self.thread_count) as executor:
             results = executor.map(self.output.store, self.input.load())
-            for stats in results:
-                statistics.update(stats)
