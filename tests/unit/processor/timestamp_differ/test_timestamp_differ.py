803899ee9b487e4ac88c53d5a50379d0cf0a547d
ekneg54 <ekneg54@pm.me>
2024-10-19 21:10:10 +0200

    remove ciso8601

--- tests/unit/processor/timestamp_differ/test_timestamp_differ.py
+++ tests/unit/processor/timestamp_differ/test_timestamp_differ.py
@@ -343,7 +343,7 @@ failure_test_cases = [  # testcase, rule, event, expected, error_message
             "subfield": {"field2": "2022-12-05 12:00:00"},
             "tags": ["_timestamp_differ_failure"],
         },
-        r".*ProcessingWarning.*Invalid character while parsing",
+        r".*ProcessingWarning.*Invalid isoformat string: 'non-timestamp'",
     ),
     (
         "diff between two timestamps with partial timestamp format match",
