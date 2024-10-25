0e945c0f4a5d5088a4b491942551e5ccecbebd89
dtrai2 <d.trai282@gmail.com>
2024-10-25 11:52:06 +0200

    add *args to LogprepException
    - InvalidConfigurationError receives an unspecified amount of arguments that couldn't be successfully forwarded to the LogprepException

--- logprep/abc/exceptions.py
+++ logprep/abc/exceptions.py
@@ -4,9 +4,9 @@
 class LogprepException(Exception):
     """Base class for Logprep related exceptions."""
 
-    def __init__(self, message: str) -> None:
+    def __init__(self, message: str, *args) -> None:
         self.message = message
-        super().__init__(message)
+        super().__init__(message, *args)
 
     def __eq__(self, __value: object) -> bool:
         return type(self) is type(__value) and self.args == __value.args
