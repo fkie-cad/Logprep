28d12b0303d511bbae122ce1e0aa41c6d1ca0aeb
dtrai2 <d.trai282@gmail.com>
2024-11-11 11:39:49 +0100

    fix auto_rule_tester

--- tests/unit/util/test_auto_rule_tester.py
+++ tests/unit/util/test_auto_rule_tester.py
@@ -281,8 +281,8 @@ class TestAutoRuleTester:
         ]
 
         expected_overall_results = [
-            "+ Successful Tests: 32",
-            "- Failed Tests: 6",
+            "+ Successful Tests: 31",
+            "- Failed Tests: 7",
             "~ Warning: 2",
             "Rule Test Coverage: 72.72727272727273",
             "Total Tests: 38",
