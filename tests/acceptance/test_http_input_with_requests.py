1c4481f4c2c89def7a9ea73f19429fdc91b4dca5
ekneg54 <ekneg54@pm.me>
2025-01-02 15:22:05 +0100

    fix test

--- tests/acceptance/test_http_input_with_requests.py
+++ tests/acceptance/test_http_input_with_requests.py
@@ -71,7 +71,8 @@ def test_http_input_accepts_message_for_single_pipeline(tmp_path: Path, config: 
 
 
 @pytest.mark.skipif(
-    all([sys.version_info[1] == 13, os.environ["CI"]]), reason="This test is not working"
+    all([sys.version_info[1] == 13, os.environ.get("CI", False)]),
+    reason="This test complains for already used ports on python 3.13 in CI",
 )
 @pytest.mark.filterwarnings("ignore:Unverified HTTPS request is being made to host '127.0.0.1'")
 def test_http_input_accepts_message_for_multiple_pipelines(tmp_path: Path, config: Configuration):
