3a771e29675ee5af7e32f5e77c355450ce43f048
Jörg Zimmermann <ekneg54@pm.me>
2025-02-04 11:30:00 +0100

    add method to registry to get the rule class by rule_definition
    add method to registry to get the rule class by rule_definition
    
    add method to registry to get the rule class by rule_definition

--- logprep/registry.py
+++ logprep/registry.py
@@ -3,6 +3,10 @@ it is used to check if a processor is known to the system.
 you have to register new processors here by import them and add to `ProcessorRegistry.mapping`
 """
 
+from typing import Dict, Type
+
+from logprep.abc.connector import Connector
+from logprep.abc.processor import Processor
 from logprep.connector.confluent_kafka.input import ConfluentKafkaInput
 from logprep.connector.confluent_kafka.output import ConfluentKafkaOutput
 from logprep.connector.console.output import ConsoleOutput
@@ -17,6 +21,7 @@ from logprep.connector.jsonl.output import JsonlOutput
 from logprep.connector.opensearch.output import OpensearchOutput
 from logprep.connector.s3.output import S3Output
 from logprep.processor.amides.processor import Amides
+from logprep.processor.base.rule import Rule
 from logprep.processor.calculator.processor import Calculator
 from logprep.processor.clusterer.processor import Clusterer
 from logprep.processor.concatenator.processor import Concatenator
@@ -48,7 +53,7 @@ from logprep.processor.timestamper.processor import Timestamper
 class Registry:
     """Component Registry"""
 
-    mapping = {
+    mapping: Dict[str, Type[Processor | Connector]] = {
         # Processors
         "amides": Amides,
         "calculator": Calculator,
@@ -94,7 +99,7 @@ class Registry:
     }
 
     @classmethod
-    def get_class(cls, component_type):
+    def get_class(cls, component_type: str) -> Type[Processor | Connector]:
         """return the processor class for a given type
 
         Parameters
@@ -107,4 +112,40 @@ class Registry:
         _type_
             _description_
         """
-        return cls.mapping.get(component_type)
+        processor_class = cls.mapping.get(component_type)
+        if processor_class is None:
+            raise ValueError(f"Unknown processor type: {component_type}")
+        return processor_class
+
+    @classmethod
+    def get_rule_class_by_rule_definition(cls, rule_definition: dict) -> Type[Rule]:
+        """return the processor class for a given type
+
+        Parameters
+        ----------
+        processor_type : str
+            the processor type
+
+        Returns
+        -------
+        rule_class
+            The class of the rule
+
+        Raises
+        ------
+        ValueError
+            if the rule type is unknown
+        """
+        processor_type = {elem for elem in rule_definition if elem in cls.mapping}
+        if not processor_type:
+            raise ValueError("Unknown rule type")
+        processor_type = processor_type.pop()
+        if not isinstance(processor_type, str):
+            raise ValueError("Unknown rule type")
+        processor_class = cls.get_class(processor_type)
+        if not issubclass(processor_class, Processor):
+            raise ValueError(f"Unknown processor type: {processor_type}")
+        rule_class = processor_class.rule_class
+        if rule_class is None:
+            raise ValueError(f"Rule type not set in processor: {processor_type}")
+        return rule_class
