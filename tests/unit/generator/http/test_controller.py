d85e11e5a97a6d788e23bee26a2b4b46e34ad91d
kmeinerz1 <kaithomas.meinerz@bwi.de>
2025-02-07 13:37:48 +0100

    wip added kafka output and a test

--- tests/unit/generator/http/test_controller.py
+++ tests/unit/generator/http/test_controller.py
@@ -4,7 +4,6 @@
 import os
 from unittest import mock
 
-import pytest
 import responses
 
 from logprep.generator.http.controller import Controller
@@ -25,7 +24,10 @@ class TestController:
             user="test-user",
             password="pass",
             thread_count=1,
+            kafka_output=None,
         )
+
+        self.controller_kafka = Controller(kafka_output='{"bootstrap.servers": "127.0.0.1:9092"}')
 
     # @pytest.mark.skip(reason="This test blocks and has to be fixed")  # TODO: Fix this test
     @responses.activate
@@ -108,3 +110,51 @@ class TestController:
         self.controller.run()
         mock_executor_class.assert_called_with(max_workers=2)
         mock_executor_instance.map.assert_called()
+
+    @responses.activate
+    def test_run_kafka_output(self, tmp_path):
+        dataset_path = tmp_path / "dataset"
+        class_one_number_events = 100
+        class_one_config = {
+            "target_path": "/target-one",
+            "timestamps": [{"key": "source.field", "format": "%Y%M%d"}],
+        }
+        create_test_event_files(
+            dataset_path,
+            {"example": "event"},
+            class_one_number_events,
+            class_name="class_one",
+            config=class_one_config,
+        )
+        class_two_number_events = 100
+        class_two_config = {
+            "target_path": "/target-two",
+            "timestamps": [{"key": "source.field", "format": "%H%M%S"}],
+        }
+        create_test_event_files(
+            dataset_path,
+            {"example": "event"},
+            class_two_number_events,
+            class_name="class_two",
+            config=class_two_config,
+        )
+        self.controller_kafka.input.input_root_path = dataset_path
+        self.controller_kafka.input.temp_dir = tmp_path / "tmp_input_file"  # Mock temp dir for test
+        os.makedirs(self.controller_kafka.input._temp_dir, exist_ok=True)
+        expected_status_code = 200
+        responses.add(responses.POST, f"{self.target_url}/target-one", status=expected_status_code)
+        responses.add(responses.POST, f"{self.target_url}/target-two", status=expected_status_code)
+        self.controller_kafka.run()
+
+        for call_id, call in enumerate(responses.calls):
+            if call_id < (class_one_number_events / self.batch_size):
+                assert (
+                    call.request.url == f"{self.target_url}/target-one"
+                ), f"Call {call_id} has wrong target"
+            else:
+                assert (
+                    call.request.url == f"{self.target_url}/target-two"
+                ), f"Call {call_id} has wrong target"
+            expected_http_header = "application/x-ndjson; charset=utf-8"
+            assert call.request.headers.get("Content-Type") == expected_http_header
+            assert call.response.status_code == 200
