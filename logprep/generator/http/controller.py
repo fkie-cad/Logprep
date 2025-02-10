672d90e70d431e395630eaba815f72e3484b0b5b
kmeinerz1 <kaithomas.meinerz@bwi.de>
2025-02-10 13:44:40 +0100

    restored legacy commands and added more tests

--- logprep/generator/http/controller.py
+++ logprep/generator/http/controller.py
@@ -8,9 +8,7 @@ import logging
 import time
 from concurrent.futures import ThreadPoolExecutor
 from logging import Logger
-from typing import Any
 
-from logprep.abc.output import Output
 from logprep.connector.confluent_kafka.output import ConfluentKafkaOutput
 from logprep.connector.http.output import HttpOutput
 from logprep.factory import Factory
@@ -22,7 +20,7 @@ logger: Logger = logging.getLogger("Generator")
 
 class Controller:
     """
-    Controls the workflow of the generator by reading inputs, manipulating events
+    Generally Controls the workflow of the generator by reading inputs, manipulating events
     and sending them to outputs
     """
 
@@ -32,7 +30,11 @@ class Controller:
         self._setup_logging()
         self.thread_count: int = kwargs.get("thread_count")
         self.input: Input = Input(self.config)
-        self.output = create_output(kwargs)
+        self.output = self._create_output(kwargs)
+
+    def _create_output(self, kwargs):
+        """To be implemented by subclasses."""
+        raise NotImplementedError("Subclasses must implement `_create_output`")
 
     def _setup_logging(self):
         console_logger = logging.getLogger("console")
@@ -42,6 +44,32 @@ class Controller:
             console_handler = console_logger.handlers.pop()  # last handler is console
             self.loghandler = LogprepMPQueueListener(logqueue, console_handler)
             self.loghandler.start()
+
+    def run(self):
+        raise NotImplementedError("Subclasses must implement `run`")
+
+    def _generate_load(self):
+        with ThreadPoolExecutor(max_workers=self.thread_count) as executor:
+            executor.map(self.output.store, self.input.load())
+
+
+class HTTPController(Controller):
+    """
+    Controller for the HTTP Generator
+    """
+
+    def _create_output(self, kwargs):
+        output_config = {
+            "generator_output": {
+                "type": "http_output",
+                "user": kwargs.get("user"),
+                "password": kwargs.get("password"),
+                "target_url": kwargs.get("target_url"),
+                "timeout": kwargs.get("timeout", 2),
+            }
+        }
+        self.output: HttpOutput = Factory.create(output_config)
+        return self.output
 
     def run(self) -> str:
         """
@@ -63,31 +91,39 @@ class Controller:
             self.loghandler.stop()
         return stats
 
-    def _generate_load(self):
-        with ThreadPoolExecutor(max_workers=self.thread_count) as executor:
-            executor.map(self.output.store, self.input.load())
 
+class KafkaController(Controller):
+    """
+    Controller for the HTTP Generator using Kafka output
+    """
 
-def create_output(kwargs: dict[str, Any]) -> Output:
-    """Factory function to create the appropriate output handler."""
-    if kwargs.get("kafka_config") is None:
-        output_config = {
-            "generator_output": {
-                "type": "http_output",
-                "user": kwargs.get("user"),
-                "password": kwargs.get("password"),
-                "target_url": kwargs.get("target_url"),
-                "timeout": kwargs.get("timeout", 2),
-            }
-        }
-        output: HttpOutput = Factory.create(output_config)
-    else:
+    def _create_output(self, kwargs):
         output_config = {
             "generator_output": {
                 "type": "confluentkafka_output",
                 "topic": "producer",
-                "kafka_config": json.loads(kwargs.get("kafka_config")),
+                "kafka_config": json.loads(kwargs.get("output_config")),
             },
         }
-        output: ConfluentKafkaOutput = Factory.create(output_config)
-    return output
+        self.output: ConfluentKafkaOutput = Factory.create(output_config)
+        return self.output
+
+    def run(self) -> str:
+        """
+        Iterate over all event classes, trigger their processing and count the return statistics
+        """
+        logger.info("Started Data Processing")
+        self.input.reformat_dataset()
+        run_time_start = time.perf_counter()
+        try:
+            self._generate_load()
+        except KeyboardInterrupt:
+            logger.info("Gracefully shutting down...")
+        self.input.clean_up_tempdir()
+        run_duration = time.perf_counter() - run_time_start
+        # stats = self.output.statistics
+        # logger.info("Completed with following statistics: %s", stats)
+        logger.info("Execution time: %f seconds", run_duration)
+        if self.loghandler is not None:
+            self.loghandler.stop()
+        return "some stats"
