2b4018284c294807f4256428610862c6c7caeaf9
kmeinerz1 <kaithomas.meinerz@bwi.de>
2025-02-10 16:16:56 +0100

    refactoring of code and test

--- logprep/generator/confluent_kafka/controller.py
+++ logprep/generator/confluent_kafka/controller.py
@@ -3,33 +3,32 @@ This generator will parse example events, manipulate their timestamps and send t
 a defined output
 """
 
+import json
 import logging
 import time
 from logging import Logger
 
 from logprep.abc.controller import Controller
-from logprep.connector.http.output import HttpOutput
+from logprep.connector.confluent_kafka.output import ConfluentKafkaOutput
 from logprep.factory import Factory
 
 logger: Logger = logging.getLogger("Generator")
 
 
-class HTTPController(Controller):
+class KafkaController(Controller):
     """
-    Controller for the HTTP Generator
+    Controller for the HTTP Generator using Kafka output
     """
 
     def _create_output(self, kwargs):
         output_config = {
             "generator_output": {
-                "type": "http_output",
-                "user": kwargs.get("user"),
-                "password": kwargs.get("password"),
-                "target_url": kwargs.get("target_url"),
-                "timeout": kwargs.get("timeout", 2),
-            }
+                "type": "confluentkafka_output",
+                "topic": "producer",
+                "kafka_config": json.loads(kwargs.get("output_config")),
+            },
         }
-        self.output: HttpOutput = Factory.create(output_config)
+        self.output: ConfluentKafkaOutput = Factory.create(output_config)
         return self.output
 
     def run(self) -> str:
@@ -45,9 +44,9 @@ class HTTPController(Controller):
             logger.info("Gracefully shutting down...")
         self.input.clean_up_tempdir()
         run_duration = time.perf_counter() - run_time_start
-        stats = self.output.statistics
-        logger.info("Completed with following statistics: %s", stats)
+        health = self.output.health()
+        logger.info("Completed with a healthy kafka producer: %s", health)
         logger.info("Execution time: %f seconds", run_duration)
         if self.loghandler is not None:
             self.loghandler.stop()
-        return stats
+        return "some stats"
