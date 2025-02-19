2b7ca2652c24dd1473c013d304aee4a8d38c6e24
Jörg Zimmermann <ekneg54@pm.me>
2025-02-19 11:09:56 +0100

    fix batcher tests

--- logprep/generator/http/batcher.py
+++ logprep/generator/http/batcher.py
@@ -1,10 +1,8 @@
 import logging
-import time
-from typing import List
+from itertools import islice
+from typing import Generator, Iterable, List, Tuple
 
-from logprep.abc.output import Output
-from logprep.generator.http.loader import FileLoader
-from logprep.util.defaults import DEFAULT_BATCH_SIZE, DEFAULT_BATCH_TIME
+from logprep.util.defaults import DEFAULT_BATCH_SIZE
 
 logger = logging.getLogger("Batcher")
 
@@ -12,39 +10,16 @@ logger = logging.getLogger("Batcher")
 class Batcher:
     """Batches messages from an EventBuffer and forwards them in controlled batches."""
 
-    def __init__(
-        self,
-        file_loader: FileLoader,
-        output: Output,
-        batch_size: int = DEFAULT_BATCH_SIZE,
-        batch_time: float = DEFAULT_BATCH_TIME,
-    ):
-
-        self.file_loader = file_loader
+    def __init__(self, batches: Iterable, batch_size: int = DEFAULT_BATCH_SIZE):
+        self.events = batches
         self.batch_size = batch_size
-        self.batch_time = batch_time
-        self.output = output
+        self._batch: List[str] = []
 
-    def process_batches(self):
-        """Processes messages from EventBuffer and sends them in batches."""
-        batch = []
-        last_sent_time = time.time()
-
-        with self.file_loader as file_loader:
-            for message in file_loader.read_lines():
-                batch.append(message)
-                if len(batch) >= self.batch_size:
-                    self._send_batch(batch)
-                    batch = []
-                    last_sent_time = time.time()
-
-                if (time.time() - last_sent_time) >= self.batch_time and batch:
-                    self._send_batch(batch)
-                    batch = []
-                    last_sent_time = time.time()
-        if batch:
-            self._send_batch(batch)
-
-    def _send_batch(self, batch: List[str]):
-        logger.info("Sending batch of %s messages.", len(batch))
-        self.output(batch)
+    def get_batch(self) -> Generator[Tuple[str], None, None]:
+        "Batch data into tuples of length n. The last batch may be shorter."
+        # TODO: Refactor this method to itertools.batched if support for 3.11 is dropped
+        if self.batch_size < 1:
+            raise ValueError("'batch_size' must be at least one")
+        it = iter(self.events)
+        while batch := tuple(islice(it, self.batch_size)):
+            yield batch
--- tests/unit/generator/http/test_batcher.py
+++ tests/unit/generator/http/test_batcher.py
@@ -1,87 +1,54 @@
 # pylint: disable=missing-docstring
 # pylint: disable=protected-access
 # pylint: disable=attribute-defined-outside-init
-import logging
 import random
-from unittest import mock
+
+import pytest
 
 from logprep.generator.http.batcher import Batcher
-from logprep.util.defaults import DEFAULT_BATCH_SIZE, DEFAULT_BATCH_TIME
+from logprep.util.defaults import DEFAULT_BATCH_SIZE
 
 
 class TestBatcher:
 
     def setup_method(self):
-        self.mock_file_loader = mock.MagicMock()
-        self.mock_output = mock.MagicMock()
-        self.mock_file_loader.read_lines.return_value = iter(
-            ["msg1", "msg2", "msg3", "msg4", "msg5", "msg6"]
-        )
-        self.mock_file_loader.__enter__.return_value = self.mock_file_loader
-        self.mock_file_loader.__exit__.return_value = None
+        self.batches = iter(["msg1", "msg2", "msg3"])
 
     def test_init(self):
-        batcher = Batcher(self.mock_file_loader, self.mock_output)
+        batcher = Batcher(self.batches)
         assert batcher
-        assert batcher.file_loader is self.mock_file_loader
-        assert batcher.output is self.mock_output
+        assert batcher.events is self.batches
 
     def test_init_defaults(self):
-        batcher = Batcher(self.mock_file_loader, self.mock_output)
+        batcher = Batcher(self.batches)
         assert batcher.batch_size is DEFAULT_BATCH_SIZE
-        assert batcher.batch_time is DEFAULT_BATCH_TIME
 
     def test_init_overwrites_defaults(self):
         random_size = random.randint(1, 100000)
-        random_time = random.random()
-        batcher = Batcher(self.mock_file_loader, self.mock_output, random_size, random_time)
+        batcher = Batcher(self.batches, random_size)
         assert batcher.batch_size is random_size
-        assert batcher.batch_time is random_time
 
-    def test_batcher_sends_everything_at_once(self):
-        batcher = Batcher(
-            self.mock_file_loader, output=self.mock_output, batch_size=10, batch_time=10
-        )
-        with (mock.patch.object(batcher, "_send_batch") as mock_send_batch,):
-            batcher.process_batches()
+    def test_get_yields_batches(self):
+        batcher = Batcher(self.batches, batch_size=10)
 
-            mock_send_batch.assert_any_call(["msg1", "msg2", "msg3", "msg4", "msg5", "msg6"])
-            assert mock_send_batch.call_count == 1
+        batch = next(batcher.get_batch())
 
-    def test_batcher_sends_batches_by_size(self):
-        batcher = Batcher(
-            self.mock_file_loader, output=self.mock_output, batch_size=3, batch_time=10
-        )
+        assert batch == ("msg1", "msg2", "msg3")
 
-        with (mock.patch.object(batcher, "_send_batch") as mock_send_batch,):
-            batcher.process_batches()
+    def test_get_yields_batches_by_size(self):
+        batcher = Batcher(self.batches, batch_size=2)
 
-            mock_send_batch.assert_any_call(["msg1", "msg2", "msg3"])
-            mock_send_batch.assert_any_call(["msg4", "msg5", "msg6"])
-            assert mock_send_batch.call_count == 2
+        batch_generator = batcher.get_batch()
+        batch = next(batch_generator)
+        assert batch == ("msg1", "msg2")
+        assert next(batcher.get_batch()) == ("msg3",)
 
-    def test_batcher_sends_batches_by_time(self):
+    def test_raises_value_error_on_negative_batch_size(self):
+        batcher = Batcher(self.batches, batch_size=-2)
+        with pytest.raises(ValueError):
+            next(batcher.get_batch())
 
-        batcher = Batcher(
-            self.mock_file_loader, output=self.mock_output, batch_size=10, batch_time=0.3
-        )
-
-        with (
-            mock.patch("time.time", side_effect=[0, 0.05, 0.15, 0.25, 0.35, 0.45, 0.55, 0.65]),
-            mock.patch.object(batcher, "_send_batch") as mock_send_batch,
-        ):
-            batcher.process_batches()
-
-            mock_send_batch.assert_any_call(["msg1", "msg2", "msg3", "msg4"])
-            mock_send_batch.assert_any_call(["msg5", "msg6"])
-            assert mock_send_batch.call_count == 2
-
-    def test_send_batch(self, caplog):
-        caplog.set_level(logging.INFO)
-        batcher = Batcher(
-            self.mock_file_loader, output=self.mock_output, batch_size=10, batch_time=10
-        )
-        batcher._send_batch(["msg1", "msg2"])
-
-        assert "Sending batch of 2 messages." in caplog.text
-        assert batcher.output.call_count == 1
+    def test_raises_value_error_on_zero_batch_size(self):
+        batcher = Batcher(self.batches, batch_size=0)
+        with pytest.raises(ValueError):
+            next(batcher.get_batch())
