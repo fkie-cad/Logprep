d3303a28efc6b971c204858d5131e0ea6d172c9a
Jörg Zimmermann <ekneg54@pm.me>
2025-02-21 16:33:51 +0100

    ensure full batches

--- logprep/generator/http/batcher.py
+++ logprep/generator/http/batcher.py
@@ -1,7 +1,6 @@
 import logging
 import threading
-from itertools import islice
-from typing import Generator, Iterable, List, Tuple
+from typing import Dict, Generator, Iterable
 
 from logprep.util.defaults import DEFAULT_BATCH_SIZE
 
@@ -11,21 +10,36 @@ logger = logging.getLogger("Batcher")
 class Batcher:
     """Batches messages from an EventBuffer and forwards them in controlled batches."""
 
-    def __init__(self, batches: Iterable, **config) -> None:
+    def __init__(self, input_events: Iterable[str], **config) -> None:
         self.batch_size: int = config.get("batch_size", DEFAULT_BATCH_SIZE)
-        self.events = iter(batches)
-        self._batch: List[str] = []
+        self.input_events = iter(input_events)
         self.lock = threading.Lock()
+        self._batches: Dict[str, str] = {}
 
     @property
-    def batches(self) -> Generator[Tuple[str], None, None]:
+    def batches(self) -> Generator[str, None, None]:
         "Batch data into tuples of length n. The last batch may be shorter."
-        # TODO: Refactor this method to itertools.batched if support for 3.11 is dropped
         if self.batch_size < 1:
             raise ValueError("'batch_size' must be at least one")
-        while True:
-            with self.lock:
-                batch = tuple(islice(self.events, self.batch_size))
-                if not batch:
-                    return
-                yield batch
+        with self.lock:
+            if self.batch_size == 1:
+                raise NotImplementedError("Batch size of 1 is not supported")
+            for event in self.input_events:
+                path, _, message = event.partition(",")
+                if path not in self._batches:
+                    self._batches[path] = event
+                else:
+                    self._batches[path] += f";{message}"
+                    if self._batches[path].count(";") + 1 >= self.batch_size:
+                        message = self._batches.pop(path)
+                        yield f"{message}\n"
+            while self._batches:
+                path, message = self._batches.popitem()
+                while message.count(";") + 1 < self.batch_size:
+                    if message.count(";") == 0:
+                        last_message = message.rpartition(",")[-1]
+                        message = f"{message};{last_message}\n"
+                    else:
+                        last_message = message.rpartition(";")[-1]
+                        message = f"{message};{last_message}\n"
+                yield message
--- tests/unit/generator/http/test_batcher.py
+++ tests/unit/generator/http/test_batcher.py
@@ -12,12 +12,12 @@ from logprep.util.defaults import DEFAULT_BATCH_SIZE
 class TestBatcher:
 
     def setup_method(self):
-        self.batches = iter(["msg1", "msg2", "msg3"])
+        self.batches = iter(["/path/to,msg1", "/path/to,msg2", "/path/to,msg3"])
 
     def test_init(self):
         batcher = Batcher(self.batches)
         assert batcher
-        assert batcher.events is self.batches
+        assert batcher.input_events is self.batches
 
     def test_init_defaults(self):
         batcher = Batcher(self.batches)
@@ -29,16 +29,18 @@ class TestBatcher:
         assert batcher.batch_size is random_size
 
     def test_get_yields_batches(self):
-        batcher = Batcher(self.batches, batch_size=10)
-
+        batcher = Batcher(self.batches, batch_size=2)
         batch = next(batcher.batches)
-
-        assert batch == ("msg1", "msg2", "msg3")
+        assert batch == "/path/to,msg1;msg2\n"
 
     def test_get_yields_batches_by_size(self):
         batcher = Batcher(self.batches, batch_size=2)
-        assert next(batcher.batches) == ("msg1", "msg2")
-        assert next(batcher.batches) == ("msg3",)
+        assert next(batcher.batches) == "/path/to,msg1;msg2\n"
+        assert next(batcher.batches) == "/path/to,msg3;msg3\n"
+
+    def test_get_yields_batches_by_size1(self):
+        batcher = Batcher(self.batches, batch_size=4)
+        assert next(batcher.batches) == "/path/to,msg1;msg2;msg3;msg3\n"
 
     def test_raises_value_error_on_negative_batch_size(self):
         batcher = Batcher(self.batches, batch_size=-2)
