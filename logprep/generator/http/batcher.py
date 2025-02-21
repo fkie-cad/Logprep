046bac9f5f68b5226df6350e3b50ae1f5710f69f
Jörg Zimmermann <ekneg54@pm.me>
2025-02-21 17:33:33 +0100

    add batcher event rotation

--- logprep/generator/http/batcher.py
+++ logprep/generator/http/batcher.py
@@ -1,5 +1,5 @@
+import itertools
 import logging
-import threading
 from typing import Dict, Generator, Iterable
 
 from logprep.util.defaults import DEFAULT_BATCH_SIZE
@@ -14,7 +14,6 @@ class Batcher:
         self.batch_size: int = config.get("batch_size", DEFAULT_BATCH_SIZE)
         self.event_count = config.get("events", 1)
         self.input_events = iter(input_events)
-        self.lock = threading.Lock()
         self._batches: Dict[str, str] = {}
 
     @property
@@ -22,25 +21,29 @@ class Batcher:
         "Batch data into tuples of length n. The last batch may be shorter."
         if self.batch_size < 1:
             raise ValueError("'batch_size' must be at least one")
-        with self.lock:
+        for event in itertools.cycle(self.input_events):
+            if self.event_count == 0:
+                break
             if self.batch_size == 1:
-                raise NotImplementedError("Batch size of 1 is not supported")
-            for event in self.input_events:
-                path, _, message = event.partition(",")
-                if path not in self._batches:
-                    self._batches[path] = event
+                self.event_count -= 1
+                yield f"{event}\n"
+            path, _, message = event.partition(",")
+            if path not in self._batches:
+                self._batches[path] = event
+                self.event_count -= 1
+            else:
+                self._batches[path] += f";{message}"
+                self.event_count -= 1
+                if self.event_count == 0 or self._batches[path].count(";") + 1 >= self.batch_size:
+                    message = self._batches.pop(path)
+                    yield f"{message}\n"
+        while self._batches and self.event_count > 0:
+            path, message = self._batches.popitem()
+            while message.count(";") + 1 < self.batch_size:
+                if message.count(";") == 0:
+                    last_message = message.rpartition(",")[-1]
+                    message = f"{message};{last_message}\n"
                 else:
-                    self._batches[path] += f";{message}"
-                    if self._batches[path].count(";") + 1 >= self.batch_size:
-                        message = self._batches.pop(path)
-                        yield f"{message}\n"
-            while self._batches:
-                path, message = self._batches.popitem()
-                while message.count(";") + 1 < self.batch_size:
-                    if message.count(";") == 0:
-                        last_message = message.rpartition(",")[-1]
-                        message = f"{message};{last_message}\n"
-                    else:
-                        last_message = message.rpartition(";")[-1]
-                        message = f"{message};{last_message}\n"
-                yield message
+                    last_message = message.rpartition(";")[-1]
+                    message = f"{message};{last_message}\n"
+            yield message
