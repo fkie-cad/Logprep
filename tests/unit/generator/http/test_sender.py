e07a0e065f1778a33613fbf325d460dfbe0d2480
kmeinerz1 <kaithomas.meinerz@bwi.de>
2025-02-28 14:31:35 +0100

    reverted change in document loader and more test fixes

--- tests/unit/generator/http/test_sender.py
+++ tests/unit/generator/http/test_sender.py
@@ -1,7 +1,10 @@
 # pylint: disable=missing-docstring
 # pylint: disable=protected-access
 # pylint: disable=attribute-defined-outside-init
+import threading
 from unittest import mock
+
+import pytest
 
 from logprep.generator.sender import Sender  # Adjust import as needed
 
@@ -34,3 +37,21 @@ class TestSender:
         self.sender.send_batches()
         self.mock_output.store.assert_has_calls(expected_calls, any_order=False)
         assert self.mock_output.store.call_count == 2
+
+    @pytest.mark.parametrize("thread_count", [2, 4, 8])
+    def test_sender_uses_multiple_threads(self, thread_count):
+        input_events = [{"event": i} for i in range(20)]
+        mock_output = mock.Mock()
+
+        active_threads = set()
+        event = threading.Event()
+
+        def mock_store(_):
+            active_threads.add(threading.current_thread().name)
+            event.wait(0.01)
+
+        mock_output.store.side_effect = mock_store
+        sender = Sender(input_events, mock_output, thread_count=thread_count)
+        sender.send_batches()
+        print(active_threads)
+        assert len(active_threads) == thread_count
