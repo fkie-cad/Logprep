48d3bcfa1913c9d585d0c60a3246d87336e0392a
kmeinerz1 <kaithomas.meinerz@bwi.de>
2025-03-04 01:03:06 +0100

    fixed remaining tests

--- tests/unit/connector/test_confluent_kafka_output.py
+++ tests/unit/connector/test_confluent_kafka_output.py
@@ -8,6 +8,7 @@ import json
 from copy import deepcopy
 from unittest import mock
 
+import msgspec
 import pytest
 from confluent_kafka.error import KafkaException
 
@@ -23,6 +24,10 @@ KAFKA_STATS_JSON_PATH = "tests/testdata/kafka_stats_return_value.json"
 
 
 class TestConfluentKafkaOutput(BaseOutputTestCase, CommonConfluentKafkaTestCase):
+
+    # def setup_method(self):
+    #     self.encoder = msgspec.json.Encoder()
+
     CONFIG = {
         "type": "confluentkafka_output",
         "topic": "test_input_raw",
@@ -46,6 +51,7 @@ class TestConfluentKafkaOutput(BaseOutputTestCase, CommonConfluentKafkaTestCase)
         "logprep_confluent_kafka_output_librdkafka_txmsg_bytes",
         "logprep_processing_time_per_event",
         "logprep_number_of_processed_events",
+        "logprep_processed_batches",
         "logprep_number_of_warnings",
         "logprep_number_of_errors",
     ]
@@ -57,19 +63,21 @@ class TestConfluentKafkaOutput(BaseOutputTestCase, CommonConfluentKafkaTestCase)
 
     @mock.patch("logprep.connector.confluent_kafka.output.Producer")
     def test_store_sends_event_to_expected_topic(self, _):
+        encoder = msgspec.json.Encoder()
         kafka_producer = self.object._producer
-        event = {"field": "content"}
-        event_raw = json.dumps(event, separators=(",", ":")).encode("utf-8")
+        event = '{"field": "content"}'
+        event_raw = encoder.encode(json.loads(event))
         expected_call = mock.call(self.CONFIG.get("topic"), value=event_raw)
-        self.object.store(event)
+        self.object.store(f'{self.CONFIG.get("topic")},' + event)
         kafka_producer.produce.assert_called()
         assert expected_call in kafka_producer.produce.mock_calls
 
     @mock.patch("logprep.connector.confluent_kafka.output.Producer")
     def test_store_custom_sends_event_to_expected_topic(self, _):
+        encoder = msgspec.json.Encoder()
         kafka_producer = self.object._producer
-        event = {"field": "content"}
-        event_raw = json.dumps(event, separators=(",", ":")).encode("utf-8")
+        event = '{"field": "content"}'
+        event_raw = encoder.encode(json.loads(event))
         expected_call = mock.call(self.CONFIG.get("topic"), value=event_raw)
         self.object.store_custom(event, self.CONFIG.get("topic"))
         kafka_producer.produce.assert_called()
@@ -79,7 +87,8 @@ class TestConfluentKafkaOutput(BaseOutputTestCase, CommonConfluentKafkaTestCase)
     def test_store_custom_calls_producer_flush_on_buffererror(self, _):
         kafka_producer = self.object._producer
         kafka_producer.produce.side_effect = BufferError
-        self.object.store_custom({"message": "does not matter"}, "doesnotcare")
+        event = '{"message": "does not matter"}'
+        self.object.store_custom(event, "doesnotcare")
         kafka_producer.flush.assert_called()
 
     @mock.patch("logprep.connector.confluent_kafka.output.Producer")
@@ -99,12 +108,12 @@ class TestConfluentKafkaOutput(BaseOutputTestCase, CommonConfluentKafkaTestCase)
             CriticalOutputError,
             match=r"bad things happened",
         ):
-            self.object.store({"message": "test message"})
+            self.object.store('topic,{"message": "test message"}')
 
     @mock.patch("logprep.connector.confluent_kafka.output.Producer")
     def test_store_counts_processed_events(self, _):  # pylint: disable=arguments-differ
         self.object.metrics.number_of_processed_events = 0
-        self.object.store({"message": "my event message"})
+        self.object.store('topic,{"message": "my event message"}')
         assert self.object.metrics.number_of_processed_events == 1
 
     def test_setup_raises_fatal_output_error_on_invalid_config(self):
