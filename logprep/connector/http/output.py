c030ccf4d60dd2ccf78e3a27675bbd4a2567624b
Jörg Zimmermann <joerg.zimmermann@bwi.de>
2025-04-04 12:51:12 +0000

    set secure default for http output verify

--- logprep/connector/http/output.py
+++ logprep/connector/http/output.py
@@ -121,7 +121,7 @@ class HttpOutput(Output):
         timeout: int = field(validator=validators.instance_of(int), default=2)
         """Timeout in seconds for the http request"""
         verify: bool | str = field(
-            default=False,
+            default=True,
             converter=lambda x: (
                 False
                 if x is None
