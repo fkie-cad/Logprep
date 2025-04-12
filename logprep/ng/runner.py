38e4d5c14f4e9350352f1f88d5cd812de0c98c06
Jörg Zimmermann <joerg.zimmermann@bwi.de>
2025-04-12 09:38:08 +0000

    add benchmark prerequisites

--- logprep/ng/runner.py
+++ logprep/ng/runner.py
@@ -1,7 +1,9 @@
+import json
+import tempfile
 from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor
 from itertools import islice
+from pathlib import Path
 
-from logprep.factory import Factory
 from logprep.util.time import TimeParser
 
 
@@ -10,9 +12,16 @@ class Input:
 
     _counter = 0
 
+    _elements = 10
+
+    def __init__(self, events: int):
+        self._elements = events
+
     def get_next(self):
         """retrieves Event"""
-        Input._counter += 1
+        self._counter += 1
+        if self._counter >= self._elements:
+            raise RuntimeError("No more events")
         return f"event {Input._counter}"
 
     def __next__(self):
@@ -39,9 +48,15 @@ class Pipeline:
         return self
 
     def compute(self, event):
+        """Does some computational work"""
+        result = 0
+        for i in range(1000):
+            for j in range(1000):
+                result += i * j
         return {
             "processed": event,
             "@timestamp": TimeParser.from_timestamp(TimeParser.now().timestamp()).isoformat(),
+            "result": result,
         }
 
     def process_pipeline(self):
@@ -56,9 +71,20 @@ class Pipeline:
 class Output:
     """output class"""
 
-    def store(self, result):
+    def __init__(self):
+        self.tempdir = tempfile.gettempdir()
+
+    def store(self, result: dict):
         """Stores results"""
+        with open(self.tempdir + "/result.txt", "w") as file:
+            for i in range(1000):
+                result.update({"line": i})
+                json_line = json.dumps(result)
+                file.write(json_line)
         print(f"stored {result}")
+
+    def shut_down(self):
+        Path(self.tempdir + "/result.txt").unlink(missing_ok=True)
 
 
 class Sender:
@@ -95,20 +121,29 @@ class Runner:
         "desired_cluster_status": ["green", "yellow"],
     }
 
-    def __init__(self, process_count=10):
+    def __init__(self, events=100, process_count=10):
         self.process_count = process_count
-        self.input = Input()
-        # self.output = Output()
-        self.output = Factory.create({"opesearch": self.output_config})
+        self.input = Input(events)
+        self.output = Output()
+        # self.output = Factory.create({"opesearch": self.output_config})
         self.pipeline = Pipeline(self.input)
         self.sender = Sender(self.pipeline, self.output)
 
     def run(self):
         """Run the pipeline"""
-        for _ in self.sender.send():
-            pass
+        try:
+            for _ in self.sender.send():
+                pass
+        except RuntimeError as error:
+            print(error)
 
 
 if __name__ == "__main__":
-    runner = Runner()
+    runner = Runner(1000)
     runner.run()
+    # input_connector = Input()
+    # exit_received = False
+    # while not exit_received:
+    #     events = islice(input_connector, 1)
+    #     for event in events:
+    #         print(event)
