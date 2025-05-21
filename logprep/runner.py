6967b91c804c8bcd5aeb62c7492e41028dcf55fc
Jörg Zimmermann <joerg.zimmermann@bwi.de>
2025-05-21 14:38:41 +0000

    remove configuration refresh behavior

--- logprep/runner.py
+++ logprep/runner.py
@@ -14,12 +14,7 @@ from schedule import Scheduler
 from logprep.abc.component import Component
 from logprep.framework.pipeline_manager import PipelineManager
 from logprep.metrics.metrics import CounterMetric, GaugeMetric
-from logprep.util.configuration import (
-    ConfigGetterException,
-    Configuration,
-    ConfigVersionDidNotChangeError,
-    InvalidConfigurationError,
-)
+from logprep.util.configuration import Configuration
 from logprep.util.defaults import EXITCODES
 
 
@@ -51,8 +46,6 @@ class Runner:
     _metrics: "Runner.Metrics"
 
     _exit_received: bool = False
-
-    scheduler: Scheduler
 
     @define(kw_only=True)
     class Metrics(Component.Metrics):
@@ -112,21 +105,6 @@ class Runner:
         }
         return labels
 
-    @property
-    def _config_refresh_interval(self) -> int:
-        """Indicates the configuration refresh interval in seconds."""
-        return self._configuration.config_refresh_interval
-
-    @_config_refresh_interval.setter
-    def _config_refresh_interval(self, value: int | None) -> None:
-        """Set the configuration refresh interval in seconds."""
-        if value is None:
-            self._configuration.config_refresh_interval = None
-        elif value <= 5:
-            self._configuration.config_refresh_interval = 5
-        else:
-            self._configuration.config_refresh_interval = value
-
     # Use this method to obtain a runner singleton for production
     @staticmethod
     def get_runner(configuration: Configuration) -> "Runner":
@@ -153,7 +131,7 @@ class Runner:
         error occurs.
         """
         self._set_version_info_metric()
-        self._schedule_config_refresh_job()
+        self._configuration.schedule_config_refresh()
         self._manager.start()
         self._logger.info("Startup complete")
         self._logger.debug("Runner iterating")
@@ -169,7 +147,7 @@ class Runner:
         for _ in self._keep_iterating():
             if self._exit_received:
                 break
-            self.scheduler.run_pending()
+            self._configuration.refresh()
             if self._manager.should_exit():
                 self.exit_code = EXITCODES.PIPELINE_ERROR.value
                 self._logger.error("Restart count exceeded. Exiting.")
@@ -177,28 +155,6 @@ class Runner:
             if self._manager.error_queue is not None:
                 self.metrics.number_of_events_in_error_queue += self._manager.error_queue.qsize()
             self._manager.restart_failed_pipeline()
-
-    def reload_configuration(self):
-        """Reloads the configuration"""
-        try:
-            self._configuration.reload()
-            self._logger.info("Successfully reloaded configuration")
-            self.metrics.number_of_config_refreshes += 1
-            self._manager.reload()
-            self._schedule_config_refresh_job()
-            self._logger.info(f"Configuration version: {self._configuration.version}")
-            self._set_version_info_metric()
-        except ConfigGetterException as error:
-            self._logger.warning(f"Failed to load configuration: {error}")
-            self.metrics.number_of_config_refresh_failures += 1
-            self._config_refresh_interval = int(self._config_refresh_interval / 4)
-            self._schedule_config_refresh_job()
-        except ConfigVersionDidNotChangeError as error:
-            self._logger.info(str(error))
-            self._schedule_config_refresh_job()
-        except InvalidConfigurationError as error:
-            self._logger.error(str(error))
-            self.metrics.number_of_config_refresh_failures += 1
 
     def _set_version_info_metric(self):
         self.metrics.version_info.add_with_labels(
@@ -210,16 +166,6 @@ class Runner:
         """Stop the logprep runner. Is called by the signal handler
         in run_logprep.py."""
         self._exit_received = True
-
-    def _schedule_config_refresh_job(self):
-        refresh_interval = self._config_refresh_interval
-        scheduler = self.scheduler
-        if scheduler.jobs:
-            scheduler.cancel_job(scheduler.jobs[0])
-        if isinstance(refresh_interval, (float, int)):
-            self.metrics.config_refresh_interval += refresh_interval
-            scheduler.every(refresh_interval).seconds.do(self.reload_configuration)
-            self._logger.info(f"Config refresh interval is set to: {refresh_interval} seconds")
 
     def _keep_iterating(self) -> Generator:
         """Indicates whether the runner should keep iterating."""
