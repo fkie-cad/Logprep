12a057b06fb918420c35f8a8e8c89fed0308f519
kmeinerz1 <kaithomas.meinerz@bwi.de>
2025-06-25 11:20:46 +0200

    added pseudonym Event class

---
+++ tests/unit/ng/events/test_pseudonym_event.py
@@ -0,0 +1,39 @@
+# pylint: disable=missing-docstring
+# pylint: disable=protected-access
+# pylint: disable=too-few-public-methods
+# pylint: disable=redefined-slots-in-subclass
+
+
+from logprep.ng.abc.event import Event
+from logprep.ng.event_state import EventState, EventStateType
+from logprep.ng.events.pseudonym_event import PseudonymEvent
+from tests.unit.ng.test_event import TestEventClass
+
+
+class DummyEvent(Event):
+    __slots__ = Event.__slots__
+
+
+class TestPseudonymEvents(TestEventClass):
+
+    def test_pseudonym_event_initializes_correctly(self) -> None:
+        pseudonym_event = PseudonymEvent(data={"foo": "bar"})
+
+        assert isinstance(pseudonym_event.state, EventState)
+
+    def test_pseudonym_event_preserves_state_on_init(self) -> None:
+        state = EventState()
+        state.current_state = EventStateType.STORED_IN_OUTPUT
+        pseudonym_event = PseudonymEvent(data={"msg": "payload"}, state=state)
+
+        assert pseudonym_event.state.current_state is EventStateType.STORED_IN_OUTPUT
+
+    def test_pseudonym_event_transition_to_next(self) -> None:
+
+        pseudonym_event = PseudonymEvent(
+            data={"parent": "yes"},
+        )
+        pseudonym_event.state.current_state = EventStateType.PROCESSING
+
+        pseudonym_event.state.next_state(success=True)
+        assert pseudonym_event.state.current_state == EventStateType.PROCESSED
