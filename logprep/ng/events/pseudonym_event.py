21e9e28745d02c1ed379affa79279d0fef988233
kmeinerz1 <kaithomas.meinerz@bwi.de>
2025-06-30 17:58:20 +0200

    fix code quality

--- logprep/ng/events/pseudonym_event.py
+++ logprep/ng/events/pseudonym_event.py
@@ -1,11 +1,7 @@
 """Concrete Pseudonym Event implementation"""
 
-from typing import Any
-
 from logprep.ng.abc.event import Event
-from logprep.ng.event_state import EventState
 
 
 class PseudonymEvent(Event):
     """Concrete Pseudonym event class."""
-
