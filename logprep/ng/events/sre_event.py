6cb907f8467d4b4fdd26dd78d50787f7f4122f1a
kmeinerz1 <kaithomas.meinerz@bwi.de>
2025-06-30 17:50:41 +0200

    small fix

--- logprep/ng/events/sre_event.py
+++ logprep/ng/events/sre_event.py
@@ -1,13 +1,7 @@
 """Concrete Sre Event implementation"""
 
-from typing import Any
-
 from logprep.ng.abc.event import Event
-from logprep.ng.event_state import EventState
 
 
 class SreEvent(Event):
     """Concrete Sre event class."""
-
-        """
-        self._state = value
