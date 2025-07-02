aabf45d82d117a55dcf452b3e9ea73b025bfeaf4
Jörg Zimmermann <joerg.zimmermann@bwi.de>
2025-07-02 12:36:08 +0000

    remove metrics from processor abc

--- logprep/ng/abc/processor.py
+++ logprep/ng/abc/processor.py
@@ -24,9 +24,6 @@ logger = logging.getLogger("Processor")
 class Processor(Component):
     """Abstract Processor Class to define the Interface"""
 
-    class Metrics(Component.Metrics):
-        """Metrics for Processor"""
-
     @define(kw_only=True, slots=False)
     class Config(Component.Config):
         """Common Configurations"""
