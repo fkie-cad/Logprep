283ada49bce56390a3abd7819334725de0bc9c32
kmeinerz1 <kaithomas.meinerz@bwi.de>
2025-07-04 12:29:34 +0200

    started update deleter

---
+++ tests/unit/ng/processor/deleter/test_deleter.py
@@ -0,0 +1,27 @@
+# pylint: disable=missing-docstring
+import pytest
+
+from tests.unit.processor.base import BaseProcessorTestCase
+
+
+class TestDeleter(BaseProcessorTestCase):
+    CONFIG = {
+        "type": "deleter",
+        "rules": ["tests/testdata/unit/deleter/rules"],
+    }
+
+    @pytest.mark.parametrize(
+        "event, testcase",
+        [
+            ({"not_needed_message": "i am not needed anymore"}, "deletes simple event"),
+            (
+                {"not_needed_message": {"nested_block": {"deeper": "string"}}},
+                "deletes nested events",
+            ),
+            ({}, "deletes empty event"),
+        ],
+    )
+    def test_process_deletes_event(self, event, testcase):
+        self.object.process(event)
+        assert not event, testcase
+        assert isinstance(event, dict), testcase
