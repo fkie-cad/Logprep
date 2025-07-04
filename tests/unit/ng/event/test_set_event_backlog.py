d0f681730cbacdaa85fc0032b6d3779ba4f1166e
David Kaya <david.kaya@gmx.net>
2025-07-04 11:39:31 +0200

    add seteventbacklog + add/adapt tests

---
+++ tests/unit/ng/event/test_set_event_backlog.py
@@ -0,0 +1,84 @@
+# pylint: disable=missing-docstring
+
+import pytest
+
+from logprep.ng.abc.event import Event
+from logprep.ng.event.event_state import EventStateType
+from logprep.ng.event.set_event_backlog import SetEventBacklog
+
+
+class DummyEvent(Event):
+    __slots__ = Event.__slots__
+
+
+class TestSetEventBacklog:
+    def test_register_adds_events(self):
+        backlog = SetEventBacklog()
+
+        e1 = DummyEvent(data={"foo": "bar"})
+        e2 = DummyEvent(data={"john": "doe"})
+
+        backlog.register([e1, e2])
+
+        assert len(backlog.backlog) == 2
+        assert e1 in backlog.backlog
+        assert e2 in backlog.backlog
+
+    def test_get_returns_events_but_does_not_remove(self):
+        backlog = SetEventBacklog()
+
+        e1 = DummyEvent(data={"foo": "bar"})
+        e1.state.current_state = EventStateType.RECEIVED
+
+        e2 = DummyEvent(data={"john": "doe"})
+        e3 = DummyEvent(data={"state": "done"})
+
+        backlog.register([e1, e2, e3])
+        result = backlog.get(EventStateType.RECEIVED)
+
+        assert len(result) == 1
+
+    def test_unregister_removes_events_with_failed_state(self):
+        backlog = SetEventBacklog()
+
+        e1 = DummyEvent(data={"foo": "bar"})
+        e1.state.current_state = EventStateType.ACKED
+
+        e2 = DummyEvent(data={"john": "doe"})
+        e2.state.current_state = EventStateType.FAILED
+
+        e3 = DummyEvent(data={"state": "done"})
+
+        backlog.register([e1, e2, e3])
+        result = backlog.unregister(EventStateType.FAILED)
+
+        assert result == [e2]
+        assert e1 in backlog.backlog
+        assert e2 not in backlog.backlog
+        assert e3 in backlog.backlog
+
+    def test_unregister_removes_events_with_acked_state(self):
+        backlog = SetEventBacklog()
+
+        e1 = DummyEvent(data={"foo": "bar"})
+        e1.state.current_state = EventStateType.ACKED
+
+        e2 = DummyEvent(data={"john": "doe"})
+        e2.state.current_state = EventStateType.FAILED
+
+        e3 = DummyEvent(data={"state": "done"})
+        e3.state.current_state = EventStateType.ACKED
+
+        backlog.register([e1, e2, e3])
+        result = backlog.unregister(state_type=EventStateType.ACKED)
+
+        assert result == [e1, e3]
+        assert e1 not in backlog.backlog
+        assert e2 in backlog.backlog
+        assert e3 not in backlog.backlog
+
+    def test_unregister_with_invalid_state_raises(self):
+        backlog = SetEventBacklog()
+
+        with pytest.raises(ValueError, match="Invalid state_type"):
+            backlog.unregister(state_type=EventStateType.STORED_IN_ERROR)  # type: ignore
