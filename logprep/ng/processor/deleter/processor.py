8041e3d19cd0d859283cbe30f2808595d3a33e38
kmeinerz <meinerzkai@gmail.com>
2025-07-08 07:26:35 +0000

    Make deleter use LogEvent class (#846)
    * started update deleter
    
    * added ng deleter
    
    ---------
    
    Co-authored-by: kmeinerz1 <kaithomas.meinerz@bwi.de>

---
+++ logprep/ng/processor/deleter/processor.py
@@ -0,0 +1,37 @@
+"""
+Deleter
+=======
+
+The `deleter` is a processor that removes an entire event from further pipeline processing.
+
+Processor Configuration
+^^^^^^^^^^^^^^^^^^^^^^^
+..  code-block:: yaml
+    :linenos:
+
+    - deletename:
+        type: deleter
+        rules:
+            - tests/testdata/rules/rules
+
+.. autoclass:: logprep.processor.deleter.processor.Deleter.Config
+   :members:
+   :undoc-members:
+   :inherited-members:
+   :noindex:
+
+.. automodule:: logprep.processor.deleter.rule
+"""
+
+from logprep.ng.abc.processor import Processor
+from logprep.processor.deleter.rule import DeleterRule
+
+
+class Deleter(Processor):
+    """A processor that deletes processed log events from further pipeline process"""
+
+    rule_class = DeleterRule
+
+    def _apply_rules(self, event: dict, rule: DeleterRule) -> None:
+        if rule.delete_event:
+            event.clear()
--- logprep/registry.py
+++ logprep/registry.py
@@ -26,6 +26,7 @@ from logprep.ng.processor.amides.processor import Amides as NgAmides
 from logprep.ng.processor.calculator.processor import Calculator as NGCalculator
 from logprep.ng.processor.clusterer.processor import Clusterer as NgClusterer
 from logprep.ng.processor.concatenator.processor import Concatenator as NgConcatenator
+from logprep.ng.processor.deleter.processor import Deleter as NgDeleter
 from logprep.ng.processor.datetime_extractor.processor import (
     DatetimeExtractor as NgDatetimeExtractor,
 )
@@ -88,6 +89,7 @@ class Registry:
         "ng_calculator": NGCalculator,
         "ng_clusterer": NgClusterer,
         "ng_concatenator": NgConcatenator,
+        "ng_deleter": NgDeleter,
         "ng_datetime_extractor": NgDatetimeExtractor,
         "ng_field_manager": NgFieldManager,
         "ng_amides": NgAmides,
