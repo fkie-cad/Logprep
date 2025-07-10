88082f800dd256debad889ca2cf8b1365c9833a8
Jörg Zimmermann <joerg.zimmermann@bwi.de>
2025-07-10 13:29:10 +0000

    add ng selective extractor

---
+++ tests/unit/ng/processor/selective_extractor/test_selective_extractor.py
@@ -0,0 +1,178 @@
+# pylint: disable=missing-docstring
+# pylint: disable=protected-access
+import uuid
+from unittest import mock
+
+from logprep.abc.processor import ProcessorResult
+from logprep.processor.selective_extractor.rule import SelectiveExtractorRule
+from tests.unit.ng.processor.base import BaseProcessorTestCase
+
+
+class TestSelectiveExtractor(BaseProcessorTestCase):
+    CONFIG = {
+        "type": "ng_selective_extractor",
+        "rules": ["tests/testdata/unit/selective_extractor/rules"],
+    }
+
+    def test_selective_extractor_does_not_change_orig_doc(self):
+        document = {"user": "test_user", "other": "field"}
+        exp_document = {"user": "test_user", "other": "field"}
+
+        self.object.process(document)
+
+        assert document == exp_document
+
+    def test_process_returns_list_of_tuples(self):
+        document = {"message": "test_message", "other": "field"}
+        tuple_list = self.object.process(document)
+        assert isinstance(tuple_list, ProcessorResult)
+        assert len(tuple_list.data) > 0
+
+    def test_process_returns_tuple_list_with_extraction_fields_from_rule(self):
+        field_name = f"{uuid.uuid4()}"
+        rule = SelectiveExtractorRule.create_from_dict(
+            {
+                "filter": field_name,
+                "selective_extractor": {
+                    "source_fields": [field_name],
+                    "outputs": [{"kafka": "topic"}],
+                },
+            }
+        )
+        self.object._rule_tree.add_rule(rule)
+        document = {field_name: "the value"}
+        tuple_list = self.object.process(document)
+        for filtered_event, _ in tuple_list.data:
+            if field_name in filtered_event:
+                break
+        else:
+            assert False
+
+    def test_process_returns_selective_extractor_target_topic(self):
+        field_name = f"{uuid.uuid4()}"
+        rule = {
+            "filter": field_name,
+            "selective_extractor": {
+                "source_fields": [field_name],
+                "outputs": [{"opensearch": "my topic"}],
+            },
+        }
+        self._load_rule(rule)
+        document = {field_name: "test_message", "other": "field"}
+        result = self.object.process(document)
+        output = result.data[0][1][0]
+        assert "my topic" in output.values()
+
+    def test_process_returns_selective_extractor_target_output(self):
+        field_name = f"{uuid.uuid4()}"
+        rule = {
+            "filter": field_name,
+            "selective_extractor": {
+                "source_fields": [field_name],
+                "outputs": [{"opensearch": "index"}],
+            },
+        }
+        self._load_rule(rule)
+        document = {field_name: "test_message", "other": "field"}
+        result = self.object.process(document)
+        output = result.data[0][1][0]
+        assert "opensearch" in output.keys()
+
+    def test_process_returns_extracted_fields(self):
+        document = {"message": "test_message", "other": "field"}
+        rule = {
+            "filter": "message",
+            "selective_extractor": {
+                "source_fields": ["message"],
+                "outputs": [{"opensearch": "index"}],
+            },
+        }
+        self._load_rule(rule)
+        result = self.object.process(document)
+        for filtered_event, *_ in result.data:
+            if filtered_event == {"message": "test_message"}:
+                break
+        else:
+            assert False
+
+    def test_process_returns_none_when_no_extraction_field_matches(self):
+        document = {"nomessage": "test_message", "other": "field"}
+        result = self.object.process(document)
+        assert isinstance(result, ProcessorResult)
+        assert result.data == []
+        assert result.errors == []
+        assert result.processor_name == "Test Instance Name"
+
+    def test_gets_matching_rules_from_rules_tree(self):
+        matching_rules = self.object._rule_tree.get_matching_rules({"message": "the message"})
+        assert isinstance(matching_rules, list)
+        assert len(matching_rules) > 0
+
+    def test_apply_rules_is_called(self):
+        with mock.patch(
+            f"{self.object.__module__}.{self.object.__class__.__name__}._apply_rules"
+        ) as mock_apply_rules:
+            self.object.process({"message": "the message"})
+            mock_apply_rules.assert_called()
+
+    def test_process_extracts_dotted_fields(self):
+        rule = {
+            "filter": "message",
+            "selective_extractor": {
+                "source_fields": ["other.message", "message"],
+                "outputs": [{"opensearch": "index"}],
+            },
+        }
+        self._load_rule(rule)
+        document = {"message": "test_message", "other": {"message": "my message value"}}
+        result = self.object.process(document)
+
+        for extracted_event, *_ in result.data:
+            if extracted_event.get("other", {}).get("message") is not None:
+                break
+        else:
+            assert False, f"other.message not in {result}"
+
+    def test_process_clears_internal_filtered_events_list_before_every_event(self):
+        document = {"message": "test_message", "other": {"message": "my message value"}}
+        _ = self.object.process(document)
+        assert len(self.object.result.data) == 1
+        _ = self.object.process(document)
+        assert len(self.object.result.data) == 1
+
+    def test_process_extracts_dotted_fields_complains_on_missing_fields(self):
+        rule = {
+            "filter": "message",
+            "selective_extractor": {
+                "source_fields": ["other.message", "not.exists", "message"],
+                "outputs": [{"opensearch": "index"}],
+                "ignore_missing_fields": False,
+            },
+        }
+        self._load_rule(rule)
+        document = {"message": "test_message", "other": {"message": "my message value"}}
+        expected = {
+            "message": "test_message",
+            "other": {"message": "my message value"},
+            "tags": ["_selective_extractor_missing_field_warning"],
+        }
+        self.object.process(document)
+        assert document == expected
+
+    def test_process_extracts_dotted_fields_and_ignores_missing_fields(self):
+        rule = {
+            "filter": "message",
+            "selective_extractor": {
+                "source_fields": ["other.message", "message", "not.exists"],
+                "outputs": [{"opensearch": "index"}],
+                "ignore_missing_fields": True,
+            },
+        }
+        self._load_rule(rule)
+        document = {"message": "test_message", "other": {"message": "my message value"}}
+        expected = {
+            "message": "test_message",
+            "other": {"message": "my message value"},
+        }
+        self.object.process(document)
+        assert document == expected
