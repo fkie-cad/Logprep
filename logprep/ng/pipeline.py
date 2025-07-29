0752f0364bb8317929134248dd9101d8c77d9b60
Jörg Zimmermann <joerg.zimmermann@bwi.de>
2025-07-29 14:11:06 +0000

    add state handling

--- logprep/ng/pipeline.py
+++ logprep/ng/pipeline.py
@@ -75,7 +75,6 @@ class Pipeline(Iterator):
 
     def _process_event(self, event: LogEvent) -> LogEvent:
         """process all processors for one event"""
-        event.state.next_state()
         for processor in self._processors:
             if not event.data:
                 break
