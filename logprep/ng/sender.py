1a60abfd14d37d12c24e5c7bbb4bb743cca58bbe
Jörg Zimmermann <joerg.zimmermann@bwi.de>
2025-07-31 12:56:36 +0000

    add simple output processing

--- logprep/ng/sender.py
+++ logprep/ng/sender.py
@@ -1,9 +1,43 @@
+from collections.abc import Iterator
+from itertools import islice
+from typing import Generator
+
 from logprep.ng.abc.output import Output
+from logprep.ng.event.event_state import EventStateType
+from logprep.ng.event.log_event import LogEvent
 from logprep.ng.pipeline import Pipeline
 
 
-class Sender:
+class Sender(Iterator):
     def __init__(self, pipeline: Pipeline, outputs: list[Output], error_output: Output) -> None:
-        self.pipeline = pipeline
-        self.outputs = outputs
-        self.error_output = error_output
+        self._pipeline = pipeline
+        self._outputs = {output.name: output for output in outputs}
+        self._error_output = error_output
+
+    def __next__(self):
+        event = next(self._pipeline)
+        self._send(event)
+
+    def __iter__(self) -> Generator[LogEvent, None, None]:
+        """Iterate over processed events."""
+        while True:
+            events = (event for event in self._pipeline if event is not None and event.data)
+            batch = list(islice(events, self._pipeline._process_count))
+            if not batch:
+                break
+            yield from map(self._send, batch)
+
+    def _send_extra_data(self, event: LogEvent) -> None:
+        for extra_data in event.extra_data:
+            pass
+
+    def _send(self, event: LogEvent) -> LogEvent:
+        if event.extra_data:
+            self._send_extra_data(event)
+        if event.state.current_state == EventStateType.PROCESSED:
+            for _, output in self._outputs.items():
+                if output.default:
+                    output.store(event)
+        elif event.state.current_state == EventStateType.FAILED and self._error_output:
+            self._error_output.store(event)
+        return event
--- tests/unit/ng/test_sender.py
+++ tests/unit/ng/test_sender.py
@@ -1,4 +1,7 @@
 # pylint: disable=missing-docstring
+# pylint: disable=protected-access
+from unittest import mock
+
 import pytest
 
 from logprep.factory import Factory
@@ -63,6 +66,39 @@ def get_processors_mock():
     return processors
 
 
+@pytest.fixture(name="opensearch_output")
+def get_opensearch_mock():
+    return Factory.create(
+        {
+            "opensearch": {
+                "type": "ng_dummy_output",
+            }
+        }
+    )
+
+
+@pytest.fixture(name="kafka_output")
+def get_kafka_mock():
+    return Factory.create(
+        {
+            "kafka": {
+                "type": "ng_dummy_output",
+            }
+        }
+    )
+
+
+@pytest.fixture(name="error_output")
+def get_error_output_mock():
+    return Factory.create(
+        {
+            "error": {
+                "type": "ng_dummy_output",
+            }
+        }
+    )
+
+
 @pytest.fixture(name="pipeline")
 def get_pipeline_mock(input_connector, processors):
     """Create a mock for the Pipeline class."""
@@ -72,6 +108,24 @@ def get_pipeline_mock(input_connector, processors):
 class TestSender:
     """Test the Sender class."""
 
-    def test_sender_initialization(self, pipeline):
-        sender = Sender(pipeline=pipeline, outputs=[], error_output=None)
+    def test_sender_initialization(self, pipeline, opensearch_output):
+        sender = Sender(pipeline=pipeline, outputs=[opensearch_output], error_output=None)
         assert sender
+        assert sender._pipeline == pipeline
+        assert sender._outputs == {opensearch_output.name: opensearch_output}
+        assert sender._error_output is None
+
+    def test_sender_sends_events_to_output(self, pipeline, kafka_output):
+        sender = Sender(pipeline=pipeline, outputs=[kafka_output], error_output=None)
+        events = list(sender)
+        assert len(events) == 3
+        assert len(kafka_output.events) == 3, "all events should be sent to the output"
+
+    def test_sender_sends_failed_events_to_error_output(
+        self, pipeline, opensearch_output, error_output
+    ):
+        sender = Sender(pipeline=pipeline, outputs=[opensearch_output], error_output=error_output)
+        with mock.patch.object(sender._pipeline._processors[0], "_apply_rules") as mock_process:
+            mock_process.side_effect = Exception("Processing error")
+            events = list(sender)
+            assert len(events) == 3
