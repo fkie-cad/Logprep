654a33bea5ef8fb7e26275175a6e494bd60858e1
Jörg Zimmermann <joerg.zimmermann@bwi.de>
2025-08-26 11:49:01 +0000

    add factroy method to create runner

--- logprep/ng/runner.py
+++ logprep/ng/runner.py
@@ -2,11 +2,36 @@
 Runner module
 """
 
+from logprep.factory import Factory
+from logprep.ng.pipeline import Pipeline
+from logprep.ng.sender import Sender
 from logprep.util.configuration import Configuration
 
 
 class Runner:
     """Class responsible for running the log processing pipeline."""
 
-    def __init__(self, configuration: Configuration) -> None:
-        self._configuration = configuration
+    def __init__(self, sender: Sender) -> None:
+        self.sender = sender
+
+    @classmethod
+    def from_configuration(cls, configuration: Configuration) -> "Runner":
+        """Factory method to build the Runner"""
+        input_connector = Factory.create(configuration.input) if configuration.input else iter([])
+        output_connectors = [
+            Factory.create({output_name: output})
+            for output_name, output in configuration.output.items()
+        ]
+        error_output = (
+            Factory.create(configuration.error_output) if configuration.error_output else None
+        )
+        processors = [
+            Factory.create(processor_config) for processor_config in configuration.pipeline
+        ]
+        pipeline = Pipeline(
+            input_connector=input_connector,
+            processors=processors,
+            process_count=configuration.process_count,
+        )
+        sender = Sender(pipeline=pipeline, outputs=output_connectors, error_output=error_output)
+        return cls(sender)
--- tests/unit/ng/test_runner.py
+++ tests/unit/ng/test_runner.py
@@ -2,18 +2,135 @@
 # pylint: disable=attribute-defined-outside-init
 import pytest
 
+from logprep.factory import Factory
+from logprep.ng.event.event_state import EventStateType
+from logprep.ng.event.log_event import LogEvent
+from logprep.ng.pipeline import Pipeline
 from logprep.ng.runner import Runner
+from logprep.ng.sender import Sender
 from logprep.util.configuration import Configuration
 
 
+@pytest.fixture(name="input_connector")
+def get_input_mock():
+    return iter(
+        [
+            LogEvent({"message": "Log message 1"}, original=b"", state=EventStateType.RECEIVED),
+            LogEvent({"message": "Log message 2"}, original=b"", state=EventStateType.RECEIVED),
+            LogEvent({"user": {"name": "John Doe"}}, original=b"", state=EventStateType.RECEIVED),
+        ]
+    )
+
+
+@pytest.fixture(name="opensearch_output")
+def get_opensearch_mock():
+    return Factory.create(
+        {
+            "opensearch": {
+                "type": "ng_dummy_output",
+            }
+        }
+    )
+
+
+@pytest.fixture(name="kafka_output")
+def get_kafka_mock():
+    return Factory.create(
+        {
+            "kafka": {
+                "type": "ng_dummy_output",
+                "default": False,
+            }
+        }
+    )
+
+
+@pytest.fixture(name="error_output")
+def get_error_output_mock():
+    return Factory.create(
+        {
+            "error": {
+                "type": "ng_dummy_output",
+            }
+        }
+    )
+
+
+@pytest.fixture(name="pipeline")
+def get_pipeline_mock(input_connector, processors):
+    """Create a mock for the Pipeline class."""
+    return Pipeline(input_connector, processors)
+
+
+@pytest.fixture(name="sender")
+def get_sender_mock(pipeline, kafka_output, opensearch_output, error_output):
+    """Create a mock for the Sender class."""
+    return Sender(
+        pipeline=pipeline, outputs=[kafka_output, opensearch_output], error_output=error_output
+    )
+
+
 @pytest.fixture(name="configuration")
-def get_logprep_config() -> Configuration:
-    return Configuration()
+def get_logprep_config():
+    config_dict = {
+        "pipeline": [
+            {
+                "processor_0": {
+                    "type": "ng_generic_adder",
+                    "rules": [
+                        {
+                            "filter": "*",
+                            "generic_adder": {"add": {"event.tags": "generic added tag"}},
+                        }
+                    ],
+                }
+            },
+            {
+                "processor_1": {
+                    "type": "ng_pseudonymizer",
+                    "pubkey_analyst": "examples/exampledata/rules/pseudonymizer/example_analyst_pub.pem",
+                    "pubkey_depseudo": "examples/exampledata/rules/pseudonymizer/example_depseudo_pub.pem",
+                    "regex_mapping": "examples/exampledata/rules/pseudonymizer/regex_mapping.yml",
+                    "hash_salt": "a_secret_tasty_ingredient",
+                    "outputs": [{"kafka": "pseudonyms"}],
+                    "rules": [
+                        {
+                            "filter": "user.name",
+                            "pseudonymizer": {
+                                "id": "pseudonymizer-1a3c69b2-5d54-4b6b-ab07-c7ddbea7917c",
+                                "mapping": {"user.name": "RE_WHOLE_FIELD"},
+                            },
+                        }
+                    ],
+                    "max_cached_pseudonyms": 1000000,
+                }
+            },
+        ],
+        "output": {
+            "kafka": {
+                "type": "ng_dummy_output",
+                "default": False,
+            },
+            "opensearch": {
+                "type": "ng_dummy_output",
+            },
+        },
+        "error_output": {
+            "error": {
+                "type": "ng_dummy_output",
+            }
+        },
+    }
+    return Configuration(**config_dict)
 
 
 class TestRunner:
-    def setup_method(self):
-        self.runner = Runner()
+    def setup_method(self, sender):
+        self.runner = Runner(sender=sender)
 
     def test_runner_initializes(self):
-        assert self.runner is not None
+        assert isinstance(self.runner, Runner)
+
+    def test_from_configuration(self, configuration):
+        runner = Runner.from_configuration(configuration)
+        assert isinstance(runner, Runner)
