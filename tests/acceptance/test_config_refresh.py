f03df6d3dd885b5881290842a7f16fc3abfc173c
Piotr Pauksztelo <piotr.pauksztelo@fkie.fraunhofer.de>
2025-10-31 15:44:53 +0100

    Fix acceptance test for configuration

--- tests/acceptance/test_config_refresh.py
+++ tests/acceptance/test_config_refresh.py
@@ -56,12 +56,12 @@ def test_two_times_config_refresh_after_5_seconds(tmp_path, config):
     stop_logprep(proc)
 
 
-def test_no_config_refresh_after_5_seconds(tmp_path, config):
+def test_config_refresh_after_5_seconds_without_change(tmp_path, config):
     config.config_refresh_interval = 5
     config.metrics = {"enabled": False}
     config_path = tmp_path / "generated_config.yml"
     config_path.write_text(config.as_json())
     proc = start_logprep(config_path)
     wait_for_output(proc, "Config refresh interval is set to: 5 seconds", test_timeout=8)
-    wait_for_output(proc, "Configuration didn't change.", test_timeout=7)
+    wait_for_output(proc, "Successfully reloaded configuration", test_timeout=7)
     stop_logprep(proc)
