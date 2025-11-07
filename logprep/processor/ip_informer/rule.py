27e465a2585e1de6eb975181611442b8489b0e08
Pablu23 <me@pablu.de>
2025-11-07 12:18:52 +0000

    Fix ip_informer to work for python3.14 and prior

--- logprep/processor/ip_informer/rule.py
+++ logprep/processor/ip_informer/rule.py
@@ -67,11 +67,21 @@ from logprep.processor.field_manager.rule import FieldManagerRule
 
 def get_ip_property_names(cls):
     """Returns all property names"""
-    return [
+
+    # Python version depending: in 3.14+ max_prefixlen and version are now constants hanging off of either IPv4Address or IPv6Address
+    # making them not appear in dir(cls) as properties anymore. So we add them manually here.
+    constants = [
+        "max_prefixlen",
+        "version"
+    ]
+    properties = [
         prop_name
-        for prop_name in filter(lambda x: x not in ("packed", "ipv6_mapped"), dir(cls))
+        # we have to remove the property `packed` because it is not json serializable, as well as max_prefixlen and version because they are constants
+        for prop_name in filter(lambda x: x not in ("packed", "ipv6_mapped", "max_prefixlen", "version"), dir(cls))
         if isinstance(getattr(cls, prop_name), property)
-    ]  # we have to remove the property `packed` because it is not json serializable
+    ]
+
+    return [*constants, *properties] 
 
 
 IP_PROPERTIES = [*get_ip_property_names(IPv4Address), *get_ip_property_names(IPv6Address)]
