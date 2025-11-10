84a81b22d427d89bbf28c7d50468c49a604308f4
Piotr Pauksztelo <piotr.pauksztelo@fkie.fraunhofer.de>
2025-11-10 09:38:05 +0100

    Remove None type from generic adder rule

--- logprep/processor/generic_adder/rule.py
+++ logprep/processor/generic_adder/rule.py
@@ -174,7 +174,7 @@ class GenericAdderRule(FieldManagerRule):
                     missing_files.append(add_file)
                     continue
                 if isinstance(add_dict, dict) and all(
-                    isinstance(value, (str, bool, list, int, float, type(None)))
+                    isinstance(value, (str, bool, list, int, float))
                     for value in add_dict.values()
                 ):
                     self.add = {**self.add, **add_dict}
