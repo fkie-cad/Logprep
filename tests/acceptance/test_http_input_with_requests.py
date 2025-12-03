b8ab16003bb727392e7c4a559232c2603c2cc135
kaya-david <david.kaya@bwi.de>
2025-12-03 13:42:13 +0100

    feat: add acceptance tests for HTTP input metadata collection (#911)
    * test: add acceptance tests for HTTP input metadata collection

--- tests/acceptance/test_http_input_with_requests.py
+++ tests/acceptance/test_http_input_with_requests.py
@@ -1,6 +1,8 @@
 # pylint: disable=missing-docstring
 # pylint: disable=line-too-long
+import json
 import time
+from collections.abc import Iterable
 from logging import DEBUG, basicConfig, getLogger
 from pathlib import Path
 
@@ -77,3 +79,70 @@ def test_http_input_accepts_message_for_multiple_pipelines(tmp_path: Path, confi
         time.sleep(0.5)
 
         assert "my message" in output_path.read_text()
+
+
+@pytest.mark.parametrize(
+    ("collect_meta", "expect_specific_metafield_name", "expect_metafields"),
+    [
+        pytest.param(
+            True,
+            None,
+            ("url", "remote_addr", "user_agent"),
+            id="collect_meta_true_by_default_metafield_name",
+        ),
+        pytest.param(False, None, None, id="collect_meta_false_by_default_metafield_name"),
+        pytest.param(
+            True,
+            "custom",
+            ("url", "remote_addr", "user_agent"),
+            id="collect_meta_true_by_specific_metafield_name",
+        ),
+    ],
+)
+def test_http_input_respects_collect_meta_flag(
+    tmp_path: Path,
+    config: Configuration,
+    collect_meta: bool,
+    expect_specific_metafield_name: str,
+    expect_metafields: Iterable[str] | None,
+):
+    config.input["testinput"]["collect_meta"] = collect_meta
+
+    if expect_specific_metafield_name is not None:
+        config.input["testinput"]["metafield_name"] = expect_specific_metafield_name
+    else:
+        expect_specific_metafield_name = "@metadata"
+
+    output_path = tmp_path / "output.jsonl"
+    config.output = {
+        "testoutput": {
+            "type": "jsonl_output",
+            "output_file": str(output_path),
+        }
+    }
+
+    config_path = tmp_path / "generated_config.yml"
+    config_path.write_text(config.as_yaml())
+
+    with run_logprep(config_path) as proc:
+        wait_for_output(proc, "Uvicorn running on https://127.0.0.1:9000", test_timeout=15)
+
+        requests.post(
+            "https://127.0.0.1:9000/plaintext",
+            data="my message",
+            verify=False,
+            timeout=5,
+        )
+
+        time.sleep(0.5)
+        output_str = output_path.read_text()
+        output_json = json.loads(output_str)
+
+        if expect_metafields is not None:
+            assert expect_specific_metafield_name in output_json, output_str
+            for field in expect_metafields:
+                assert (
+                    field in output_json[expect_specific_metafield_name]
+                ), f"{field} not in {output_str}"
+        else:
+            assert expect_specific_metafield_name not in output_json, output_str
