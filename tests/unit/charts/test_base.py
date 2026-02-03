a6e57c71a483e7f2e88d0ef512e9f6d3c161f328
Pablu23 <me@pablu.de>
2026-02-03 11:29:32 +0000

    Update tests, enable service accounts by default

--- tests/unit/charts/test_base.py
+++ tests/unit/charts/test_base.py
@@ -49,7 +49,7 @@ class TestLogprepChart(TestBaseChartTest):
     def test_manifests_are_rendered(self):
         assert self.manifests
         assert len(self.manifests) > 0
-        assert len(self.manifests) == 8
+        assert len(self.manifests) == 9
 
     def test_extra_labels_are_populated(self):
         logprep_values = {"extraLabels": {"foo": "bar"}}
