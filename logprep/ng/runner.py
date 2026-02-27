bbec749bb9497357df26142fb1bd611bfcc3b54c
Michael Hoff <9436725+mhoff@users.noreply.github.com>
2026-02-27 12:44:42 +0100

    refactor to use more composition and less events in top-level code

--- logprep/ng/runner.py
+++ logprep/ng/runner.py
@@ -12,13 +12,15 @@ from collections.abc import AsyncGenerator
 from attrs import asdict
 
 from logprep.ng.manager import PipelineManager
+from logprep.ng.util.async_helpers import TerminateTaskGroup, restart_task_on_iter
 from logprep.ng.util.configuration import Configuration
 from logprep.ng.util.defaults import DEFAULT_LOG_CONFIG
 
 logger = logging.getLogger("Runner")
 
 
-GRACEFUL_SHUTDOWN_TIMEOUT = 10
+GRACEFUL_SHUTDOWN_TIMEOUT = 3
+HARD_SHUTDOWN_TIMEOUT = 5
 MAX_CONFIG_REFRESH_INTERVAL_DEVIATION_PERCENT = 0.05
 
 
@@ -39,22 +41,23 @@ class Runner:
         Component wiring is deferred to `setup()` to preserve the required init order.
         """
 
-        self.configuration = configuration
+        self.config = configuration
         self._running_config_version: None | str = None
-        self._main_task: asyncio.Task | None = None
+        self._task_group = asyncio.TaskGroup()
+        self._stop_event = asyncio.Event()
 
         self._pipeline_manager: PipelineManager | None = None
 
     async def _refresh_configuration_gen(self) -> AsyncGenerator[Configuration, None]:
-        self.configuration.schedule_config_refresh()
-        refresh_interval = self.configuration.config_refresh_interval
+        self.config.schedule_config_refresh()
+        refresh_interval = self.config.config_refresh_interval
         while True:
-            self.configuration.refresh()
+            self.config.refresh()
 
-            if self.configuration.version != self._running_config_version:
-                yield self.configuration
-                self._running_config_version = self.configuration.version
-                refresh_interval = self.configuration.config_refresh_interval
+            if self.config.version != self._running_config_version:
+                yield self.config
+                self._running_config_version = self.config.version
+                refresh_interval = self.config.config_refresh_interval
 
             if refresh_interval is not None:
                 try:
@@ -73,66 +76,52 @@ class Runner:
                 logger.debug("Config refresh has been disabled.")
                 break
 
-    async def _run_pipeline(self, config: Configuration) -> tuple[PipelineManager, asyncio.Task]:
-        manager = PipelineManager(config)
-        manager_task = asyncio.create_task(manager.run(), name="pipeline_manager")
-        return manager, manager_task
-
-    async def _shut_down_pipeline(
-        self, manager: PipelineManager, manager_task: asyncio.Task
-    ) -> None:
-        await manager.shut_down()
-        try:
-            await asyncio.wait_for(manager_task, GRACEFUL_SHUTDOWN_TIMEOUT)
-            logger.info("graceful shut down of pipeline manager succeeded")
-        except TimeoutError:
-            logger.error(
-                "could not gracefully shut down pipeline manager within timeframe", exc_info=True
-            )
-
-    async def _run(self) -> None:
-        logger.debug("Running _run")
-        try:
-            manager, manager_task = await self._run_pipeline(self.configuration)
-
-            async for refreshed_config in self._refresh_configuration_gen():
-                logger.debug("Configuration change detected. Restarting pipeline...")
-                await self._shut_down_pipeline(manager, manager_task)
-                manager, manager_task = await self._run_pipeline(refreshed_config)
-
-            logger.debug("Configuration refresh disabled. Waiting for ")
-            await manager_task
-        except asyncio.CancelledError:
-            if manager is not None and manager_task is not None:
-                await self._shut_down_pipeline(manager, manager_task)
-
-        logger.debug("End of _run")
-
     async def run(self) -> None:
         """Run the runner and continuously process events until stopped."""
-        self._running_config_version = self.configuration.version
+        self._running_config_version = self.config.version
 
-        self._main_task = asyncio.create_task(self._run(), name="config_refresh")
+        try:
+            async with self._task_group as tg:
+                tg.create_task(TerminateTaskGroup.raise_on_event(self._stop_event))
 
-        await self._main_task
+                def start_pipeline(config: Configuration) -> asyncio.Task:
+                    return tg.create_task(
+                        PipelineManager(config, shutdown_timeout_s=GRACEFUL_SHUTDOWN_TIMEOUT).run(),
+                        name="pipeline_manager",
+                    )
 
-        self.shut_down()
+                try:
+                    async for _ in restart_task_on_iter(
+                        source=self._refresh_configuration_gen(),
+                        task_factory=start_pipeline,
+                        cancel_timeout_s=HARD_SHUTDOWN_TIMEOUT,
+                        inital_task=start_pipeline(self.config),
+                    ):
+                        logger.debug(
+                            "A new pipeline task has been spawned based on the latest configuration"
+                        )
+                except TimeoutError:
+                    logger.error(
+                        "Could not gracefully shut down pipeline manager within timeframe",
+                        exc_info=True,
+                    )
+                    raise
+        except ExceptionGroup as eg:
+            if not eg.exceptions or len(eg.exceptions) > 1:
+                raise
+            match list(eg.exceptions)[0]:
+                case TerminateTaskGroup():
+                    logger.debug("Task group terminated")
+                case _:
+                    raise
+
         logger.debug("End log processing.")
 
     def stop(self) -> None:
         """Stop the runner and signal the underlying processing pipeline to exit."""
 
         logger.info("Stopping runner and exiting...")
-        if self._main_task is not None:
-            logger.debug("Cancelling runner main task")
-            self._main_task.cancel()
-        else:
-            logger.debug("Attempting to stop inactive runner")
-
-    def shut_down(self) -> None:
-        """Shut down runner components, and required runner attributes."""
-
-        logger.info("Runner shut down complete.")
+        self._stop_event.set()
 
     def setup_logging(self) -> None:
         """Setup the logging configuration.
@@ -143,6 +132,6 @@ class Runner:
 
         warnings.simplefilter("always", DeprecationWarning)
         logging.captureWarnings(True)
-        log_config = DEFAULT_LOG_CONFIG | asdict(self.configuration.logger)
+        log_config = DEFAULT_LOG_CONFIG | asdict(self.config.logger)
         os.environ["LOGPREP_LOG_CONFIG"] = json.dumps(log_config)
         logging.config.dictConfig(log_config)
