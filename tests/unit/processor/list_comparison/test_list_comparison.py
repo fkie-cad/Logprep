11e03d45aa8d3e142d704a56da51601f09049c5d
Michael Hoff <9436725+mhoff@users.noreply.github.com>
2026-07-21 22:20:54 +0200

    wip

--- tests/unit/processor/list_comparison/test_list_comparison.py
+++ tests/unit/processor/list_comparison/test_list_comparison.py
@@ -11,7 +11,7 @@ import pytest
 import responses
 
 from logprep.factory import Factory
-from logprep.processor.base.exceptions import FieldExistsWarning, ProcessingWarning
+from logprep.processor.base.exceptions import ProcessingWarning
 from logprep.processor.list_comparison.rule import ListComparisonRule
 from logprep.util.defaults import ENV_NAME_LOGPREP_GETTER_CONFIG
 from logprep.util.getter import (
@@ -270,15 +270,6 @@ class TestListComparison(BaseProcessorTestCase):
 
         assert document == expected
 
-    def test_element_not_in_list(self):
-        # Test if user Charlotte is not in user list
-        document = {"user": "Charlotte"}
-
-        self.object.process(document)
-
-        assert len(document.get("user_results", {}).get("not_in_list")) == 1
-        assert document.get("user_results", {}).get("in_list") is None
-
     def test_element_in_two_lists(self):
         # Tests if the system name Franz appears in two lists, username Mark is in no list
         document = {"user": "Mark", "system": "Franz"}
@@ -302,16 +293,6 @@ class TestListComparison(BaseProcessorTestCase):
         assert len(document.get("user_results", {}).get("not_in_list")) == 1
         assert document.get("user_results", {}).get("in_list") is None
 
-    def test_two_lists_with_one_matched(self):
-        document = {"system": "Alpha", "user": "Charlotte"}
-
-        self.object.process(document)
-
-        assert len(document.get("user_results", {}).get("not_in_list")) != 0
-        assert document.get("user_results", {}).get("in_list") is None
-        assert document.get("user_and_system_results", {}).get("not_in_list") is None
-        assert len(document.get("user_and_system_results", {}).get("in_list")) != 0
-
     def test_dotted_output_field(self):
         # tests if outputting list_comparison results to dotted fields works
         document = {"dot_channel": "test", "user": "Franz"}
@@ -331,28 +312,6 @@ class TestListComparison(BaseProcessorTestCase):
             .get("not_in_list")
             is None
         )
-
-    def test_extend_dotted_output_field(self):
-        # tests if list_comparison properly extends lists already present in output fields.
-        document = {
-            "user": "Franz",
-            "dotted": {"user_results": {"in_list": ["already_present"]}},
-        }
-
-        rule_dict = {
-            "filter": "user",
-            "list_comparison": {
-                "source_fields": ["user"],
-                "target_field": "dotted.user_results",
-                "list_file_paths": ["../lists/user_list.txt"],
-            },
-        }
-        self._load_rule(rule_dict)
-        self.object.setup()
-        self.object.process(document)
-
-        assert document.get("dotted", {}).get("user_results", {}).get("not_in_list") is None
-        assert len(document.get("dotted", {}).get("user_results", {}).get("in_list")) == 2
 
     def test_dotted_parent_field_exists_but_subfield_doesnt(self):
         # tests if list_comparison properly extends lists already present in output fields.
@@ -378,112 +337,6 @@ class TestListComparison(BaseProcessorTestCase):
         assert (
             len(document.get("dotted", {}).get("preexistent_output_field", {}).get("in_list")) == 1
         )
-
-    def test_target_field_exists_and_cant_be_extended(self):
-        document = {"dot_channel": "test", "user": "Franz", "dotted": "dotted_Franz"}
-        expected = {
-            "dot_channel": "test",
-            "user": "Franz",
-            "dotted": "dotted_Franz",
-            "tags": ["_list_comparison_failure"],
-        }
-
-        rule_dict = {
-            "filter": "user",
-            "list_comparison": {
-                "source_fields": ["user"],
-                "target_field": "dotted.user_results",
-                "list_file_paths": ["../lists/user_list.txt"],
-            },
-        }
-        self._load_rule(rule_dict)
-        self.object.setup()
-        result = self.object.process(document)
-        assert len(result.warnings) == 1
-        assert isinstance(result.warnings[0], FieldExistsWarning)
-        assert document == expected
-
-    def test_intermediate_output_field_is_wrong_type(self):
-        document = {
-            "dot_channel": "test",
-            "user": "Franz",
-            "dotted": {"user_results": ["do_not_look_here"]},
-        }
-        expected = {
-            "tags": ["_list_comparison_failure"],
-            "dot_channel": "test",
-            "user": "Franz",
-            "dotted": {"user_results": ["do_not_look_here"]},
-        }
-
-        rule_dict = {
-            "filter": "user",
-            "list_comparison": {
-                "source_fields": ["user"],
-                "target_field": "dotted.user_results.do_not_look_here",
-                "list_file_paths": ["../lists/user_list.txt"],
-            },
-        }
-        self._load_rule(rule_dict)
-        self.object.setup()
-        result = self.object.process(document)
-        assert len(result.warnings) == 1
-        assert isinstance(result.warnings[0], FieldExistsWarning)
-        assert document == expected
-
-    def test_check_in_dotted_subfield(self):
-        document = {"channel": {"type": "fast"}}
-
-        self.object.process(document)
-
-        assert len(document.get("channel_results", {}).get("not_in_list")) == 2
-        assert document.get("channel_results", {}).get("in_list") is None
-
-    def test_ignore_comment_in_list(self):
-        # Tests for a comment inside a list, but as a field inside a document to check
-        # if the comment is actually ignored
-        document = {"user": "# This is a doc string for testing"}
-
-        self.object.process(document)
-
-        assert len(document.get("user_results", {}).get("not_in_list")) == 1
-        assert document.get("user_results", {}).get("in_list") is None
-
-    def test_delete_source_field(self):
-        document = {"user": "Franz"}
-        rule_dict = {
-            "filter": "user",
-            "list_comparison": {
-                "source_fields": ["user"],
-                "target_field": "user_results",
-                "list_file_paths": ["../lists/user_list.txt"],
-                "delete_source_fields": True,
-            },
-        }
-        expected = {"user_results": {"in_list": ["user_list.txt"]}}
-        self._load_rule(rule_dict)
-        self.object.setup()
-        self.object.process(document)
-        assert document == expected
-
-    def test_overwrite_target_field(self):
-        document = {"user": "Franz"}
-        expected = {"user": "Franz", "tags": ["_list_comparison_failure"]}
-        rule_dict = {
-            "filter": "user",
-            "list_comparison": {
-                "source_fields": ["user"],
-                "target_field": "user",
-                "list_file_paths": ["../lists/user_list.txt"],
-                "overwrite_target": True,
-            },
-        }
-        self._load_rule(rule_dict)
-        self.object.setup()
-        result = self.object.process(document)
-        assert len(result.warnings) == 1
-        assert isinstance(result.warnings[0], FieldExistsWarning)
-        assert document == expected
 
     @responses.activate
     def test_list_comparison_loads_rule_with_http_template_in_list_search_base_path(self):
@@ -1331,40 +1184,6 @@ Heinz
         self.object.setup()
         self.object.process(document)
         assert document == expected
-
-    @pytest.mark.parametrize(
-        "testcase, system, result",
-        [
-            ("string in list", "Alpha", {"in_list": ["system_list.txt"]}),
-            ("string not in list", "Omega", {"not_in_list": ["system_list.txt"]}),
-            ("list element in list", ["Alpha"], {"in_list": ["system_list.txt"]}),
-            ("list element not in list", ["Omega"], {"not_in_list": ["system_list.txt"]}),
-            ("one list element in list", ["Alpha", "Omega"], {"in_list": ["system_list.txt"]}),
-            ("multiple list elements in list", ["Alpha", "Beta"], {"in_list": ["system_list.txt"]}),
-        ],
-    )
-    def test_match_list_field(self, testcase, system, result):
-        document = {"system": system}
-        expected = {"system": system, "system_results": result}
-        rule_dict = {
-            "filter": "system",
-            "list_comparison": {
-                "source_fields": ["system"],
-                "target_field": "system_results",
-                "list_file_paths": ["../lists/system_list.txt"],
-            },
-        }
-        config = {
-            "type": "list_comparison",
-            "rules": [],
-            "list_search_base_path": self.CONFIG["list_search_base_path"],
-        }
-        processor = Factory.create({"custom_lister": config})
-        rule = processor.rule_class.create_from_dict(rule_dict)
-        processor._rule_tree.add_rule(rule)
-        processor.setup()
-        processor.process(document)
-        assert document == expected, testcase
 
     @pytest.mark.parametrize(
         "http_list_content, expected_result",
