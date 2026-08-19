4cd2a7586ab05c00a5dd889ada56a1e301d5b122
David Kaya <david.kaya@gmx.net>
2026-08-19 15:37:59 +0200

    fix: clean up generic resolver refresh callbacks

--- logprep/ng/processor/generic_resolver/processor.py
+++ logprep/ng/processor/generic_resolver/processor.py
@@ -34,6 +34,7 @@ from logprep.metrics.metrics import GaugeMetric
 from logprep.ng.abc.processor import Processor
 from logprep.ng.processor.field_manager.processor import FieldManager
 from logprep.ng.processor.generic_resolver.rule import GenericResolverRule
+from logprep.ng.util.getter import RefreshableGetter
 from logprep.processor.base.exceptions import FieldExistsWarning
 from logprep.processor.base.rule import Rule
 from logprep.util.helper import (
@@ -217,8 +218,14 @@ class GenericResolver(FieldManager):
         """Set up the processor and initialize its rules."""
         await super().setup()
 
+        RefreshableGetter.remove_callbacks_for_tag(self._job_tag_for_cleanup)
+
         for rule in self.rules:
             rule = typing.cast(GenericResolverRule, rule)
-            await rule.setup()
+            await rule.setup(self._job_tag_for_cleanup)
 
         self._cache_metrics_skip_count = 0
+
+    def _shut_down(self) -> None:
+        RefreshableGetter.remove_callbacks_for_tag(self._job_tag_for_cleanup)
+        return super()._shut_down()
