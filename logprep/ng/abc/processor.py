5a574368ca374e6eda7ab79b61ccc49e2a03552e
Michael Hoff <9436725+mhoff@users.noreply.github.com>
2026-08-19 22:17:42 +0200

    refactor measure_time decorator

--- logprep/ng/abc/processor.py
+++ logprep/ng/abc/processor.py
@@ -143,7 +143,7 @@ class Processor(Component):
         await self._process_rule_tree(event.data, self._rule_tree)
         return self._event
 
-    @Metric.measure_time_async(self_arg=1)
+    @Metric.measure_time(self_arg=1)
     async def _process_rule(self, rule, event):
         await self._apply_rules_wrapper(event, rule)
         rule.metrics.number_of_processed_events += 1
