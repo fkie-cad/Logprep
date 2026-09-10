fce5acfcbd8e057f3bf6bc4e4c73a930c2e0e0f6
Jakob Grote <jakob.grote@protonmail.com>
2026-09-10 14:32:01 +0200

    rework test_key_checker

--- tests/unit/processor/key_checker/test_key_checker.py
+++ tests/unit/processor/key_checker/test_key_checker.py
@@ -5,11 +5,11 @@
 import pytest
 
 from logprep.processor.base.exceptions import FieldExistsWarning
+from tests.conftest import normalize_test_cases
 from tests.unit.processor.base import BaseProcessorTestCase
 
-test_cases = [  # testcase, rule, event, expected
-    (
-        "writes missing root-key in the missing_fields Field",
+example_test_cases = [
+    pytest.param(  # testcase, rule, event, expected
         {
             "filter": "*",
             "key_checker": {
@@ -26,9 +26,9 @@ test_cases = [  # testcase, rule, event, expected
             "_index": "value",
             "missing_fields": ["key2"],
         },
+        id="writes_missing_root-key_in_the_missing_fields_Field",
     ),
-    (
-        "writes missing sub-key in the missing_fields Field",
+    pytest.param(
         {
             "filter": "*",
             "key_checker": {
@@ -44,9 +44,62 @@ test_cases = [  # testcase, rule, event, expected
             },
             "missing_fields": ["testkey.key2"],
         },
+        id="writes_missing_sub-key_in_the_missing_fields_Field",
     ),
-    (
-        "writes the missing key from a list with one missing and 3 existing keys in the missing_fields Field",
+    pytest.param(
+        {
+            "filter": "*",
+            "key_checker": {
+                "source_fields": ["key1.key2", "key1", "key1.key2.key3"],
+                "target_field": "missing_fields",
+            },
+        },
+        {
+            "key1": {
+                "key2": {"key3": {"key3": "key3_value"}, "random_key": "random_key_value"},
+                "_index": "value",
+            }
+        },
+        {
+            "key1": {
+                "key2": {"key3": {"key3": "key3_value"}, "random_key": "random_key_value"},
+                "_index": "value",
+            }
+        },
+        id="detects_multiple_keys",
+    ),
+    pytest.param(
+        {
+            "filter": "*",
+            "key_checker": {
+                "source_fields": ["not.existing.key"],
+                "target_field": "missing_fields",
+                "overwrite_target": True,
+            },
+        },
+        {
+            "key1": {
+                "key2": {"key3": {"key3": "key3_value"}, "random_key": "random_key_value"},
+                "_index": "value",
+            },
+            "randomkey2": "randomvalue2",
+            "missing_fields": ["not.existing.key"],
+        },
+        {
+            "key1": {
+                "key2": {"key3": {"key3": "key3_value"}, "random_key": "random_key_value"},
+                "_index": "value",
+            },
+            "randomkey2": "randomvalue2",
+            "missing_fields": ["not.existing.key"],
+        },
+        id="prevents_duplicates_in_output_field_by_setting_overwrite_target_to_True",
+    ),
+]
+
+test_cases = normalize_test_cases(
+    *example_test_cases,
+    pytest.param(
         {
             "filter": "*",
             "key_checker": {
@@ -67,9 +120,9 @@ test_cases = [  # testcase, rule, event, expected
             },
             "missing_fields": ["key4"],
         },
+        id="writes_the_missing_key_from_a_list_with_one_missing_and_3_existing_keys_in_the_missing_fields_Field",
     ),
-    (
-        "Detects 'root-key1' in the event",
+    pytest.param(
         {
             "filter": "*",
             "key_checker": {
@@ -89,9 +142,9 @@ test_cases = [  # testcase, rule, event, expected
                 "_index": "value",
             }
         },
+        id="detects_root-key1_in_the_event",
     ),
-    (
-        "Detects 'sub-key2' in the event",
+    pytest.param(
         {
             "filter": "*",
             "key_checker": {
@@ -111,31 +164,9 @@ test_cases = [  # testcase, rule, event, expected
                 "_index": "value",
             },
         },
+        id="detects_sub-key2_in_the_event",
     ),
-    (
-        "Detects multiple Keys",
-        {
-            "filter": "*",
-            "key_checker": {
-                "source_fields": ["key1.key2", "key1", "key1.key2.key3"],
-                "target_field": "missing_fields",
-            },
-        },
-        {
-            "key1": {
-                "key2": {"key3": {"key3": "key3_value"}, "random_key": "random_key_value"},
-                "_index": "value",
-            }
-        },
-        {
-            "key1": {
-                "key2": {"key3": {"key3": "key3_value"}, "random_key": "random_key_value"},
-                "_index": "value",
-            }
-        },
-    ),
-    (
-        "Detect key duplication",
+    pytest.param(
         {
             "filter": "*",
             "key_checker": {
@@ -158,9 +189,9 @@ test_cases = [  # testcase, rule, event, expected
             "randomkey2": "randomvalue2",
             "missing_fields": ["key1"],
         },
+        id="detect_key_duplication_1",
     ),
-    (
-        "Detect key duplication2",
+    pytest.param(
         {
             "filter": "*",
             "key_checker": {
@@ -182,9 +213,9 @@ test_cases = [  # testcase, rule, event, expected
             },
             "randomkey2": "randomvalue2",
         },
+        id="detect_key_duplication_2",
     ),
-    (
-        "Extends existing output field list by setting overwrite_target",
+    pytest.param(
         {
             "filter": "*",
             "key_checker": {
@@ -209,35 +240,9 @@ test_cases = [  # testcase, rule, event, expected
             "randomkey2": "randomvalue2",
             "missing_fields": ["i.exists.already", "not.existing.key"],
         },
+        id="extends_existing_output_field_list_by_setting_overwrite_target",
     ),
-    (
-        "Prevents duplicates in output field by setting overwrite_target to True",
-        {
-            "filter": "*",
-            "key_checker": {
-                "source_fields": ["not.existing.key"],
-                "target_field": "missing_fields",
-                "overwrite_target": True,
-            },
-        },
-        {
-            "key1": {
-                "key2": {"key3": {"key3": "key3_value"}, "random_key": "random_key_value"},
-                "_index": "value",
-            },
-            "randomkey2": "randomvalue2",
-            "missing_fields": ["not.existing.key"],
-        },
-        {
-            "key1": {
-                "key2": {"key3": {"key3": "key3_value"}, "random_key": "random_key_value"},
-                "_index": "value",
-            },
-            "randomkey2": "randomvalue2",
-            "missing_fields": ["not.existing.key"],
-        },
-    ),
-]
+)
 
 
 class TestKeyChecker(BaseProcessorTestCase):
@@ -246,10 +251,9 @@ class TestKeyChecker(BaseProcessorTestCase):
         "rules": ["tests/testdata/unit/key_checker/rules"],
     }
 
-    @pytest.mark.parametrize("testcase, rule, event, expected", test_cases)
-    def test_testcases_positiv(
-        self, testcase, rule, event, expected
-    ):  # pylint: disable=unused-argument
+    @pytest.mark.parametrize(["rule", "event", "expected", "context"], test_cases)
+    def test_testcases_positiv(self, rule, event, expected, context, provision_context):
+        provision_context(context)
         self._load_rule(rule)
         self.object.process(event)
         assert event == expected
